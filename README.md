175c00ec3b6730e5621f7576da9821cbdf589a48
Taylor Salo <tsalo006@fiu.edu>
2018-03-24 20:58:34 -0400

    Update README and fill in IBMA methods.
    The IBMA methods are mostly Python translations of the MATLAB code in
    the IBMA toolbox. Still not sure about the empirical null
    distributions, and the MFX GLM is still empty.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-## NiMARE: Neuroimaging Meta-Analysis Research Environment
-A Python library for Coordinate- and Image-based Meta-Analysis.
+# NiMARE: Neuroimaging Meta-Analysis Research Environment
+A Python library for coordinate- and image-based meta-analysis.
+
+## Supported meta-analytic methods (`nimare.meta`)
+- Coordinate-based methods (`nimare.meta.cbma`)
+    - Kernel-based methods
+        - Activation likelihood estimation (ALE)
+        - Specific coactivation likelihood estimation (SCALE)
+        - Multilevel kernel density analysis (MKDA)
+        - Kernel density analysis (KDA)
+    - Model-based methods (`nimare.meta.cbma.model`)
+        - Bayesian hierarchical cluster process model (BHICP)
+        - Hierarchical Poisson/Gamma random field model (HPGRF)
+        - Spatial Bayesian latent factor regression (SBLFR)
+        - Spatial binary regression (SBR)
+- Image-based methods (`nimare.meta.ibma`)
+    - Mixed effects general linear model (MFX-GLM)
+    - Random effects general linear model (RFX-GLM)
+    - Fixed effects general linear model (FFX-GLM)
+    - Stouffer's meta-analysis
+    - Random effects Stouffer's meta-analysis
+    - Weighted Stouffer's meta-analysis
+    - Fisher's meta-analysis
+
+## Additional functionality
+- Functional characterization analysis (`nimare.decode`)
+    - Generalized correspondence latent Dirichlet allocation (GCLDA)
+    - Neurosynth correlation-based decoding
+    - Neurosynth MKDA-based decoding
+    - BrainMap decoding
--- nimare/meta/ibma/ibma.py
+++ nimare/meta/ibma/ibma.py
@@ -4,7 +4,6 @@ Image-based meta-analysis estimators
 from __future__ import division
 
 import numpy as np
-from sklearn.preprocessing import normalize
 from scipy import stats
 
 from .base import IBMAEstimator
@@ -13,145 +12,219 @@ from ..base import MetaResult
 
 class Stouffers(IBMAEstimator):
     """
-    An image-based meta-analytic test using z-statistic images.
-    Average Z, rescaled to N(0,1)
+    A t-test on z-statistic images.
+
+    Parameters
+    ----------
+    dataset : :obj:`nimare.dataset.Dataset`
+        Dataset to analyze.
+    inference : {'rfx', 'ffx'}
+        Whether to run a random- or fixed-effects model.
+    null : {'theoretical', 'empirical'}
+        Whether to compare test statistics to theoretical or empirical null
+        distribution. Empirical null distribution is only possible when
+        inference is set to 'rfx'.
+
+    Requirements:
+        - z
     """
-    def __init__(self, n_iters=10000, voxel_thresh=0.001, clust_thresh=0.05,
-                 corr='FWE', verbose=True, n_cores=4):
+    def __init__(self, dataset, thresh=0.05, inference='ffx', null='theoretical', n_iters=None):
         self.sample = None
         self.mask = None
+        self.dataset = dataset
+        self.thresh = thresh
+        self.inference = inference
+        self.null = null
         self.n_iters = n_iters
-        self.voxel_thresh = voxel_thresh
-        self.clust_thresh = clust_thresh
-        self.corr = corr
-        self.verbose = verbose
-        self.n_cores = n_cores
 
     def fit(self, sample):
-        """
-        look at xarray, or perhaps a dictionary-like object with keys for
-        different input maps, but also some way of logging level of specificity
-        (e.g., exps vs studies), study IDs, and mask.
+        if self.inference == 'rfx':
+            if self.null == 'theoretical':
+                z_maps = sample.get('z')
+                t_map, p_map = stats.ttest_1samp(z_maps, popmean=0, axis=0)
+                log_p_map = -np.log10(p_map)
+                result = MetaResult(t=t_map, p=p_map, log_p=log_p_map, mask=self.mask)
+            elif self.null == 'empirical':
+                n_iters = self.n_iters
+                z_maps = sample.get('z')
+                k = z_maps.shape[0]
+                t_map, _ = stats.ttest_1samp(z_maps, popmean=0, axis=0)
+                p_map = np.ones(t_map.shape)
+                iter_t_maps = np.zeros((n_iters, t_map.shape[0]))
+                for i in n_iters:
+                    iter_z_maps = np.copy(z_maps)
+                    signs = np.random.choice(a=2, size=k, p=[0.5, 0.5])
+                    signs[signs == 0] = -1
+                    iter_z_maps *= signs[:, None]
+                    iter_t_maps[i, :], _ = stats.ttest_1samp(iter_z_maps, popmean=0, axis=0)
 
-        """
-        self.sample = sample
-        self.mask = sample.mask
-
-        # Rescale z maps to mean of zero, variance of one
-        z_maps = self.sample.get('z')
-        z_maps = normalize(z_maps, axis=1)
-        k = z_maps.shape[0]
-
-        # Combine
-        z_map = np.sqrt(k) * np.mean(z_maps, axis=0)
-        p_map = stats.norm.sf(abs(z_map))*2
-
-        result = MetaResult(z=z_map, p=p_map, mask=self.mask)
+                for voxel in range(iter_t_maps.shape[0]):
+                    voxel_perc = stats.percentileofscore(iter_t_maps[:, voxel], t_map[voxel]) / 100.
+                    p_map[voxel] = 1. - (np.abs(voxel_perc - 0.5) * 2)
+                log_p_map = -np.log10(p_map)
+                result = MetaResult(t=t_map, p=p_map, log_p=log_p_map, mask=self.mask)
+            else:
+                raise ValueError('Input null must be "theoretical" or "empirical".')
+        elif self.inference == 'ffx':
+            if self.null == 'theoretical':
+                z_maps = self.sample.get('z')
+                k = z_maps.shape[0]
+                z_map = np.sum(z_maps, axis=0) / np.sqrt(k)
+                p_map = stats.norm.cdf(z_map, loc=0, scale=1)
+                log_p_map = -np.log10(p_map)
+                result = MetaResult(z=z_map, p=p_map, log_p=log_p_map, mask=self.mask)
+            else:
+                raise ValueError('Only theoretical null distribution may be used '
+                                 'for FFX Stouffers.')
+        else:
+            raise ValueError('Input inference must be "rfx" or "ffx".')
         return result
-
-
-class StouffersRFX(IBMAEstimator):
-    """
-    An image-based meta-analytic test using z-statistic images.
-    Submit Zs to one-sample t-test
-    """
-    def __init__(self, dataset, n_iters=10000, voxel_thresh=0.001, clust_thresh=0.05,
-                 corr='FWE', verbose=True, n_cores=4):
-        self.dataset = dataset
-        self.n_iters = n_iters
-        self.voxel_thresh = voxel_thresh
-        self.clust_thresh = clust_thresh
-        self.corr = corr
-        self.verbose = verbose
-        self.n_cores = n_cores
-
-    def fit(self, sample):
-        # Rescale z maps to mean of zero, variance of one
-        z_maps = sample.get('z')
-        t_map, p_map = stats.ttest_1samp(z_maps, popmean=0, axis=0)
-        return t_map, p_map
-
-
-class MFX_GLM(IBMAEstimator):
-    """
-    The gold standard image-based meta-analytic test. Uses contrast and standard
-    error images.
-    """
-    def __init__(self, n_iters=10000, voxel_thresh=0.001, clust_thresh=0.05,
-                 corr='FWE', verbose=True, n_cores=4):
-        self.dataset = None
-        self.n_iters = n_iters
-        self.voxel_thresh = voxel_thresh
-        self.clust_thresh = clust_thresh
-        self.corr = corr
-        self.verbose = verbose
-        self.n_cores = n_cores
-
-    def fit(self, sample, dataset):
-        pass
-
-
-class RFX_GLM(IBMAEstimator):
-    """
-    An image-based meta-analytic test using contrast images.
-    Analyze per-study contrasts as "dat"
-    """
-    def __init__(self, dataset, n_iters=10000, voxel_thresh=0.001, clust_thresh=0.05,
-                 corr='FWE', verbose=True, n_cores=4):
-        self.dataset = dataset
-        self.n_iters = n_iters
-        self.voxel_thresh = voxel_thresh
-        self.clust_thresh = clust_thresh
-        self.corr = corr
-        self.verbose = verbose
-        self.n_cores = n_cores
-
-    def fit(self, sample):
-        pass
-
-
-class FFX_GLM(IBMAEstimator):
-    """
-    An image-based meta-analytic test using contrast and standard error images.
-    Don't estimate variance, just take from first level
-    """
-    def __init__(self, dataset, n_iters=10000, voxel_thresh=0.001, clust_thresh=0.05,
-                 corr='FWE', verbose=True, n_cores=4):
-        self.dataset = dataset
-        self.n_iters = n_iters
-        self.voxel_thresh = voxel_thresh
-        self.clust_thresh = clust_thresh
-        self.corr = corr
-        self.verbose = verbose
-        self.n_cores = n_cores
-
-    def fit(self, sample):
-        studies_matching_criteria = self.dataset.get(images='con AND se')
 
 
 class Fishers(IBMAEstimator):
     """
     An image-based meta-analytic test using t- or z-statistic images.
     Sum of -log P-values (from T/Zs converted to Ps)
+
+    Requirements:
+        - t OR z
     """
-    def __init__(self, dataset, n_iters=10000, voxel_thresh=0.001, clust_thresh=0.05,
-                 corr='FWE', verbose=True, n_cores=4):
+    def __init__(self, dataset, thresh=0.05):
+        self.sample = None
+        self.mask = None
         self.dataset = dataset
-        self.n_iters = n_iters
-        self.voxel_thresh = voxel_thresh
-        self.clust_thresh = clust_thresh
-        self.corr = corr
-        self.verbose = verbose
-        self.n_cores = n_cores
+        self.thresh = thresh
 
     def fit(self, sample):
-        pass
+        z_maps = sample.get('z')
+        k = z_maps.shape[0]
+        ffx_stat_map = -2 * np.sum(np.log10(stats.norm.cdf(-z_maps, loc=0,
+                                                           scale=1)), axis=0)
+        p_map = stats.chi2.cdf(ffx_stat_map, 2*k)
+        log_p_map = -np.log10(p_map)
+        result = MetaResult(ffx_stat=ffx_stat_map, p=p_map, log_p=log_p_map, mask=self.mask)
+        return result
 
 
 class WeightedStouffers(IBMAEstimator):
     """
     An image-based meta-analytic test using z-statistic images and sample sizes.
     Zs from bigger studies get bigger weight
+
+    Requirements:
+        - z
+        - n
+    """
+    def __init__(self, dataset, thresh=0.05):
+        self.sample = None
+        self.mask = None
+        self.dataset = dataset
+        self.thresh = thresh
+
+    def fit(self, sample):
+        z_maps = sample.get('z')
+        sample_sizes = sample.get('n')
+        weighted_z_maps = z_maps * np.sqrt(sample_sizes)[:, None]
+        ffx_stat_map = np.sum(weighted_z_maps, axis=0) / np.sqrt(np.sum(sample_sizes))
+        p_map = stats.norm.cdf(-ffx_stat_map, loc=0, scale=1)
+        log_p_map = -np.log10(p_map)
+        result = MetaResult(ffx_stat=ffx_stat_map, p=p_map, log_p=log_p_map, mask=self.mask)
+        return result
+
+
+class RFX_GLM(IBMAEstimator):
+    """
+    A t-test on contrast images.
+
+    Requirements:
+        - con
+    """
+    def __init__(self, dataset, thresh=0.05, null='theoretical', n_iters=None):
+        self.sample = None
+        self.mask = None
+        self.dataset = dataset
+        self.thresh = thresh
+        self.null = null
+        self.n_iters = n_iters
+
+    def fit(self, sample):
+        con_maps = sample.get('con')
+        if self.null == 'theoretical':
+            t_map, p_map = stats.ttest_1samp(con_maps, popmean=0, axis=0)
+            log_p_map = -np.log10(p_map)
+            result = MetaResult(t=t_map, p=p_map, log_p=log_p_map, mask=self.mask)
+        elif self.null == 'empirical':
+            n_iters = self.n_iters
+            z_maps = sample.get('z')
+            k = z_maps.shape[0]
+            t_map, _ = stats.ttest_1samp(con_maps, popmean=0, axis=0)
+            p_map = np.ones(t_map.shape)
+            iter_t_maps = np.zeros((n_iters, t_map.shape[0]))
+            for i in n_iters:
+                iter_con_maps = np.copy(con_maps)
+                signs = np.random.choice(a=2, size=k, p=[0.5, 0.5])
+                signs[signs == 0] = -1
+                iter_con_maps *= signs[:, None]
+                iter_t_maps[i, :], _ = stats.ttest_1samp(iter_con_maps, popmean=0, axis=0)
+
+            for voxel in range(iter_t_maps.shape[0]):
+                voxel_perc = stats.percentileofscore(iter_t_maps[:, voxel], t_map[voxel]) / 100.
+                p_map[voxel] = 1. - (np.abs(voxel_perc - 0.5) * 2)
+            log_p_map = -np.log10(p_map)
+            result = MetaResult(t=t_map, p=p_map, log_p=log_p_map, mask=self.mask)
+        else:
+            raise ValueError('Input null must be "theoretical" or "empirical".')
+        return result
+
+
+class FFX_GLM(IBMAEstimator):
+    """
+    An image-based meta-analytic test using contrast and standard error images.
+    Don't estimate variance, just take from first level
+
+    Requirements:
+        - con
+        - se
+    """
+    def __init__(self, dataset, n_subjects=None, thresh=0.05, equal_var=True):
+        self.dataset = dataset
+        self.n_subjects = n_subjects
+        self.thresh = thresh
+        self.equal_var = equal_var
+
+    def fit(self, sample):
+        con_maps = sample.get('con')
+        var_maps = sample.get('con_se')
+        k = con_maps.shape[0]
+        if self.n_subjects is not None:
+            n_subjects = np.repeat(self.n_subjects, k)
+        else:
+            n_subjects = sample.get('n')
+
+        if self.equal_var:
+            weighted_con_maps = con_maps * n_subjects[:, None]
+            sum_weighted_con_map = np.sum(weighted_con_maps, axis=0)
+            weighted_ss_maps = var_maps * (n_subjects[:, None] - 1)
+            est_ss_map = (1. / (n_subjects - 1)) * np.sum(weighted_ss_maps, axis=0)
+            ffx_stat_map = (1. / np.sqrt(np.sum(n_subjects))) * sum_weighted_con_map / np.sqrt(est_ss_map)
+            dof = np.sum(n_subjects - 1)
+        else:
+            raise Exception('Unequal variances not available yet.')
+
+        p_map = stats.t.cdf(-ffx_stat_map, df=dof, loc=0, scale=1)
+        log_p_map = -np.log10(p_map)
+        result = MetaResult(ffx_stat=ffx_stat_map, p=p_map, log_p=log_p_map, mask=self.mask)
+        return result
+
+
+class MFX_GLM(IBMAEstimator):
+    """
+    The gold standard image-based meta-analytic test. Uses contrast and standard
+    error images.
+
+    Requirements:
+        - con
+        - se
     """
     def __init__(self, dataset, n_iters=10000, voxel_thresh=0.001, clust_thresh=0.05,
                  corr='FWE', verbose=True, n_cores=4):
@@ -164,4 +237,7 @@ class WeightedStouffers(IBMAEstimator):
         self.n_cores = n_cores
 
     def fit(self, sample):
-        pass
+        con_images = self.dataset.images
+        se_images = self.dataset.images
+        con_data = con_images  # xyz + contrast 4D array
+        se_data = se_images  # xyz + contrast 4D array
