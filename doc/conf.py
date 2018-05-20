66557c70e39b07ac994787758562e6a282fa051a
Taylor Salo <tsalo006@fiu.edu>
2018-05-20 17:04:54 -0400

    Add IBMA example
    I still need to figure out how to call download_collection during
    testing/doc generation to get the files for image-based meta-analyses.

--- doc/conf.py
+++ doc/conf.py
@@ -92,7 +92,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'utils/*']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -161,6 +161,7 @@ sphinx_gallery_conf = {
     # Modules for which function level galleries are created.  In
     # this case sphinx_gallery and numpy in a tuple of strings.
     'doc_module'        : ('nimare'),
+    'ignore_patterns'   : ['utils/'],
     }
 
 # -- Options for Texinfo output -------------------------------------------
