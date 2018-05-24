f0f2eb2f6ff10c2ec8e02970706a4c9dbef8ff24
Taylor Salo <tsalo006@fiu.edu>
2018-05-24 15:03:01 -0400

    Fix paths in dataset tests.
    All tests passing now.

--- nimare/tests/test_dataset.py
+++ nimare/tests/test_dataset.py
@@ -12,7 +12,7 @@ def test_database_smoke():
     """
     Smoke test for nimare.dataset.Database initialization.
     """
-    db_file = op.join(get_test_data_path(), 'dset.json')
+    db_file = op.join(get_test_data_path(), 'neurosynth_dset.json')
     dbase = dataset.Database(db_file)
     assert isinstance(dbase, nimare.dataset.Database)
 
@@ -21,7 +21,7 @@ def test_dataset_smoke():
     """
     Smoke test for nimare.dataset.Dataset initialization.
     """
-    db_file = op.join(get_test_data_path(), 'dset.json')
+    db_file = op.join(get_test_data_path(), 'neurosynth_dset.json')
     dbase = dataset.Database(db_file)
     dset = dbase.get_dataset()
     assert isinstance(dset, nimare.dataset.Dataset)
--- nimare/tests/test_ibma.py
+++ nimare/tests/test_ibma.py
@@ -138,47 +138,71 @@ def test_z_perm():
 
 
 def test_stouffers_ffx():
+    """
+    Smoke test for Stouffer's FFX.
+    """
     result = ibma.stouffers(pytest.z_data, pytest.mask_img,
                             inference='ffx', null='theoretical', n_iters=None)
     assert isinstance(result, nimare.meta.base.MetaResult)
 
 
 def test_stouffers_rfx():
+    """
+    Smoke test for Stouffer's RFX.
+    """
     result = ibma.weighted_stouffers(pytest.z_data, pytest.sample_sizes_z,
                                      pytest.mask_img)
     assert isinstance(result, nimare.meta.base.MetaResult)
 
 
 def test_weighted_stouffers():
+    """
+    Smoke test for Weighted Stouffer's.
+    """
     result = ibma.stouffers(pytest.z_data, pytest.mask_img,
                             inference='rfx', null='theoretical', n_iters=None)
     assert isinstance(result, nimare.meta.base.MetaResult)
 
 
 def test_fishers():
+    """
+    Smoke test for Fisher's.
+    """
     result = ibma.fishers(pytest.z_data, pytest.mask_img)
     assert isinstance(result, nimare.meta.base.MetaResult)
 
 
 def test_con_perm():
+    """
+    Smoke test for contrast permutation.
+    """
     result = ibma.rfx_glm(pytest.con_data, pytest.mask_img, null='empirical',
                           n_iters=10, corr='FDR')
     assert isinstance(result, nimare.meta.base.MetaResult)
 
 
 def test_rfx_glm():
+    """
+    Smoke test for RFX GLM.
+    """
     result = ibma.rfx_glm(pytest.con_data, pytest.mask_img, null='theoretical',
                           n_iters=None)
     assert isinstance(result, nimare.meta.base.MetaResult)
 
 
 def test_ffx_glm():
+    """
+    Smoke test for FFX GLM.
+    """
     result = ibma.ffx_glm(pytest.con_data, pytest.se_data,
                           pytest.sample_sizes_con, pytest.mask_img)
     assert isinstance(result, nimare.meta.base.MetaResult)
 
 
 def test_mfx_glm():
+    """
+    Smoke test for MFX GLM.
+    """
     result = ibma.mfx_glm(pytest.con_data, pytest.se_data,
                           pytest.sample_sizes_con, pytest.mask_img)
     assert isinstance(result, nimare.meta.base.MetaResult)
