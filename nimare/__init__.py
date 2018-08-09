9f230011213db7d2a7fb2f5ae6b4a2ca37920193
Taylor Salo <tsalo006@fiu.edu>
2018-08-09 13:30:54 -0400

    Add Neuroinformatics 2018 poster citation.

--- nimare/__init__.py
+++ nimare/__init__.py
@@ -1,8 +1,18 @@
 """
 NiMARE: Neuroimaging Meta-Analysis Research Environment
 """
-from .dataset import Dataset
+from .due import (due, Doi)
+from .dataset import Dataset, Database
 from .meta import cbma
+from .decode import GCLDADiscreteDecoder
 from .version import __version__
 
-del cbma, Dataset
+del cbma, Dataset, Database, GCLDADiscreteDecoder
+
+
+# Citation for the algorithm.
+due.cite(Doi('10.7490/f1000research.1115905.1'),
+         description='Neuroinformatics 2018 poster introducing NiMARE.',
+         path='nimare', cite_module=True)
+
+# Citation for package version
