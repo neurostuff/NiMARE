a01a3bf48b4162688fea47f60205a5eb319e4f1c
Taylor Salo <tsalo006@fiu.edu>
2018-08-13 09:53:26 -0400

    Fix comment.

--- nimare/__init__.py
+++ nimare/__init__.py
@@ -9,7 +9,7 @@ from .version import __version__
 del cbma, Dataset
 
 
-# Citation for the algorithm.
+# General citation for the package
 due.cite(Doi('10.7490/f1000research.1115905.1'),
          description='Neuroinformatics 2018 poster introducing NiMARE.',
          path='nimare', cite_module=True)
