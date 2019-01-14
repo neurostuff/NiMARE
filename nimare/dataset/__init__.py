e650a231e9e647d0ce760d96c7a0eb896229aa2b
James Kent <james-kent@uiowa.edu>
2019-01-14 12:05:20 -0500

    attempt to get nidm_pain_meta-analyses.ipynb working

--- nimare/dataset/__init__.py
+++ nimare/dataset/__init__.py
@@ -3,4 +3,4 @@ Top-level namespace for datasets.
 """
 from ..base import Contrast
 from .dataset import Database, Dataset
-from .extract import convert_sleuth
+from .extract import convert_sleuth, DataSource
