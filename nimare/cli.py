c0d1dbe5f710a804349e95b3344194b426d89e44
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2019-01-16 13:33:55 -0500

    adding peaks2maps CLI

--- nimare/cli.py
+++ nimare/cli.py
@@ -1,6 +1,7 @@
 import click
 
 from nimare.workflows.ale import ale_sleuth_inference
+from nimare.workflows.peaks2maps import peaks2maps
 
 
 @click.group()
@@ -9,3 +10,4 @@ def cli():
 
 
 cli.add_command(ale_sleuth_inference)
+cli.add_command(peaks2maps)
