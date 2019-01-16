69e522f1785eda9e5dcb8428b020c00e1a6ce08f
Satrajit Ghosh <satra@mit.edu>
2019-01-16 10:43:14 -0500

    fix: suppress all warnings for now

--- nimare/__init__.py
+++ nimare/__init__.py
@@ -1,11 +1,15 @@
 """
 NiMARE: Neuroimaging Meta-Analysis Research Environment
 """
-from .dataset import Dataset
-from .meta import cbma
-from .annotate import text
-from .decode import gclda_decode_roi
-from .parcellate import CoordCBP
-from .version import __version__
+import warnings
 
-del cbma, Dataset, text, gclda_decode_roi, CoordCBP
+with warnings.catch_warnings(record=True) as w:
+    warnings.simplefilter("ignore")
+    from .dataset import Dataset
+    from .meta import cbma
+    from .annotate import text
+    from .decode import gclda_decode_roi
+    from .parcellate import CoordCBP
+    from .version import __version__
+
+    del cbma, Dataset, text, gclda_decode_roi, CoordCBP
