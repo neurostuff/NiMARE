4e450530a2aa940f67cb66c671c3677649f20781
Taylor Salo <tsalo006@fiu.edu>
2019-05-29 09:38:29 -0400

    Fix up GCLDA and text extraction methods.

--- nimare/annotate/text.py
+++ nimare/annotate/text.py
@@ -86,7 +86,7 @@ def download_abstracts(dataset, email):
     return dataset
 
 
-def generate_counts(text_df, tfidf=True):
+def generate_counts(text_df, text_column='abstract', tfidf=True):
     """
     Generate tf-idf weights for unigrams/bigrams derived from textual data.
 
@@ -101,8 +101,21 @@ def generate_counts(text_df, tfidf=True):
         A DataFrame where the index is 'id' and the columns are the
         unigrams/bigrams derived from the data. D = document. T = term.
     """
+    if text_column not in text_df.columns:
+        raise ValueError('Column "{0}" not found in DataFrame'.format(text_column))
+
+    # Remove rows with empty text cells
+    orig_ids = text_df['id'].tolist()
+    text_df = text_df.fillna('')
+    keep_ids = text_df.loc[text_df[text_column] != '', 'id']
+    text_df = text_df.loc[text_df['id'].isin(keep_ids)]
+
+    if len(keep_ids) != len(orig_ids):
+        LGR.info('Retaining {0}/{1} studies'.format(len(keep_ids),
+                                                    len(orig_ids)))
+
     ids = text_df['id'].tolist()
-    text = text_df['text'].tolist()
+    text = text_df[text_column].tolist()
     stoplist = op.join(get_resource_path(), 'neurosynth_stoplist.txt')
     with open(stoplist, 'r') as fo:
         stop_words = fo.read().splitlines()
