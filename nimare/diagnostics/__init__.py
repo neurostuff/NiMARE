b492f9c9434daa51472624c47fcc3f4830cd4a00
Taylor Salo <tsalo006@fiu.edu>
2019-06-23 12:25:49 -0400

    Add empty diagnostics module.

---
+++ nimare/diagnostics/__init__.py
@@ -0,0 +1,12 @@
+"""
+Diagnostics for meta-analyses
+"""
+import warnings
+
+warnings.simplefilter('default')
+
+warnings.warn(
+    "{} is an experimental module under active development; use it at your "
+    "own risk.".format(__name__),
+    ImportWarning
+)
