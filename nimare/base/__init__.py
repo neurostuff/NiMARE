e2db7504e1a757b71971201f1e2a9ee7bf28edea
Taylor Salo <tsalo006@fiu.edu>
2019-07-21 11:54:01 -0400

    Remove input base classes (Analyzable, ConnMatrix, etc.)

--- nimare/base/__init__.py
+++ nimare/base/__init__.py
@@ -2,17 +2,13 @@
 Top-level namespace for nimare base.
 """
 
-from .inputs import (Analyzable, Mappable, ConnMatrix, Image, CoordinateSet,
-                     Surface)
 from .decode import Decoder
 from .meta import (KernelTransformer, MetaEstimator, CBMAEstimator,
                    IBMAEstimator, MetaResult)
 from .annotate import (AnnotationModel)
 from .misc import (Parcellator)
 
-__all__ = ['Analyzable', 'Mappable', 'ConnMatrix', 'Image', 'CoordinateSet',
-           'Surface',
-           'Decoder',
+__all__ = ['Decoder',
            'KernelTransformer', 'MetaEstimator', 'CBMAEstimator',
            'IBMAEstimator', 'MetaResult',
            'AnnotationModel', 'Parcellator']
