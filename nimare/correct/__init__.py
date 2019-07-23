c5934846f8cceeb462933a08b75d4655bb9511c4
Taylor Salo <tsalo006@fiu.edu>
2019-07-23 11:53:21 -0400

    Add Tal's Corrector classes.

--- nimare/correct/__init__.py
+++ nimare/correct/__init__.py
@@ -3,17 +3,11 @@ Multiple comparisons correction
 """
 import warnings
 
-from .fdr import FDRCorrector
 
+from .correct import FDRCorrector
 
 __all__ = [
     'FDRCorrector'
 ]
 
 warnings.simplefilter('default')
-
-warnings.warn(
-    "{} is an experimental module under active development; use it at your "
-    "own risk.".format(__name__),
-    ImportWarning
-)
