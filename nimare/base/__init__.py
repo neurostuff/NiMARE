0fe48594c4a110d88379c6e96a6b4d08ea9fe8e4
Taylor Salo <tsalo006@fiu.edu>
2019-07-23 11:45:13 -0400

    Move MetaResult from base.meta to base.base.

--- nimare/base/__init__.py
+++ nimare/base/__init__.py
@@ -2,9 +2,10 @@
 Top-level namespace for nimare base.
 """
 
+from .base import MetaResult
 from .decode import Decoder
 from .meta import (KernelTransformer, MetaEstimator, CBMAEstimator,
-                   IBMAEstimator, MetaResult)
+                   IBMAEstimator)
 from .annotate import (AnnotationModel)
 from .misc import (Parcellator)
 
