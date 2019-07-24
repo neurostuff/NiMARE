960274b2e1fb47b2501c99da2a857a1abf82d21a
Taylor Salo <tsalo006@fiu.edu>
2019-07-24 11:54:15 -0400

    Drop Corrector stuff from this PR.

--- nimare/correct/__init__.py
+++ nimare/correct/__init__.py
@@ -4,10 +4,16 @@ Multiple comparisons correction
 import warnings
 
 
-from .correct import FDRCorrector
+from .fdr import FDRCorrector
 
 __all__ = [
     'FDRCorrector'
 ]
 
 warnings.simplefilter('default')
+
+warnings.warn(
+    "{} is an experimental module under active development; use it at your "
+    "own risk.".format(__name__),
+    ImportWarning
+)
