c057c6f563090ff52f5322447c0dfabab1a6040a
Taylor Salo <tsalo006@fiu.edu>
2019-07-25 18:28:07 -0400

    Update Estimator base class.

--- nimare/base/estimators.py
+++ nimare/base/estimators.py
@@ -32,16 +32,36 @@ class Transformer(NiMAREBase):
 class Estimator(NiMAREBase):
     """Estimators take in Datasets and return MetaResults
     """
-    def __init__(self):
-        pass
 
-    @abstractmethod
-    def fit(self, dataset):
-        """Apply estimation to dataset and output results.
-        """
+    # Inputs that must be available in input Dataset. Keys are names of
+    # attributes to set; values are strings indicating location in Dataset.
+    _inputs = {}
+
+    def _validate_input(self, dataset):
         if not isinstance(dataset, Dataset):
             raise ValueError('Argument "dataset" must be a valid Dataset '
                              'object, not a {0}'.format(type(dataset)))
+        for k, v in self._inputs.items():
+            data = dataset.get(v[0], **v[1])
+            if not data:
+                raise ValueError("Estimator {0} requires input dataset to "
+                                 "contain {1} with filters {2}, but none "
+                                 "were found.".format(self.__class__.__name__,
+                                                      v[0], v[1]))
+            setattr(self, k, data)
+
+    def fit(self, dataset):
+        self._validate_input(dataset)
+        maps = self._fit(dataset)
+        self.results = MetaResult(self, dataset, maps)
+
+    @abstractmethod
+    def _fit(self, dataset):
+        """Apply estimation to dataset and output results. Must return a
+        dictionary of results, where keys are names of images and values are
+        ndarrays.
+        """
+        pass
 
 
 class Result(with_metaclass(ABCMeta)):
