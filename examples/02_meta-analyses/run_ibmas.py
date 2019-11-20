fa5a25b6620f2fe86f130aab37067bb60b92e7a6
Taylor Salo <tsalo006@fiu.edu>
2019-11-20 09:45:38 -0500

    Fix examples.

--- examples/02_meta-analyses/run_ibmas.py
+++ examples/02_meta-analyses/run_ibmas.py
@@ -19,22 +19,19 @@ uploaded by Dr. Camille Maumet.
 ###############################################################################
 # Start with the necessary imports
 # --------------------------------
-import json
-import numpy as np
-from glob import glob
-from os.path import basename, join, dirname, isfile
-import urllib.request
 import os
+import urllib.request
 
+import numpy as np
 import pandas as pd
 import nibabel as nib
-from scipy.stats import t
-from nilearn.masking import apply_mask
+from nilearn.masking import apply_mask, unmask
 from nilearn.plotting import plot_stat_map
 
 import nimare
-from nimare.meta.ibma import (stouffers, fishers, weighted_stouffers,
-                              rfx_glm, ffx_glm)
+from nimare.meta.esma import fishers
+from nimare.meta.ibma import (Fishers, Stouffers, WeightedStouffers,
+                              RFX_GLM, FFX_GLM, ffx_glm)
 
 ###############################################################################
 # Download data
@@ -50,141 +47,89 @@ with open("download_test_data.py", "wb") as f:
 
 # download the requisite data
 from download_test_data import download_dataset
-download_dataset()
+dset_dir = download_dataset()
 os.remove("download_test_data.py")
 
 ###############################################################################
 # Load Dataset
 # --------------------------------------------------
-dset_file = join(dirname(nimare.__file__), 'tests', 'data', 'nidm_pain_dset.json')
-with open(dset_file, 'r') as fo:
-    dset_dict = json.load(fo)
+dset_file = os.path.join(os.path.dirname(nimare.__file__),
+                         'tests', 'data', 'nidm_pain_dset.json')
 dset = nimare.dataset.Dataset(dset_file)
+dset.update_path(dset_dir)
 
-mask_img = dset.mask
+mask_img = dset.masker.mask_img
 
 logp_thresh = -np.log(.05)
 
 ###############################################################################
-# Define functions for querying set of NIDM-Results packs
-# -------------------------------------------------------
-def _get_file(cdict, t):
-    """Return the file associated with a given data type within a
-    folder if it exists. Otherwise, returns an empty list.
-    """
-    temp = ''
-    if t == 'con':
-        temp = cdict['images'].get('con')
-    elif t == 'se':
-        temp = cdict['images'].get('se')
-    elif t == 't':
-        temp = cdict['images'].get('t')
-    elif t == 'z':
-        temp = cdict['images'].get('z')
-    elif t == 'n':
-        temp = cdict.get('sample_sizes', [])
-        if temp:
-            temp = np.mean(temp)
-    else:
-        raise Exception('Input type "{0}" not recognized.'.format(t))
-
-    return temp
-
-
-def get_files(ddict, types):
-    """Returns a list of files associated with a given data type
-    from a set of subfolders within a directory. Allows for
-    multiple data types and only returns a set of files from folders
-    with all of the requested types.
-    """
-    all_files = []
-    for study in ddict.keys():
-        files = []
-        cdict = ddict[study]['contrasts']['1']
-        for t in types:
-            temp = _get_file(cdict, t)
-            if temp:
-                files.append(temp)
-
-        if len(files) == len(types):
-            all_files.append(files)
-    all_files = list(map(list, zip(*all_files)))
-    return all_files
-
-###############################################################################
-# Get z-maps
+# Fisher's (using functions)
 # --------------------------------------------------
-files = get_files(dset_dict, ['z'])
-z_imgs = [nib.load(join(dirname(nimare.__file__), f)) for f in files[0]]
+# Get images for analysis
+files = dset.get_images(imtype='z')
+files = [f for f in files if f]
+z_imgs = [nib.load(f) for f in files]
 z_data = apply_mask(z_imgs, mask_img)
 print('{0} studies found.'.format(z_data.shape[0]))
 
-###############################################################################
-# Fisher's
-# --------------------------------------------------
 result = fishers(z_data, mask_img)
-plot_stat_map(result.images['z'], cut_coords=[0, 0, -8],
+fishers_result = unmask(result['z'], mask_img)
+plot_stat_map(fishers_result, cut_coords=[0, 0, -8],
+              draw_cross=False, cmap='RdBu_r')
+
+###############################################################################
+# Fisher's (using Estimators)
+# --------------------------------------------------
+# Here is the object-oriented approach
+meta = Fishers()
+meta.fit(dset)
+plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
               draw_cross=False, cmap='RdBu_r')
 
 ###############################################################################
 # Stouffer's with fixed-effects inference
 # --------------------------------------------------
-result = stouffers(z_data, mask_img, inference='ffx',
-                   null='theoretical', n_iters=None)
-plot_stat_map(result.images['z'], cut_coords=[0, 0, -8],
+meta = Stouffers(inference='ffx', null='theoretical', n_iters=None)
+meta.fit(dset)
+plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
               draw_cross=False, cmap='RdBu_r')
 
 ###############################################################################
 # Stouffer's with random-effects inference using theoretical null distribution
 # -----------------------------------------------------------------------------
-result = stouffers(z_data, mask_img, inference='rfx',
-                   null='theoretical', n_iters=None)
-plot_stat_map(result.images['z'], cut_coords=[0, 0, -8],
+meta = Stouffers(inference='rfx', null='theoretical', n_iters=None)
+meta.fit(dset)
+plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
               draw_cross=False, cmap='RdBu_r')
 
 ###############################################################################
 # Stouffer's with random-effects inference using empirical null distribution
 # -----------------------------------------------------------------------------
-result = stouffers(z_data, mask_img, inference='rfx',
-                   null='empirical', n_iters=1000)
-plot_stat_map(result.images['z'], cut_coords=[0, 0, -8],
+meta = Stouffers(inference='rfx', null='empirical', n_iters=1000)
+meta.fit(dset)
+plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
               draw_cross=False, cmap='RdBu_r')
-
-###############################################################################
-# Get z-maps + sample sizes
-# --------------------------------------------------
-z_files, ns = get_files(dset_dict, ['z', 'n'])
-z_imgs = [nib.load(join(dirname(nimare.__file__), f)) for f in z_files]
-z_data = apply_mask(z_imgs, mask_img)
-sample_sizes = np.array(ns)
-print('{0} studies found.'.format(z_data.shape[0]))
 
 ###############################################################################
 # Weighted Stouffer's
 # -------------------
-result = weighted_stouffers(z_data, sample_sizes, mask_img)
-plot_stat_map(result.images['z'], cut_coords=[0, 0, -8],
+meta = WeightedStouffers()
+meta.fit(dset)
+plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
               draw_cross=False, cmap='RdBu_r')
-
-###############################################################################
-# Get contrast maps
-# ----------------------------------------------------------------
-con_files = get_files(dset_dict, ['con'])
-con_files = con_files[0]
-con_imgs = [nib.load(join(dirname(nimare.__file__), f)) for f in con_files]
-con_data = apply_mask(con_imgs, mask_img)
-print('{0} studies found.'.format(con_data.shape[0]))
 
 ###############################################################################
 # RFX GLM with theoretical null distribution
 # ------------------------------------------
-result = rfx_glm(con_data, mask_img, null='theoretical', n_iters=None)
-plot_stat_map(result.images['z'], cut_coords=[0, 0, -8],
+meta = RFX_GLM(null='theoretical', n_iters=None)
+meta.fit(dset)
+plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
               draw_cross=False, cmap='RdBu_r')
 
 ###############################################################################
 # RFX GLM with empirical null distribution
 # ------------------------------------------
-result = rfx_glm(con_data, mask_img, null='empirical', n_iters=1000)
-plot_stat_map(result.images['z'], cut_coords=[0, 0, -8],
+meta = RFX_GLM(null='empirical', n_iters=1000)
+meta.fit(dset)
+plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
               draw_cross=False, cmap='RdBu_r')
