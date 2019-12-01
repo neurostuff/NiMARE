0011cebb09ad53af990c618cc18c5b3007dbc3cf
Taylor Salo <tsalo006@fiu.edu>
2019-12-01 14:54:23 -0500

    Add IO tests and fix bug in convert_sleuth_to_dict.

--- nimare/io.py
+++ nimare/io.py
@@ -186,14 +186,14 @@ def convert_sleuth_to_dict(text_file):
                 dict_[study_name] = {'contrasts': {}}
             dict_[study_name]['contrasts'][contrast_name] = {
                 'coords': {},
-                'sample_sizes': [],
+                'metadata': {},
             }
             dict_[study_name]['contrasts'][contrast_name]['coords'][
                 'space'] = space
             dict_[study_name]['contrasts'][contrast_name]['coords']['x'] = x
             dict_[study_name]['contrasts'][contrast_name]['coords']['y'] = y
             dict_[study_name]['contrasts'][contrast_name]['coords']['z'] = z
-            dict_[study_name]['contrasts'][contrast_name][
+            dict_[study_name]['contrasts'][contrast_name]['metadata'][
                 'sample_sizes'] = [sample_size]
     return dict_
 
