0382b9a0c2ed025851d345745fd7b34b3b67e0cd
Taylor Salo <tsalo006@fiu.edu>
2019-12-03 18:09:24 -0500

    Further Corrector improvement.

--- nimare/correct/correct.py
+++ nimare/correct/correct.py
@@ -68,7 +68,7 @@ class FDRCorrector(Corrector):
     To determine what methods are available for the Estimator you're using,
     check the Estimator's documentation. Estimators have special methods
     following the naming convention correct_[correction-type]_[method]
-    (e.g., ALE.correct_fwe_permutation).
+    (e.g., MKDAChi2.correct_fdr_bh).
     """
 
     _correction_method = 'fdr'
