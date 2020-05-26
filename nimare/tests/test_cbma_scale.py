bf8afc00a643ee47524401b16c02e81520bdf9b8
Taylor Salo <tsalo006@fiu.edu>
2020-05-26 14:21:24 -0400

    Reduce scale memory.

--- nimare/tests/test_cbma_scale.py
+++ nimare/tests/test_cbma_scale.py
@@ -11,8 +11,9 @@ def test_scale(testdata):
     """
     Smoke test for SCALE
     """
+    dset = testdata['dset'].slice(testdata.ids[:3])
     ijk = np.vstack(np.where(testdata['dset'].masker.mask_img.get_fdata())).T
     ijk = ijk[:, :20]
     meta = ale.SCALE(n_iters=5, n_cores=1, ijk=ijk)
-    res = meta.fit(testdata['dset'])
+    res = meta.fit(dset)
     assert isinstance(res, nimare.base.MetaResult)
