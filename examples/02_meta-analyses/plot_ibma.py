19bb066300856452b59097a3d9d8af16841e1ba6
Taylor Salo <tsalo006@fiu.edu>
2020-07-10 19:56:39 -0400

    Expand IBMA example.

--- examples/02_meta-analyses/plot_ibma.py
+++ examples/02_meta-analyses/plot_ibma.py
@@ -25,7 +25,7 @@ from nilearn.plotting import plot_stat_map
 
 import nimare
 from nimare.tests.utils import get_test_data_path
-from nimare.meta.ibma import (Fishers, Stouffers, RandomEffectsGLM)
+from nimare.meta import ibma
 
 ###############################################################################
 # Download data
@@ -38,12 +38,21 @@ dset_dir = nimare.extract.download_nidm_pain()
 dset_file = os.path.join(get_test_data_path(), 'nidm_pain_dset.json')
 dset = nimare.dataset.Dataset(dset_file)
 dset.update_path(dset_dir)
+# Calculate missing images
+dset.images = nimare.transforms.transform_images(
+    dset.images, target='z',
+    masker=dset.masker, metadata_df=dset.metadata
+)
+dset.images = nimare.transforms.transform_images(
+    dset.images, target='varcope',
+    masker=dset.masker, metadata_df=dset.metadata
+)
 
 ###############################################################################
 # Fisher's (using Estimators)
 # --------------------------------------------------
 # Here is the object-oriented approach
-meta = Fishers()
+meta = ibma.Fishers()
 meta.fit(dset)
 plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
               draw_cross=False, cmap='RdBu_r')
@@ -51,7 +60,7 @@ plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
 ###############################################################################
 # Stouffer's
 # --------------------------------------------------
-meta = Stouffers(use_sample_size=False)
+meta = ibma.Stouffers(use_sample_size=False)
 meta.fit(dset)
 plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
               draw_cross=False, cmap='RdBu_r')
@@ -59,7 +68,7 @@ plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
 ###############################################################################
 # Stouffer's with weighting by sample size
 # -----------------------------------------------------------------------------
-meta = Stouffers(use_sample_size=True)
+meta = ibma.Stouffers(use_sample_size=True)
 meta.fit(dset)
 plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
               draw_cross=False, cmap='RdBu_r')
@@ -67,7 +76,7 @@ plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
 ###############################################################################
 # RFX GLM with theoretical null distribution
 # ------------------------------------------
-meta = RandomEffectsGLM(null='theoretical', n_iters=None)
+meta = ibma.RandomEffectsGLM(null='theoretical', n_iters=None)
 meta.fit(dset)
 plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
               draw_cross=False, cmap='RdBu_r')
@@ -75,7 +84,31 @@ plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
 ###############################################################################
 # RFX GLM with empirical null distribution
 # ------------------------------------------
-meta = RandomEffectsGLM(null='empirical', n_iters=100)
+meta = ibma.RandomEffectsGLM(null='empirical', n_iters=100)
+meta.fit(dset)
+plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
+              draw_cross=False, cmap='RdBu_r')
+
+###############################################################################
+# Weighted Least Squares
+# ------------------------------------------
+meta = ibma.WeightedLeastSquares(method='reml')
+meta.fit(dset)
+plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
+              draw_cross=False, cmap='RdBu_r')
+
+###############################################################################
+# DerSimonianLaird
+# ------------------------------------------
+meta = ibma.Something(method='DerSimonianLaird')
+meta.fit(dset)
+plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
+              draw_cross=False, cmap='RdBu_r')
+
+###############################################################################
+# Hedge's
+# ------------------------------------------
+meta = ibma.Something(method='Hedges')
 meta.fit(dset)
 plot_stat_map(meta.results.get_map('z'), cut_coords=[0, 0, -8],
               draw_cross=False, cmap='RdBu_r')
