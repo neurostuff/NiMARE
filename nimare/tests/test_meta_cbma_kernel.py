f174169167e4093f5bca751f67df0f9084bccab9
Taylor Salo <tsalo006@fiu.edu>
2020-07-12 13:20:17 -0400

    Bump up kernel coverage.

--- nimare/tests/test_meta_cbma_kernel.py
+++ nimare/tests/test_meta_cbma_kernel.py
@@ -34,7 +34,7 @@ def test_alekernel_smoke(testdata_cbma):
     assert ma_maps.shape[0] == len(testdata_cbma.ids)
 
 
-def test_alekernel1(testdata_cbma):
+def test_alekernel_1mm(testdata_cbma):
     """
     Peaks of ALE kernel maps should match the foci fed in (assuming focus isn't
     masked out).
@@ -60,7 +60,7 @@ def test_alekernel1(testdata_cbma):
     assert np.array_equal(ijk, max_ijk)
 
 
-def test_alekernel2(testdata_cbma):
+def test_alekernel_2mm(testdata_cbma):
     """
     Peaks of ALE kernel maps should match the foci fed in (assuming focus isn't
     masked out).
@@ -87,9 +87,82 @@ def test_alekernel2(testdata_cbma):
     assert np.array_equal(ijk, max_ijk)
 
 
+def test_alekernel_dataset(testdata_cbma):
+    """
+    Peaks of ALE kernel maps should match the foci fed in (assuming focus isn't
+    masked out).
+    Test on Dataset object.
+    """
+    # Manually override dataset coordinates file sample sizes
+    # This column would be extracted from metadata and added to coordinates
+    # automatically by the Estimator
+    coordinates = testdata_cbma.coordinates.copy()
+    coordinates['sample_size'] = 20
+    testdata_cbma.coordinates = coordinates
+
+    id_ = 'pain_01.nidm-1'
+    kern = kernel.ALEKernel()
+    ma_maps = kern.transform(
+        testdata_cbma
+    )
+
+    ijk = coordinates.loc[coordinates['id'] == id_, ['i', 'j', 'k']]
+    ijk = np.squeeze(ijk.values.astype(int))
+    kern_data = ma_maps[0].get_fdata()
+    max_idx = np.array(np.where(kern_data == np.max(kern_data))).T
+    max_ijk = np.squeeze(max_idx)
+    assert np.array_equal(ijk, max_ijk)
+
+
+def test_alekernel_fwhm(testdata_cbma):
+    """
+    Peaks of ALE kernel maps should match the foci fed in (assuming focus isn't
+    masked out).
+    Test with explicit FWHM.
+    """
+    coordinates = testdata_cbma.coordinates.copy()
+
+    id_ = 'pain_01.nidm-1'
+    kern = kernel.ALEKernel(fwhm=10)
+    ma_maps = kern.transform(
+        coordinates,
+        masker=testdata_cbma.masker
+    )
+
+    ijk = coordinates.loc[coordinates['id'] == id_, ['i', 'j', 'k']]
+    ijk = np.squeeze(ijk.values.astype(int))
+    kern_data = ma_maps[0].get_fdata()
+    max_idx = np.array(np.where(kern_data == np.max(kern_data))).T
+    max_ijk = np.squeeze(max_idx)
+    assert np.array_equal(ijk, max_ijk)
+
+
+def test_alekernel_sample_size(testdata_cbma):
+    """
+    Peaks of ALE kernel maps should match the foci fed in (assuming focus isn't
+    masked out).
+    Test with explicit sample size.
+    """
+    coordinates = testdata_cbma.coordinates.copy()
+
+    id_ = 'pain_01.nidm-1'
+    kern = kernel.ALEKernel(sample_size=20)
+    ma_maps = kern.transform(
+        coordinates,
+        masker=testdata_cbma.masker
+    )
+
+    ijk = coordinates.loc[coordinates['id'] == id_, ['i', 'j', 'k']]
+    ijk = np.squeeze(ijk.values.astype(int))
+    kern_data = ma_maps[0].get_fdata()
+    max_idx = np.array(np.where(kern_data == np.max(kern_data))).T
+    max_ijk = np.squeeze(max_idx)
+    assert np.array_equal(ijk, max_ijk)
+
+
 def test_mkdakernel_smoke(testdata_cbma):
     """
-    Smoke test for nimare.meta.cbma.kernel.MKDAKernel
+    Smoke test for nimare.meta.cbma.kernel.MKDAKernel, using Dataset object.
     """
     kern = kernel.MKDAKernel()
     ma_maps = kern.transform(testdata_cbma)
@@ -102,7 +175,7 @@ def test_mkdakernel_smoke(testdata_cbma):
     assert ma_maps.shape[0] == len(testdata_cbma.ids)
 
 
-def test_mkdakernel1(testdata_cbma):
+def test_mkdakernel_1mm(testdata_cbma):
     """
     COMs of MKDA kernel maps should match the foci fed in (assuming focus isn't
     masked out and spheres don't overlap).
@@ -121,7 +194,7 @@ def test_mkdakernel1(testdata_cbma):
     assert np.array_equal(ijk, com)
 
 
-def test_mkdakernel2(testdata_cbma):
+def test_mkdakernel_2mm(testdata_cbma):
     """
     COMs of MKDA kernel maps should match the foci fed in (assuming focus isn't
     masked out and spheres don't overlap).
@@ -158,7 +231,7 @@ def test_kdakernel_smoke(testdata_cbma):
     assert ma_maps.shape[0] == len(testdata_cbma.ids)
 
 
-def test_kdakernel1(testdata_cbma):
+def test_kdakernel_1mm(testdata_cbma):
     """
     COMs of KDA kernel maps should match the foci fed in (assuming focus isn't
     masked out and spheres don't overlap).
@@ -177,7 +250,7 @@ def test_kdakernel1(testdata_cbma):
     assert np.array_equal(ijk, com)
 
 
-def test_kdakernel2(testdata_cbma):
+def test_kdakernel_2mm(testdata_cbma):
     """
     COMs of KDA kernel maps should match the foci fed in (assuming focus isn't
     masked out and spheres don't overlap).
@@ -186,6 +259,25 @@ def test_kdakernel2(testdata_cbma):
     id_ = 'pain_01.nidm-1'
     kern = kernel.KDAKernel(r=4, value=1)
     ma_maps = kern.transform(testdata_cbma.coordinates, testdata_cbma.masker)
+
+    ijk = testdata_cbma.coordinates.loc[testdata_cbma.coordinates['id'] == id_,
+                                        ['i', 'j', 'k']]
+    ijk = np.squeeze(ijk.values.astype(int))
+    kern_data = ma_maps[0].get_fdata()
+    com = np.array(center_of_mass(kern_data)).astype(int).T
+    com = np.squeeze(com)
+    assert np.array_equal(ijk, com)
+
+
+def test_kdakernel_dataset(testdata_cbma):
+    """
+    COMs of KDA kernel maps should match the foci fed in (assuming focus isn't
+    masked out and spheres don't overlap).
+    Test on Dataset object.
+    """
+    id_ = 'pain_01.nidm-1'
+    kern = kernel.KDAKernel(r=4, value=1)
+    ma_maps = kern.transform(testdata_cbma)
 
     ijk = testdata_cbma.coordinates.loc[testdata_cbma.coordinates['id'] == id_,
                                         ['i', 'j', 'k']]
