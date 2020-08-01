abec7ddddc8a27ececb2a722e129a3e697d6eedf
Taylor Salo <tsalo006@fiu.edu>
2020-08-01 11:50:40 -0400

    Drop 3.5 support. Add 3.8.

--- nimare/info.py
+++ nimare/info.py
@@ -114,8 +114,8 @@ CLASSIFIERS = [
     'Intended Audience :: Science/Research',
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Topic :: Scientific/Engineering'
 ]
