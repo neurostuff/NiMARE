8d3290dd96ffe885d324da7cc88c3353c3e21fa1
Taylor Salo <tsalo006@fiu.edu>
2020-08-01 10:33:27 -0400

    Update info.py to trigger build.

--- nimare/info.py
+++ nimare/info.py
@@ -45,7 +45,9 @@ Copyright (c) 2018--, NiMARE developers
 
 DOWNLOAD_URL = (
     'https://github.com/neurostuff/{name}/archive/{ver}.tar.gz'.format(
-        name=PACKAGENAME, ver=VERSION))
+        name=PACKAGENAME, ver=VERSION
+    )
+)
 
 REQUIRES = [
     'cognitiveatlas',
