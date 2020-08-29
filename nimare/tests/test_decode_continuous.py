87d44a500e1384cb23c6103ec25318968bf968c7
Taylor Salo <tsalo006@fiu.edu>
2020-08-29 15:11:32 -0400

    Update tests.

--- nimare/tests/test_decode_continuous.py
+++ nimare/tests/test_decode_continuous.py
@@ -3,7 +3,11 @@ Test nimare.decode.discrete.
 
 Tests for nimare.decode.discrete.gclda_decode_roi are in test_annotate_gclda.
 """
+import shutil
+import tempfile
+
 import pandas as pd
+import pytest
 
 from nimare.decode import continuous
 from nimare.meta import mkda
@@ -13,8 +17,15 @@ def test_CorrelationDecoder(testdata_laird):
     """
     Smoke test for discrete.neurosynth_decode
     """
+    testdata_laird = testdata_laird.copy()
     features = testdata_laird.get_labels(ids=testdata_laird.ids[0])[:5]
     decoder = continuous.CorrelationDecoder(features=features)
+
+    with pytest.raises(ValueError):
+        decoder.fit(testdata_laird)
+
+    temp_dir = tempfile.mkdtemp()
+    testdata_laird.update_path(temp_dir)
     decoder.fit(testdata_laird)
 
     meta = mkda.KDA()
@@ -22,3 +33,4 @@ def test_CorrelationDecoder(testdata_laird):
     img = res.get_map("of")
     decoded_df = decoder.transform(img)
     assert isinstance(decoded_df, pd.DataFrame)
+    shutil.rmtree(temp_dir)
