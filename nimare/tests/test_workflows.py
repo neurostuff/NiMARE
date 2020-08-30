a5b3a3263db3208817ae7e6af8912c16a7928a21
Taylor Salo <tsalo006@fiu.edu>
2020-08-29 22:01:37 -0400

    Use temporary directories when possible.

--- nimare/tests/test_workflows.py
+++ nimare/tests/test_workflows.py
@@ -1,144 +1,170 @@
 """
 Test nimare.workflows.
 """
-import os
 import os.path as op
-import shutil
 
 from nimare import cli, workflows
 from nimare.tests.utils import get_test_data_path
 
 
-def test_ale_workflow_smoke():
+def test_ale_workflow_function_smoke(tmp_path_factory):
     """
     Smoke test for Sleuth ALE workflow.
     """
+    tmpdir = tmp_path_factory.mktemp("test_ale_workflow_function_smoke")
     sleuth_file = op.join(get_test_data_path(), "test_sleuth_file.txt")
-    out_dir = op.join(os.getcwd(), "TEST")
     prefix = "test"
 
     # The same test is run with both workflow function and CLI
     workflows.ale_sleuth_workflow(
-        sleuth_file, output_dir=out_dir, prefix=prefix, n_iters=10, n_cores=1
+        sleuth_file, output_dir=tmpdir, prefix=prefix, n_iters=10, n_cores=1
     )
-    assert op.isfile(op.join(out_dir, "{}_input_coordinates.txt".format(prefix)))
-    shutil.rmtree(out_dir)
-
-    args = [
-        "ale",
-        "--output_dir",
-        out_dir,
-        "--prefix",
-        prefix,
-        "--n_iters",
-        "10",
-        "--n_cores",
-        "1",
-        sleuth_file,
-    ]
-    cli._main(args)
-    assert op.isfile(op.join(out_dir, "{}_input_coordinates.txt".format(prefix)))
-    shutil.rmtree(out_dir)
+    assert op.isfile(op.join(tmpdir, "{}_input_coordinates.txt".format(prefix)))
 
 
-def test_ale_workflow_smoke_2():
+def test_ale_workflow_cli_smoke(tmp_path_factory):
+    """
+    Smoke test for Sleuth ALE workflow.
+    """
+    tmpdir = tmp_path_factory.mktemp("test_ale_workflow_cli_smoke")
+    sleuth_file = op.join(get_test_data_path(), "test_sleuth_file.txt")
+    prefix = "test"
+
+    cli._main(
+        [
+            "ale",
+            "--output_dir",
+            str(tmpdir),
+            "--prefix",
+            prefix,
+            "--n_iters",
+            "10",
+            "--n_cores",
+            "1",
+            sleuth_file,
+        ]
+    )
+    assert op.isfile(op.join(tmpdir, "{}_input_coordinates.txt".format(prefix)))
+
+
+def test_ale_workflow_function_smoke_2(tmp_path_factory):
     """
     Smoke test for Sleuth ALE workflow with subtraction analysis
     """
+    tmpdir = tmp_path_factory.mktemp("test_ale_workflow_function_smoke_2")
     sleuth_file = op.join(get_test_data_path(), "test_sleuth_file.txt")
-    out_dir = op.join(os.getcwd(), "TEST")
     prefix = "test"
 
     # The same test is run with both workflow function and CLI
     workflows.ale_sleuth_workflow(
         sleuth_file,
         sleuth_file2=sleuth_file,
-        output_dir=out_dir,
+        output_dir=tmpdir,
         prefix=prefix,
         n_iters=10,
         n_cores=1,
     )
-    assert op.isfile(op.join(out_dir, "{}_group2_input_coordinates.txt".format(prefix)))
-    shutil.rmtree(out_dir)
-
-    args = [
-        "ale",
-        "--output_dir",
-        out_dir,
-        "--prefix",
-        prefix,
-        "--n_iters",
-        "10",
-        "--n_cores",
-        "1",
-        "--file2",
-        sleuth_file,
-        sleuth_file,
-    ]
-    cli._main(args)
-    assert op.isfile(op.join(out_dir, "{}_group2_input_coordinates.txt".format(prefix)))
-    shutil.rmtree(out_dir)
+    assert op.isfile(op.join(tmpdir, "{}_group2_input_coordinates.txt".format(prefix)))
 
 
-def test_scale_workflow_smoke():
+def test_ale_workflow_cli_smoke_2(tmp_path_factory):
     """
+    Smoke test for Sleuth ALE workflow with subtraction analysis
     """
+    tmpdir = tmp_path_factory.mktemp("test_ale_workflow_cli_smoke_2")
     sleuth_file = op.join(get_test_data_path(), "test_sleuth_file.txt")
-    out_dir = op.join(os.getcwd(), "TEST")
+    prefix = "test"
+    cli._main(
+        [
+            "ale",
+            "--output_dir",
+            str(tmpdir),
+            "--prefix",
+            prefix,
+            "--n_iters",
+            "10",
+            "--n_cores",
+            "1",
+            "--file2",
+            sleuth_file,
+            sleuth_file,
+        ]
+    )
+    assert op.isfile(op.join(tmpdir, "{}_group2_input_coordinates.txt".format(prefix)))
+
+
+def test_scale_workflow_function_smoke(tmp_path_factory):
+    """
+    """
+    tmpdir = tmp_path_factory.mktemp("test_scale_workflow_function_smoke")
+    sleuth_file = op.join(get_test_data_path(), "test_sleuth_file.txt")
     prefix = "test"
     baseline = op.join(get_test_data_path(), "test_baseline.txt")
 
     # The same test is run with both workflow function and CLI
     workflows.scale_workflow(
-        sleuth_file, baseline=baseline, output_dir=out_dir, prefix=prefix, n_iters=5, n_cores=1
+        sleuth_file, baseline=baseline, output_dir=tmpdir, prefix=prefix, n_iters=5, n_cores=1
     )
-    assert op.isfile(op.join(out_dir, "{}_input_coordinates.txt".format(prefix)))
-    shutil.rmtree(out_dir)
-
-    args = [
-        "scale",
-        "--baseline",
-        baseline,
-        "--output_dir",
-        out_dir,
-        "--prefix",
-        prefix,
-        "--n_iters",
-        "5",
-        "--n_cores",
-        "1",
-        sleuth_file,
-    ]
-    cli._main(args)
-    assert op.isfile(op.join(out_dir, "{}_input_coordinates.txt".format(prefix)))
-    shutil.rmtree(out_dir)
+    assert op.isfile(op.join(tmpdir, "{}_input_coordinates.txt".format(prefix)))
 
 
-def test_conperm_workflow_smoke(testdata_ibma):
+def test_scale_workflow_cli_smoke(tmp_path_factory):
+    tmpdir = tmp_path_factory.mktemp("test_scale_workflow_cli_smoke")
+    sleuth_file = op.join(get_test_data_path(), "test_sleuth_file.txt")
+    prefix = "test"
+    baseline = op.join(get_test_data_path(), "test_baseline.txt")
+
+    cli._main(
+        [
+            "scale",
+            "--baseline",
+            baseline,
+            "--output_dir",
+            str(tmpdir),
+            "--prefix",
+            prefix,
+            "--n_iters",
+            "5",
+            "--n_cores",
+            "1",
+            sleuth_file,
+        ]
+    )
+    assert op.isfile(op.join(tmpdir, "{}_input_coordinates.txt".format(prefix)))
+
+
+def test_conperm_workflow_function_smoke(testdata_ibma, tmp_path_factory):
+    tmpdir = tmp_path_factory.mktemp("test_conperm_workflow_function_smoke")
     dset = testdata_ibma
     files = dset.get_images(imtype="beta")
     mask_image = op.join(get_test_data_path(), "test_pain_dataset", "mask.nii.gz")
-    out_dir = op.join(os.getcwd(), "TEST")
     prefix = "test"
 
     # The same test is run with both workflow function and CLI
     workflows.conperm_workflow(
-        files, mask_image=mask_image, output_dir=out_dir, prefix=prefix, n_iters=5
+        files, mask_image=mask_image, output_dir=tmpdir, prefix=prefix, n_iters=5
     )
-    assert op.isfile(op.join(out_dir, "{}_logp.nii.gz".format(prefix)))
-    shutil.rmtree(out_dir)
+    assert op.isfile(op.join(tmpdir, "{}_logp.nii.gz".format(prefix)))
 
-    args = [
-        "conperm",
-        "--output_dir",
-        out_dir,
-        "--mask",
-        mask_image,
-        "--prefix",
-        prefix,
-        "--n_iters",
-        "5",
-    ] + files
-    cli._main(args)
-    assert op.isfile(op.join(out_dir, "{}_logp.nii.gz".format(prefix)))
-    shutil.rmtree(out_dir)
+
+def test_conperm_workflow_cli_smoke(testdata_ibma, tmp_path_factory):
+    tmpdir = tmp_path_factory.mktemp("test_conperm_workflow_cli_smoke")
+    dset = testdata_ibma
+    files = dset.get_images(imtype="beta")
+    mask_image = op.join(get_test_data_path(), "test_pain_dataset", "mask.nii.gz")
+    prefix = "test"
+
+    cli._main(
+        [
+            "conperm",
+            "--output_dir",
+            str(tmpdir),
+            "--mask",
+            mask_image,
+            "--prefix",
+            prefix,
+            "--n_iters",
+            "5",
+        ] + files
+    )
+    assert op.isfile(op.join(tmpdir, "{}_logp.nii.gz".format(prefix)))
