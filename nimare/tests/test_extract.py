da8a116ff73601b2dad02cbf5a61400039973f39
Taylor Salo <tsalo006@fiu.edu>
2020-09-15 12:23:03 -0400

    Rename test function to match convention.

--- nimare/tests/test_extract.py
+++ nimare/tests/test_extract.py
@@ -7,12 +7,12 @@ from glob import glob
 import nimare
 
 
-def test_extract_fetch_neurosynth(tmp_path_factory):
+def test_fetch_neurosynth(tmp_path_factory):
     """
     Smoke test for extract.fetch_neurosynth.
     Taken from the Neurosynth Python package.
     """
-    tmpdir = tmp_path_factory.mktemp("test_extract_fetch_neurosynth")
+    tmpdir = tmp_path_factory.mktemp("test_fetch_neurosynth")
     url = (
         "https://raw.githubusercontent.com/neurosynth/neurosynth/master/neurosynth/tests/"
         "data/test_data.tar.gz"
