9b66c449ef225fdb64ec16e9276c7b85783992c5
James Kent <jamesdkent21@gmail.com>
2020-09-22 17:06:47 -0500

    add doc strings and minor function rewrite

--- nimare/generate.py
+++ nimare/generate.py
@@ -11,43 +11,85 @@ from .meta.utils import compute_ma, get_ale_kernel
 from .transforms import vox2mm
 
 
-def create_coordinate_dataset(foci, studies, sample_size_mean,
-                              sample_size_variance, foci_variance=None, rng=None):
-    """Create a Dataset object with simulated data"""
+def create_coordinate_dataset(foci, fwhm, sample_size_mean,
+                              sample_size_variance, studies, rng=None):
+    """Generate coordinate based dataset for meta analysis.
+ 
+    Parameters
+    ----------
+    foci : :obj:`int` or :obj:`list` of three item array_like objects  
+        Either the number of foci to be generated or a list of foci in ijk
+        coordinates.
+    fwhm : :obj:`float`
+        Full width at half maximum to define the probability
+        spread of the foci.
+    sample_size_mean : :obj:`int`
+        Mean number of participants in each study.
+    sample_size_variance : :obj:`int`
+        Variance of the number of participants in each study.
+        (must be >=0).
+    studies : :obj:`int`
+        Number of studies to generate.
+    rng : :class:`numpy.random.RandomState` (Optional)
+        Random state to reproducibly initialize random numbers.
+
+    Returns
+    -------
+    ground_truth_foci : :obj:`list`
+        generated foci in xyz (mm) coordinates
+    dataset : :class:`nimare.Dataset`
+    """
     if rng is None:
         rng = np.random.RandomState(seed=1939)
-    # Dataset()
     sample_size_lower_limit = int(sample_size_mean - sample_size_variance)
-    sample_size_upper_limit = int(sample_size_mean + sample_size_variance)
+    # add 1 since randint upper limit is a closed interval
+    sample_size_upper_limit = int(sample_size_mean + sample_size_variance + 1)
     sample_sizes = rng.randint(sample_size_lower_limit, sample_size_upper_limit, size=studies)
-    foci_dict = create_foci(foci, studies, sample_sizes=sample_sizes, rng=rng)
+    foci_dict = create_foci(foci, studies, fwhm=fwhm, rng=rng)
+    # re-arrange foci_dict into a list of dictionaries which represent individual studies
     study_foci = [
         {a: [c[s][i] for c in foci_dict.values()] for i, a in enumerate(["x", "y", "z"])}
         for s in range(studies)
     ]
 
     source_dict = create_source(study_foci, sample_sizes)
-    
     dataset = Dataset(source_dict)
     ground_truth_foci = list(foci_dict.keys())
-    # [(-10.0, 54.0, -42.0), (52.0, -20.0, -60.0)]
+
     return ground_truth_foci, dataset
 
 
 def create_source(foci, sample_sizes, space="MNI"):
-    """create dictionary with nimads specification"""
+    """Create dictionary according to nimads(ish) specification
+
+    Parameters
+    ----------
+    foci : :obj:`list` of three item array_like objects  
+        A list of foci in xyz (mm) coordinates
+    sample_sizes : :obj:`list`
+        The sample size for each study
+    space : :obj:`str` (Default="MNI")
+        The template space the coordinates are reported in
+
+    Returns
+    -------
+    source : :obj:`dict`
+        study information in nimads format 
+    """
     source = {}
     for study_idx, (sample_size, study_foci)  in enumerate(zip(sample_sizes, foci)):
-        source[f'study-{study_idx}'] = {
+        source[f"study-{study_idx}"] = {
             "contrasts": {
                 "1": {
-                    'coords': {
-                        'space': "MNI",
-                        'x': study_foci['x'],
-                        'y': study_foci['y'],
-                        'z': study_foci['z'],
+                    "coords": {
+                        "space": space,
+                        "x": study_foci["x"],
+                        "y": study_foci["y"],
+                        "z": study_foci["z"],
                     },
-                    'sample_sizes': [sample_size],
+                    "metadata": {
+                        "sample_sizes": [sample_size],
+                    }
                 }
             }
         }
@@ -55,45 +97,74 @@ def create_source(foci, sample_sizes, space="MNI"):
     return source
 
 
-def create_foci(foci, studies, space="MNI", rng=None, sample_sizes=None, fwhm=None):
+def create_foci(foci, studies, fwhm, rng=None, space="MNI"):
+    """Generate study specific foci.
+
+    Parameters
+    ----------
+    foci : :obj:`int` or :obj:`list` of three item array_like objects  
+        Either the number of foci to be generated or a list of foci in ijk
+        coordinates.
+    studies : :obj:`int`
+        Number of studies to generate.
+    fwhm : :obj:`float`
+        Full width at half maximum to define the probability
+        spread of the foci.
+    rng : :class:`numpy.random.RandomState` (Optional)
+        Random state to reproducibly initialize random numbers.
+    space : :obj:`str` (Default="MNI")
+        The template space the coordinates are reported in.
+
+    Returns
+    -------
+    foci_dict : :obj:`dict`
+        Dictionary with keys representing the ground truth foci, and
+        whose values represent the study specific foci.
+    """
     if rng is None:
         rng = np.random.RandomState(seed=1939)
 
     if space == "MNI":
         template_img = nib.load(
-            os.path.join(get_resource_path(), 'templates', 'MNI152_2x2x2_brainmask.nii.gz')
+            os.path.join(get_resource_path(), "templates", "MNI152_2x2x2_brainmask.nii.gz")
         )
     else:
         raise NotImplementedError("Only coordinates for the MNI atlas has been defined")
 
-    if (sample_sizes is None and fwhm is None) or (sample_sizes is not None and fwhm is not None):
-        raise ValueError("either sample_sizes or fwhm must be set (mutually exclusive)")
-
+    # use a template to find all "valid" coordinates
     template_data = template_img.get_data()
     possible_i, possible_j, possible_k = np.nonzero(template_data)
     max_i = possible_i.max()
     max_j = possible_j.max()
     max_k = possible_k.max()
+    # foci were specified by the caller
     if isinstance(foci, np.ndarray):
         foci_n = foci.shape[0]
         ground_truth_foci = foci
+    # foci are to be generated randomly
     elif isinstance(foci, int):
         foci_n = foci
-        ground_truth_i = rng.choice(possible_i, foci_n)
-        ground_truth_j = rng.choice(possible_j, foci_n)
-        ground_truth_k = rng.choice(possible_k, foci_n)
+        ground_truth_i = rng.choice(possible_i, foci_n, replace=False)
+        ground_truth_j = rng.choice(possible_j, foci_n, replace=False)
+        ground_truth_k = rng.choice(possible_k, foci_n, replace=False)
         ground_truth_foci = np.vstack([ground_truth_i, ground_truth_j, ground_truth_k]).T
 
-    if isinstance(sample_sizes, int):
-        sample_sizes = [sample_sizes] * foci_n 
-    elif isinstance(sample_sizes, list):
-        if sample_sizes != foci_n:
-            raise ValueError(("sample_sizes must be a list the same size as the"
-                             " number of foci or an integer"))
+    if isinstance(fwhm, float):
+        fwhms = [fwhm] * foci_n 
+    elif isinstance(fwhm, list):
+        fwhms = fwhm
+        if len(fwhms) != foci_n:
+            raise ValueError(("fwhm must be a list the same size as the"
+                             " number of foci or a float"))
+    else:
+        try:
+            fwhms = [float(fwhm)] * foci_n
+        except TypeError as e:
+            raise TypeError("fwhm must be a float or a list")
 
     foci_dict = {}
-    for ground_truth_focus, sample_size in zip(ground_truth_foci, sample_sizes):
-        _, kernel = get_ale_kernel(template_img, sample_size=sample_size, fwhm=fwhm)
+    for ground_truth_focus, fwhm in zip(ground_truth_foci, fwhms):
+        _, kernel = get_ale_kernel(template_img, fwhm=fwhm)
         template_data_dilated = tuple([s + kernel.shape[0] for s in template_data.shape])
         prob_map = compute_ma(template_data_dilated, np.atleast_2d(ground_truth_focus), kernel)
         prob_map_ijk = np.argwhere(prob_map)
