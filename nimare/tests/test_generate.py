2a39392d615717cb15af48b6a8d38a6f54efb77b
James Kent <jamesdkent21@gmail.com>
2020-09-22 21:17:27 -0500

    clean up generate tests

--- nimare/tests/test_generate.py
+++ nimare/tests/test_generate.py
@@ -1,18 +1,26 @@
 import numpy as np
+import pytest
+
 from ..generate import create_coordinate_dataset, create_source, create_foci
 
 
-def test_create_foci():
-    foci_dict = create_foci(2, 5, fwhm=10., rng=np.random.RandomState(seed=1939))
-    list(foci_dict.keys())
+@pytest.mark.parametrize(
+    "foci,studies,fwhm",
+    [
+        (2, 5, 10.),
+    ]
+)
+def test_create_foci(foci, studies, fwhm):
+    assert create_foci(foci, studies, fwhm, rng=np.random.RandomState(seed=1939))
 
 
 def test_create_source():
-    create_source(foci=[(0, 0, 0)], sample_sizes=[25])
+    source_dict = create_source(foci=[{'x': [0], 'y': [0], 'z': [0]}], sample_sizes=[25])
+    assert source_dict['study-0']['contrasts']['1']['metadata']['sample_sizes'] == [25]
 
 
 def test_create_coordinate_dataset():
-    create_coordinate_dataset(
+    assert create_coordinate_dataset(
         foci=2,
         fwhm=10.,
         sample_size_mean=30,
