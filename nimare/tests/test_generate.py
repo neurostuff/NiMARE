51dd3648955b536173e44f5190e12440395db9fc
James Kent <jamesdkent21@gmail.com>
2020-09-28 10:49:53 -0500

    test edge cases and test outputs explicitly

--- nimare/tests/test_generate.py
+++ nimare/tests/test_generate.py
@@ -1,17 +1,27 @@
 import numpy as np
 import pytest
+from contextlib import ExitStack as does_not_raise
 
 from ..generate import create_coordinate_dataset, create_source, create_foci
+from ..dataset import Dataset
 
 
 @pytest.mark.parametrize(
-    "foci,studies,fwhm",
+    "foci,studies,fwhm,rng,space,expectation",
     [
-        (2, 5, 10.0),
+        (2, 5, 10.0, np.random.RandomState(seed=1939), "MNI", does_not_raise()),
+        (np.array([[44, 61, 70]]), 5, 10.0, None, "MNI", does_not_raise()),
+        (2, 5, [10.0, 6.0], None, "MNI", does_not_raise()),
+        (2, 5, 10.0, None, "INVALID_SPACE", pytest.raises(NotImplementedError)),
+        (2, 5, "INVALID_FWHM", None, "MNI", pytest.raises(ValueError)),
+        (2, 5, [10.0], None, "MNI", pytest.raises(ValueError)),
     ],
 )
-def test_create_foci(foci, studies, fwhm):
-    assert create_foci(foci, studies, fwhm, rng=np.random.RandomState(seed=1939))
+def test_create_foci(foci, studies, fwhm, rng, space, expectation):
+    with expectation:
+        foci = create_foci(foci, studies, fwhm, rng, space)
+    if isinstance(expectation, does_not_raise):
+        assert all(isinstance(key, tuple) for key in foci)
 
 
 def test_create_source():
@@ -20,11 +30,14 @@ def test_create_source():
 
 
 def test_create_coordinate_dataset():
-    assert create_coordinate_dataset(
-        foci=2,
+    n_foci = 2
+    ground_truth_foci, dataset = create_coordinate_dataset(
+        foci=n_foci,
         fwhm=10.0,
         sample_size_mean=30,
         sample_size_variance=10,
         studies=5,
         rng=np.random.RandomState(seed=1939),
     )
+    assert len(ground_truth_foci) == n_foci
+    assert isinstance(dataset, Dataset)
