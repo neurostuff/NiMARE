7e6caccf19901bac947e5bb5969669d41a6a982c
James Kent <jamesdkent21@gmail.com>
2020-09-28 22:42:56 -0500

    refactor tests for readability

--- nimare/tests/test_generate.py
+++ nimare/tests/test_generate.py
@@ -7,19 +7,153 @@ from ..dataset import Dataset
 
 
 @pytest.mark.parametrize(
-    "foci,studies,fwhm,rng,space,expectation",
+    "kwargs,expectation",
     [
-        (2, 5, 10.0, np.random.RandomState(seed=1939), "MNI", does_not_raise()),
-        (np.array([[44, 61, 70]]), 5, 10.0, None, "MNI", does_not_raise()),
-        (2, 5, [10.0, 6.0], None, "MNI", does_not_raise()),
-        (2, 5, 10.0, None, "INVALID_SPACE", pytest.raises(NotImplementedError)),
-        (2, 5, "INVALID_FWHM", None, "MNI", pytest.raises(ValueError)),
-        (2, 5, [10.0], None, "MNI", pytest.raises(ValueError)),
+        pytest.param(
+            {
+                "foci_num": 2,
+                "fwhm": 10.0,
+                "studies": 5,
+                "foci_coords": None,
+                "foci_noise": 0,
+                "foci_weights": [1, 0.1],
+                "rng": np.random.RandomState(seed=1939),
+                "space": "MNI",
+            },
+            does_not_raise(),
+            id="test_foci_weights",
+        ),
+        pytest.param(
+            {
+                "foci_num": 2,
+                "fwhm": 10.0,
+                "studies": 5,
+                "foci_coords": [(0, 0, 0)],
+                "foci_noise": 2,
+                "foci_weights": None,
+                "rng": None,
+                "space": "MNI",
+            },
+            does_not_raise(),
+            id="test_input_foci",
+        ),
+        pytest.param(
+            {
+                "foci_num": 2,
+                "fwhm": [10.0, 6.0],
+                "studies": 5,
+                "foci_coords": None,
+                "foci_noise": 0,
+                "foci_weights": None,
+                "rng": None,
+                "space": "MNI",
+            },
+            does_not_raise(),
+            id="test_list_of_fwhm",
+        ),
+        pytest.param(
+            {
+                "foci_num": 2,
+                "fwhm": 10.0,
+                "studies": 5,
+                "foci_coords": None,
+                "foci_noise": 0,
+                "foci_weights": None,
+                "rng": None,
+                "space": "INVALID_SPACE",
+            },
+            pytest.raises(NotImplementedError),
+            id="test_invalid_space",
+        ),
+        pytest.param(
+            {
+                "foci_num": 2,
+                "fwhm": "INVALID_FWHM",
+                "studies": 5,
+                "foci_coords": None,
+                "foci_noise": 0,
+                "foci_weights": None,
+                "rng": None,
+                "space": "MNI",
+            },
+            pytest.raises(ValueError),
+            id="test_invalid_fwhm",
+        ),
+        pytest.param(
+            {
+                "foci_num": 2,
+                "fwhm": [10.0],
+                "studies": 5,
+                "foci_coords": None,
+                "foci_noise": 0,
+                "foci_weights": None,
+                "rng": None,
+                "space": "MNI",
+            },
+            pytest.raises(ValueError),
+            id="test_invalid_list_of_fwhm",
+        ),
+        pytest.param(
+            {
+                "foci_num": 2,
+                "fwhm": 10.0,
+                "studies": 5,
+                "foci_coords": None,
+                "foci_noise": 0,
+                "foci_weights": "INVALID_FOCI_WEIGHTS",
+                "rng": None,
+                "space": "MNI",
+            },
+            pytest.raises(ValueError),
+            id="test_invalid_foci_weights",
+        ),
+        pytest.param(
+            {
+                "foci_num": 2,
+                "fwhm": 10.0,
+                "studies": 5,
+                "foci_coords": None,
+                "foci_noise": 0,
+                "foci_weights": [0.1],
+                "rng": None,
+                "space": "MNI",
+            },
+            pytest.raises(ValueError),
+            id="test_wrong_length_foci_weights",
+        ),
+        pytest.param(
+            {
+                "foci_num": [10, 1, 5],
+                "fwhm": [8.0],
+                "studies": 3,
+                "foci_coords": [(0, 0, 0)],
+                "foci_noise": [10, 20, 30],
+                "foci_weights": [0.1],
+                "rng": None,
+                "space": "MNI",
+            },
+            does_not_raise(),
+            id="test_all_manual_specifications",
+        ),
+        pytest.param(
+            {
+                "foci_num": [1, 2, 2],
+                "fwhm": [8.0, 10.0],
+                "studies": 3,
+                "foci_coords": None,
+                "foci_noise": [10, 20, 30],
+                "foci_weights": [0.1, 1],
+                "rng": None,
+                "space": "MNI",
+            },
+            does_not_raise(),
+            id="test_foci_num_list_but_no_coords",
+        ),
     ],
 )
-def test_create_foci(foci, studies, fwhm, rng, space, expectation):
+def test_create_foci(kwargs, expectation):
     with expectation:
-        ground_truth_foci, foci_dict = create_foci(foci, studies, fwhm, rng=rng, space=space)
+        ground_truth_foci, foci_dict = create_foci(**kwargs)
     if isinstance(expectation, does_not_raise):
         assert all(isinstance(key, int) for key in foci_dict)
         assert all(isinstance(coord, tuple) for coord in ground_truth_foci)
@@ -33,11 +167,14 @@ def test_create_source():
 def test_create_coordinate_dataset():
     n_foci = 2
     ground_truth_foci, dataset = create_coordinate_dataset(
-        foci=n_foci,
+        foci_num=n_foci,
         fwhm=10.0,
-        sample_size_mean=30,
+        sample_size=30,
         sample_size_variance=10,
         studies=5,
+        foci_coords=None,
+        foci_noise=0,
+        foci_weights=None,
         rng=np.random.RandomState(seed=1939),
     )
     assert len(ground_truth_foci) == n_foci
