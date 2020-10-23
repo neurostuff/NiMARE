f80195403c0cfb8f28d8b8d6aa76d9c11f674ab2
Kendra Oudyk <kendra.oudyk@gmail.com>
2020-10-23 19:44:22 -0400

    [FIX] Add try/except statements for combining kernels and estimators

--- examples/02_meta-analyses/plot_cbma_combine_estimators_and_kernels.py
+++ examples/02_meta-analyses/plot_cbma_combine_estimators_and_kernels.py
@@ -25,6 +25,8 @@ from nilearn.plotting import plot_stat_map
 import nimare
 from nimare.tests.utils import get_test_data_path
 
+import matplotlib.pyplot as plt
+
 ###############################################################################
 # Load Dataset
 # --------------------------------------------------
@@ -38,86 +40,117 @@ mask_img = dset.masker.mask_img
 # List possible kernel transformers
 # --------------------------------------------------
 kernel_transformers = {
-    "MKDAKernel": nimare.meta.kernel.MKDAKernel,
-    "KDAKernel": nimare.meta.kernel.KDAKernel,
-    "ALEKernel": nimare.meta.kernel.ALEKernel,
+    "MKDA kernel": nimare.meta.kernel.MKDAKernel,
+    "KDA kernel": nimare.meta.kernel.KDAKernel,
+    "ALE kernel": nimare.meta.kernel.ALEKernel,
 }
 
 
 ###############################################################################
 # MKDA density analysis
 # --------------------------------------------------
-
 for kt_name, kt in kernel_transformers.items():
-    mkda = nimare.meta.mkda.MKDADensity(kernel_transformer=kt)
-    mkda.fit(dset)
-    corr = nimare.correct.FWECorrector(
-        method="montecarlo", n_iters=10, n_cores=1
-    )
-    cres = corr.transform(mkda.results)
-    plot_stat_map(
-        cres.get_map("logp_level-voxel_corr-FWE_method-montecarlo"),
-        cut_coords=[0, 0, -8],
-        draw_cross=False,
-        cmap="RdBu_r",
-        title="MKDA estimator with %s" % kt_name,
-    )
+    try:
+        mkda = nimare.meta.mkda.MKDADensity(kernel_transformer=kt)
+        mkda.fit(dset)
+        corr = nimare.correct.FWECorrector(
+            method="montecarlo", n_iters=10, n_cores=1
+        )
+        cres = corr.transform(mkda.results)
+        plot_stat_map(
+            cres.get_map("logp_level-voxel_corr-FWE_method-montecarlo"),
+            cut_coords=[0, 0, -8],
+            draw_cross=False,
+            cmap="RdBu_r",
+            title="MKDA estimator with %s" % kt_name,
+        )
+
+    except IndexError:
+        print(
+            "\nError: the %s does not currently work with the MKDA meta-analysis method\n"
+            % kt_name
+        )
 
 ###############################################################################
 # MKDA Chi2
 # --------------------------------------------------
 for kt_name, kt in kernel_transformers.items():
-    mkda = nimare.meta.mkda.MKDAChi2(kernel_transformer=kt)
-    dset1 = dset.slice(dset.ids)
-    dset2 = dset.slice(dset.ids)
-    mkda.fit(dset1, dset2)
-    corr = nimare.correct.FWECorrector(
-        method="montecarlo", n_iters=10, n_cores=1
-    )
-    cres = corr.transform(mkda.results)
-    plot_stat_map(
-        cres.get_map(
-            "z_desc-consistency_level-voxel_corr-FWE_method-montecarlo"
-        ),
-        threshold=1.65,
-        cut_coords=[0, 0, -8],
-        draw_cross=False,
-        cmap="RdBu_r",
-        title="MKDA Chi2 estimator with %s" % kt_name,
-    )
+    try:
+        mkda = nimare.meta.mkda.MKDAChi2(kernel_transformer=kt)
+        dset1 = dset.slice(dset.ids)
+        dset2 = dset.slice(dset.ids)
+        mkda.fit(dset1, dset2)
+        corr = nimare.correct.FWECorrector(
+            method="montecarlo", n_iters=10, n_cores=1
+        )
+        cres = corr.transform(mkda.results)
+        plot_stat_map(
+            cres.get_map(
+                "z_desc-consistency_level-voxel_corr-FWE_method-montecarlo"
+            ),
+            threshold=1.65,
+            cut_coords=[0, 0, -8],
+            draw_cross=False,
+            cmap="RdBu_r",
+            title="MKDA Chi2 estimator with %s" % kt_name,
+        )
+
+    except IndexError:
+        print(
+            "\nError: the %s does not currently work with the MKDA Chi2 meta-analysis method\n"
+            % kt_name
+        )
 
 ###############################################################################
 # KDA
 # --------------------------------------------------
 for kt_name, kt in kernel_transformers.items():
-    kda = nimare.meta.mkda.KDA(kernel_transformer=kt)
-    kda.fit(dset)
-    corr = nimare.correct.FWECorrector(
-        method="montecarlo", n_iters=10, n_cores=1
-    )
-    cres = corr.transform(kda.results)
-    plot_stat_map(
-        cres.get_map("logp_level-voxel_corr-FWE_method-montecarlo"),
-        cut_coords=[0, 0, -8],
-        draw_cross=False,
-        cmap="RdBu_r",
-        title="KDA estimator with %s" % kt_name,
-    )
+    try:
+        kda = nimare.meta.mkda.KDA(kernel_transformer=kt)
+        kda.fit(dset)
+        corr = nimare.correct.FWECorrector(
+            method="montecarlo", n_iters=10, n_cores=1
+        )
+        cres = corr.transform(kda.results)
+        plot_stat_map(
+            cres.get_map("logp_level-voxel_corr-FWE_method-montecarlo"),
+            cut_coords=[0, 0, -8],
+            draw_cross=False,
+            cmap="RdBu_r",
+            title="KDA estimator with %s" % kt_name,
+        )
+
+    except IndexError:
+        print(
+            "\nError: the %s does not currently work with the KDA meta-analysis method\n"
+            % kt_name
+        )
 
 ###############################################################################
 # ALE
 # --------------------------------------------------
+
+
 for kt_name, kt in kernel_transformers.items():
-    ale = nimare.meta.ale.ALE()
-    ale.fit(dset)
-    corr = nimare.correct.FWECorrector(
-        method="montecarlo", n_iters=10, n_cores=1
-    )
-    cres = corr.transform(ale.results)
-    plot_stat_map(
-        cres.get_map("logp_level-cluster_corr-FWE_method-montecarlo"),
-        cut_coords=[0, 0, -8],
-        draw_cross=False,
-        cmap="RdBu_r",
-        title="ALE estimator with %s" % kt_name,
-    )
+    try:
+        ale = nimare.meta.ale.ALE(kernel_transformer=kt)
+        ale.fit(dset)
+        corr = nimare.correct.FWECorrector(
+            method="montecarlo", n_iters=10, n_cores=1
+        )
+        cres = corr.transform(ale.results)
+        plot_stat_map(
+            cres.get_map("logp_level-cluster_corr-FWE_method-montecarlo"),
+            cut_coords=[0, 0, -8],
+            draw_cross=False,
+            cmap="RdBu_r",
+            title="ALE estimator with %s" % kt_name,
+        )
+
+    except IndexError:
+        print(
+            "\nError: the %s does not currently work with the ALE meta-analysis method\n"
+            % kt_name
+        )
+
+""
