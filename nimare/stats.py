d19d4d8cbbf4ef528b85d4fc651969459f520112
Tal Yarkoni <tyarkoni@gmail.com>
2020-11-07 15:22:06 -0600

    take min and max element-wise

--- nimare/stats.py
+++ nimare/stats.py
@@ -137,6 +137,7 @@ def null_to_p(test_value, null_array, tail="two"):
     Therefore no p-values of 0 or 1 should be produced.
     """
     test_value = np.atleast_1d(test_value)
+    # TODO: this runs in N^2 time; is there a more efficient alternative?
     p = np.array([stats.percentileofscore(null_array, v, 'strict') for v in test_value])
     p /= 100.
     if tail == "two":
@@ -150,7 +151,7 @@ def null_to_p(test_value, null_array, tail="two"):
 
     # ensure p_value in the following range:
     # smallest_value <= p_value <= (1.0 - smallest_value)
-    return max(smallest_value, min(p, 1.0 - smallest_value))
+    return np.maximum(smallest_value, np.maximum(p, 1.0 - smallest_value))
 
 
 def fdr(p, q=0.05):
