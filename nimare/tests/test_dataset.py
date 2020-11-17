50229f49236ddf3a5657b82f15043ec62ed880f0
Taylor Salo <tsalo006@fiu.edu>
2020-11-17 11:08:31 -0500

    Test that the bug has a workaround.

--- nimare/tests/test_dataset.py
+++ nimare/tests/test_dataset.py
@@ -1,6 +1,7 @@
 """
 Test nimare.dataset (Dataset IO/transformations).
 """
+import os
 import os.path as op
 
 import nibabel as nib
@@ -11,10 +12,12 @@ from nimare import dataset
 from nimare.tests.utils import get_test_data_path
 
 
-def test_dataset_smoke():
+def test_dataset_smoke(tmp_path_factory):
     """
     Smoke test for nimare.dataset.Dataset initialization and get methods.
     """
+    tmpdir = tmp_path_factory.mktemp("test_dataset_smoke")
+    out_file = os.path.join(tmpdir, "test_dataset.pkl.gz")
     db_file = op.join(get_test_data_path(), "neurosynth_dset.json")
     dset = dataset.Dataset(db_file)
     dset.update_path(get_test_data_path())
@@ -32,3 +35,8 @@ def test_dataset_smoke():
     mask_data[40, 40, 40] = 1
     mask_img = nib.Nifti1Image(mask_data, dset.masker.mask_img.affine)
     assert isinstance(dset.get_studies_by_mask(mask_img), list)
+    dset2 = dset.copy()
+    assert isinstance(dset2, nimare.dataset.Dataset)
+    dset2.save(out_file)
+    dset3 = dataset.Dataset.load(out_file)
+    assert isinstance(dset3, nimare.dataset.Dataset)
--- nimare/tests/test_io.py
+++ nimare/tests/test_io.py
@@ -10,10 +10,10 @@ from nimare import io
 from nimare.tests.utils import get_test_data_path
 
 
-def test_convert_sleuth_to_dataset_smoke():
-    """
-    Smoke test for Sleuth text file conversion.
-    """
+def test_convert_sleuth_to_dataset_smoke(tmp_path_factory):
+    """Smoke test for Sleuth text file conversion."""
+    tmpdir = tmp_path_factory.mktemp("test_convert_sleuth_to_dataset_smoke")
+    out_file = os.path.join(tmpdir, "temp.pkl.gz")
     sleuth_file = os.path.join(get_test_data_path(), "test_sleuth_file.txt")
     sleuth_file2 = os.path.join(get_test_data_path(), "test_sleuth_file2.txt")
     sleuth_file3 = os.path.join(get_test_data_path(), "test_sleuth_file3.txt")
@@ -24,11 +24,19 @@ def test_convert_sleuth_to_dataset_smoke():
     assert isinstance(dset, nimare.dataset.Dataset)
     assert dset.coordinates.shape[0] == 7
     assert len(dset.ids) == 3
+    dset_temp = dset.copy()
+    assert isinstance(dset_temp, nimare.dataset.Dataset)
+    dset.save(out_file)
+    assert os.path.isfile(out_file)
     # Use two input files
     dset2 = io.convert_sleuth_to_dataset([sleuth_file, sleuth_file2])
     assert isinstance(dset2, nimare.dataset.Dataset)
     assert dset2.coordinates.shape[0] == 11
     assert len(dset2.ids) == 5
+    dset2_temp = dset2.copy()
+    assert isinstance(dset2_temp, nimare.dataset.Dataset)
+    dset2.save(out_file)
+    assert os.path.isfile(out_file)
     # Use invalid input
     with pytest.raises(ValueError):
         io.convert_sleuth_to_dataset(5)
@@ -43,11 +51,10 @@ def test_convert_sleuth_to_dataset_smoke():
         io.convert_sleuth_to_dataset(sleuth_file5)
 
 
-def test_convert_sleuth_to_json_smoke():
-    """
-    Smoke test for Sleuth text file conversion.
-    """
-    out_file = os.path.abspath("temp.json")
+def test_convert_sleuth_to_json_smoke(tmp_path_factory):
+    """Smoke test for Sleuth text file conversion."""
+    tmpdir = tmp_path_factory.mktemp("test_convert_neurosynth_to_dataset_smoke")
+    out_file = os.path.join(tmpdir, "temp.json")
     sleuth_file = os.path.join(get_test_data_path(), "test_sleuth_file.txt")
     sleuth_file2 = os.path.join(get_test_data_path(), "test_sleuth_file2.txt")
     sleuth_file3 = os.path.join(get_test_data_path(), "test_sleuth_file3.txt")
@@ -81,25 +88,27 @@ def test_convert_sleuth_to_json_smoke():
         io.convert_sleuth_to_json(sleuth_file5, out_file)
 
 
-def test_convert_neurosynth_to_dataset_smoke():
-    """
-    Smoke test for Neurosynth file conversion.
-    """
+def test_convert_neurosynth_to_dataset_smoke(tmp_path_factory):
+    """Smoke test for Neurosynth file conversion."""
+    tmpdir = tmp_path_factory.mktemp("test_convert_neurosynth_to_dataset_smoke")
+    out_file = os.path.join(tmpdir, "temp.pkl.gz")
     db_file = os.path.join(get_test_data_path(), "test_neurosynth_database.txt")
     features_file = os.path.join(get_test_data_path(), "test_neurosynth_features.txt")
     dset = io.convert_neurosynth_to_dataset(db_file, features_file)
     assert isinstance(dset, nimare.dataset.Dataset)
+    dset_temp = dset.copy()
+    assert isinstance(dset_temp, nimare.dataset.Dataset)
+    dset_temp.save(out_file)
+    assert os.path.isfile(out_file)
 
 
-def test_convert_neurosynth_to_json_smoke():
-    """
-    Smoke test for Neurosynth file conversion.
-    """
-    out_file = os.path.abspath("temp.json")
+def test_convert_neurosynth_to_json_smoke(tmp_path_factory):
+    """Smoke test for Neurosynth file conversion."""
+    tmpdir = tmp_path_factory.mktemp("test_convert_neurosynth_to_dataset_smoke")
+    out_file = os.path.join(tmpdir, "temp.json")
     db_file = os.path.join(get_test_data_path(), "test_neurosynth_database.txt")
     features_file = os.path.join(get_test_data_path(), "test_neurosynth_features.txt")
     io.convert_neurosynth_to_json(db_file, out_file, annotations_file=features_file)
-    dset = nimare.dataset.Dataset(out_file)
     assert os.path.isfile(out_file)
+    dset = nimare.dataset.Dataset(out_file)
     assert isinstance(dset, nimare.dataset.Dataset)
-    os.remove(out_file)
