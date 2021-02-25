92f1672138b44bd8ffd2953e54d8681cb758f2f1
Tal Yarkoni <tyarkoni@gmail.com>
2021-02-25 09:15:52 -0700

    expand null_to_p tests

--- nimare/tests/test_stats.py
+++ nimare/tests/test_stats.py
@@ -3,24 +3,23 @@ Test nimare.stats
 """
 import math
 
+import pytest
 import numpy as np
 
 from nimare.stats import null_to_p, nullhist_to_p
 
 
 def test_null_to_p_float():
-    """Test nimare.stats.null_to_p with single float input."""
+    """Test null_to_p with single float input, assuming asymmetric null dist."""
 
     null = [-10, -9, -9, -3, -2, -1, -1, 0, 1, 1, 1, 2, 3, 3, 4, 4, 7, 8, 8, 9]
 
     # Two-tailed
-    assert math.isclose(null_to_p(0, null, "two"), 0.95)
-    result = null_to_p(9, null, "two")
-    assert result == null_to_p(-9, null, "two")
-    assert math.isclose(result, 0.2)
-    result = null_to_p(10, null, "two")
-    assert result == null_to_p(-10, null, "two")
-    assert math.isclose(result, 0.05)
+    assert math.isclose(null_to_p(0, null, "two"), 0.8)
+    assert math.isclose(null_to_p(9, null, "two"), 0.1)
+    assert math.isclose(null_to_p(10, null, "two"), 0.05)
+    assert math.isclose(null_to_p(-9, null, "two"), 0.3)
+    assert math.isclose(null_to_p(-10, null, "two"), 0.1)
     # Still 0.05 because minimum valid p-value is 1 / len(null)
     result = null_to_p(20, null, "two")
     assert result == null_to_p(-20, null, "two")
@@ -40,6 +39,25 @@ def test_null_to_p_float():
     nulldist = np.random.normal(size=10000)
     assert math.isclose(null_to_p(20, nulldist, "two"), 1/10000)
     assert math.isclose(null_to_p(20, nulldist, "lower"), 1 - 1/10000)
+
+
+def test_null_to_p_float_symmetric():
+    """Test null_to_p with single float input, assuming symmetric null dist."""
+
+    null = [-10, -9, -9, -3, -2, -1, -1, 0, 1, 1, 1, 2, 3, 3, 4, 4, 7, 8, 8, 9]
+
+    # Only need to test two-tailed; symmetry is irrelevant for one-tailed
+    assert math.isclose(null_to_p(0, null, "two", symmetric=True), 0.95)
+    result = null_to_p(9, null, "two", symmetric=True)
+    assert result == null_to_p(-9, null, "two", symmetric=True)
+    assert math.isclose(result, 0.2)
+    result = null_to_p(10, null, "two", symmetric=True)
+    assert result == null_to_p(-10, null, "two", symmetric=True)
+    assert math.isclose(result, 0.05)
+    # Still 0.05 because minimum valid p-value is 1 / len(null)
+    result = null_to_p(20, null, "two", symmetric=True)
+    assert result == null_to_p(-20, null, "two", symmetric=True)
+    assert math.isclose(result, 0.05)
 
 
 def test_null_to_p_array():
