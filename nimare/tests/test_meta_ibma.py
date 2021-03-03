5bb2e0133c2927d621b9ed6b54e168bf0c787498
James Kent <jamesdkent21@gmail.com>
2021-03-03 14:57:03 -0600

    refactor tests and check dimensions of results

--- nimare/tests/test_meta_ibma.py
+++ nimare/tests/test_meta_ibma.py
@@ -12,97 +12,103 @@ from nimare.meta import ibma
 from .utils import get_test_data_path
 
 
-def test_Fishers(testdata_ibma):
-    """Smoke test for Fisher's."""
-    meta = ibma.Fishers()
+@pytest.mark.parametrize(
+    "meta,meta_kwargs,corrector,corrector_kwargs",
+    [
+        pytest.param(
+            ibma.Fishers,
+            {},
+            FDRCorrector,
+            {"method": "indep", "alpha": 0.001},
+            id="Fishers",
+        ),
+        pytest.param(
+            ibma.Stouffers,
+            {'use_sample_size': False},
+            None,
+            {},
+            id="Stouffers",
+        ),
+        pytest.param(
+            ibma.Stouffers,
+            {'use_sample_size': True},
+            None,
+            {},
+            id="Stouffers_weighted",
+        ),
+        pytest.param(
+            ibma.WeightedLeastSquares,
+            {'tau2': 0},
+            None,
+            {},
+            id="WeightedLeastSquares",
+        ),
+        pytest.param(
+            ibma.DerSimonianLaird,
+            {},
+            None,
+            {},
+            id="DerSimonianLaird",
+        ),
+        pytest.param(
+            ibma.Hedges,
+            {},
+            None,
+            {},
+            id="Hedges",
+        ),
+        pytest.param(
+            ibma.SampleSizeBasedLikelihood,
+            {"method": "ml"},
+            None,
+            {},
+            id="SampleSizeBasedLikelihood_ml",
+        ),
+        pytest.param(
+            ibma.SampleSizeBasedLikelihood,
+            {"method": "reml"},
+            None,
+            {},
+            id="SampleSizeBasedLikelihood_reml",
+        ),
+        pytest.param(
+            ibma.VarianceBasedLikelihood,
+            {"method": "ml"},
+            None,
+            {},
+            id="VarianceBasedLikelihood_ml",
+        ),
+        pytest.param(
+            ibma.VarianceBasedLikelihood,
+            {"method": "reml"},
+            None,
+            {},
+            id="VarianceBasedLikelihood_reml",
+        ),
+        pytest.param(
+            ibma.PermutedOLS,
+            {"two_sided": True},
+            FWECorrector,
+            {"method": "montecarlo", "n_iters": 100, "n_cores": 1},
+            id="PermutedOLS",
+        ),
+    ]
+)
+def test_ibma_smoke(testdata_ibma, meta, meta_kwargs, corrector, corrector_kwargs):
+    """
+    Smoke test for Fisher's.
+    """
+    meta = meta(**meta_kwargs)
     res = meta.fit(testdata_ibma)
-    corr = FDRCorrector(method="indep", alpha=0.001)
-    cres = corr.transform(res)
     assert isinstance(meta.results, nimare.results.MetaResult)
     assert isinstance(res, nimare.results.MetaResult)
-    assert isinstance(cres, nimare.results.MetaResult)
-
-
-def test_Stouffers(testdata_ibma):
-    """Smoke test for Stouffer's, not weighted by sample size."""
-    meta = ibma.Stouffers(use_sample_size=False)
-    res = meta.fit(testdata_ibma)
-    assert isinstance(meta.results, nimare.results.MetaResult)
-    assert isinstance(res, nimare.results.MetaResult)
-
-
-def test_Stouffers_weighted(testdata_ibma):
-    """Smoke test for Stouffer's, weighted by sample size."""
-    meta = ibma.Stouffers(use_sample_size=True)
-    res = meta.fit(testdata_ibma)
-    assert isinstance(meta.results, nimare.results.MetaResult)
-    assert isinstance(res, nimare.results.MetaResult)
-
-
-def test_WeightedLeastSquares(testdata_ibma):
-    """Smoke test for WeightedLeastSquares."""
-    meta = ibma.WeightedLeastSquares(tau2=0)
-    res = meta.fit(testdata_ibma)
-    assert isinstance(meta.results, nimare.results.MetaResult)
-    assert isinstance(res, nimare.results.MetaResult)
-
-
-def test_DerSimonianLaird(testdata_ibma):
-    """Smoke test for DerSimonianLaird."""
-    meta = ibma.DerSimonianLaird()
-    res = meta.fit(testdata_ibma)
-    assert isinstance(meta.results, nimare.results.MetaResult)
-    assert isinstance(res, nimare.results.MetaResult)
-
-
-def test_Hedges(testdata_ibma):
-    """Smoke test for Hedges."""
-    meta = ibma.Hedges()
-    res = meta.fit(testdata_ibma)
-    assert isinstance(meta.results, nimare.results.MetaResult)
-    assert isinstance(res, nimare.results.MetaResult)
-
-
-def test_SampleSizeBasedLikelihood_ml(testdata_ibma):
-    """Smoke test for SampleSizeBasedLikelihood with ML."""
-    meta = ibma.SampleSizeBasedLikelihood(method="ml")
-    res = meta.fit(testdata_ibma)
-    assert isinstance(meta.results, nimare.results.MetaResult)
-    assert isinstance(res, nimare.results.MetaResult)
-
-
-def test_SampleSizeBasedLikelihood_reml(testdata_ibma):
-    """Smoke test for SampleSizeBasedLikelihood with REML."""
-    meta = ibma.SampleSizeBasedLikelihood(method="reml")
-    res = meta.fit(testdata_ibma)
-    assert isinstance(meta.results, nimare.results.MetaResult)
-    assert isinstance(res, nimare.results.MetaResult)
-
-
-def test_VarianceBasedLikelihood_ml(testdata_ibma):
-    """Smoke test for VarianceBasedLikelihood with ML."""
-    meta = ibma.VarianceBasedLikelihood(method="ml")
-    res = meta.fit(testdata_ibma)
-    assert isinstance(meta.results, nimare.results.MetaResult)
-    assert isinstance(res, nimare.results.MetaResult)
-
-
-def test_VarianceBasedLikelihood_reml(testdata_ibma):
-    """Smoke test for VarianceBasedLikelihood with REML."""
-    meta = ibma.VarianceBasedLikelihood(method="reml")
-    res = meta.fit(testdata_ibma)
-    assert isinstance(meta.results, nimare.results.MetaResult)
-    assert isinstance(res, nimare.results.MetaResult)
-
-
-def test_PermutedOLS(testdata_ibma):
-    """Smoke test for PermutedOLS with FWE correction."""
-    meta = ibma.PermutedOLS(two_sided=True)
-    meta.fit(testdata_ibma)
-    assert isinstance(meta.results, nimare.base.MetaResult)
-    corr = FWECorrector(method="montecarlo", n_iters=100, n_cores=1)
-    cres = corr.transform(meta.results)
-    assert isinstance(cres, nimare.base.MetaResult)
+    assert res.get_map("z", return_type="array").ndim == 1
+    assert res.get_map("z").ndim == 3
+    if corrector:
+        corr = corrector(**corrector_kwargs)
+        cres = corr.transform(res)
+        assert cres.get_map("z", return_type="array").ndim == 1
+        assert cres.get_map("z").ndim == 3
 
 
 def test_ibma_with_custom_masker(testdata_ibma):
