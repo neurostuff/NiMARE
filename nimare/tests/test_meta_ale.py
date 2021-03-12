22dd9b8edf62f2bc7a71b0357e9cd964f47ac9c1
Taylor Salo <tsalo006@fiu.edu>
2021-03-12 18:54:52 -0500

    Add ALESubtraction test.

--- nimare/tests/test_meta_ale.py
+++ nimare/tests/test_meta_ale.py
@@ -41,6 +41,32 @@ def test_ALE_ma_map_reuse(testdata_cbma, tmp_path_factory, caplog):
     assert "Loading pre-generated MA maps." in caplog.text
 
 
+def test_ALESubtraction_ma_map_reuse(testdata_cbma, tmp_path_factory, caplog):
+    """Test that MA maps are re-used when appropriate."""
+    from nimare.meta import kernel
+
+    tmpdir = tmp_path_factory.mktemp("test_ALESubtraction_ma_map_reuse")
+    testdata_cbma.update_path(tmpdir)
+
+    # ALEKernel cannot extract sample_size from a Dataset,
+    # so we need to set it for this kernel and for the later meta-analyses.
+    kern = kernel.ALEKernel(sample_size=20)
+    dset = kern.transform(testdata_cbma, return_type="dataset")
+
+    # The Dataset without the images will generate them from scratch.
+    sub_meta = ale.ALESubtraction(n_iters=10, kernel__sample_size=20)
+
+    with caplog.at_level(logging.DEBUG, logger="nimare.meta.cbma.ale"):
+        sub_meta.fit(testdata_cbma, testdata_cbma)
+    assert "Loading pre-generated MA maps" not in caplog.text
+
+    # The Dataset with the images will re-use them,
+    # as evidenced by the logger message.
+    with caplog.at_level(logging.DEBUG, logger="nimare.meta.cbma.ale"):
+        sub_meta.fit(dset, dset)
+    assert "Loading pre-generated MA maps" in caplog.text
+
+
 def test_ALE_analytic_null_unit(testdata_cbma, tmp_path_factory):
     """Unit test for ALE with analytic null_method."""
     tmpdir = tmp_path_factory.mktemp("test_ALE_analytic_null_unit")
