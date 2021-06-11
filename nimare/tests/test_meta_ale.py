914fe32b41847e55bcd18aed8a319c22b7d7c58f
Taylor Salo <tsalo006@fiu.edu>
2021-06-11 11:33:38 -0400

    Test drop_invalid = False.

--- nimare/tests/test_meta_ale.py
+++ nimare/tests/test_meta_ale.py
@@ -29,13 +29,17 @@ def test_ALE_ma_map_reuse(testdata_cbma, tmp_path_factory, caplog):
     assert any(["ALEKernel" in col for col in cols])
 
     # The Dataset without the images will generate them from scratch.
+    # If drop_invalid is False, then there should be an Exception, since two studies in the test
+    # dataset are missing coordinates.
     meta = ale.ALE(kernel__sample_size=20)
+    with pytest.raises(Exception):
+        meta.fit(testdata_cbma, drop_invalid=False)
+
     with caplog.at_level(logging.DEBUG, logger="nimare.meta.cbma.base"):
         meta.fit(testdata_cbma)
     assert "Loading pre-generated MA maps" not in caplog.text
 
-    # The Dataset with the images will re-use them,
-    # as evidenced by the logger message.
+    # The Dataset with the images will re-use them, as evidenced by the logger message.
     with caplog.at_level(logging.DEBUG, logger="nimare.meta.cbma.base"):
         meta.fit(dset)
     assert "Loading pre-generated MA maps" in caplog.text
