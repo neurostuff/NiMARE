838270439e1b546f32d1a58f8c15a74ba41efeb7
Taylor Salo <tsalo006@fiu.edu>
2021-07-01 12:35:23 -0400

    Clustering should be fairly good. Still need to implement metrics.

--- nimare/parcellate.py
+++ nimare/parcellate.py
@@ -1,11 +1,15 @@
 """Parcellation tools."""
+import datetime
 import inspect
 import logging
+import os
+from tempfile import mkstemp
 
 import numpy as np
 from sklearn.cluster import KMeans
 
 from .base import NiMAREBase
+from .extract.utils import _get_dataset_dir
 from .meta.base import CBMAEstimator
 from .meta.cbma.ale import ALE
 from .results import MetaResult
@@ -26,12 +30,18 @@ class CoordCBP(NiMAREBase):
         Currently must be in same space/resolution as Dataset mask.
     n_clusters : :obj:`list` of :obj:`int`
         Number of clusters to evaluate in clustering.
-    r : :obj:`float` or None, optional
-        Radius (in mm) within which to find studies. Mutually exclusive with ``n``.
-        Default is None.
-    n : :obj:`int` or None, optional
-        Number of closest studies to identify. Mutually exclusive with ``r``.
-        Default is None.
+        Metrics will be calculated for each cluster-count in the list, to allow users to select
+        the optimal cluster solution.
+    r : :obj:`float` or :obj:`list` of :obj:`float` or None, optional
+        Radius (in mm) within which to find studies.
+        If a list of values is provided, then MACMs and clustering will be performed across all
+        values, and a selection procedure will be performed to identify the optimal ``r``.
+        Mutually exclusive with ``n``. Default is None.
+    n : :obj:`int` or :obj:`list` of :obj:`int` or None, optional
+        Number of closest studies to identify.
+        If a list of values is provided, then MACMs and clustering will be performed across all
+        values, and a selection procedure will be performed to identify the optimal ``n``.
+        Mutually exclusive with ``r``. Default is None.
     meta_estimator : :obj:`nimare.meta.base.CBMAEstimator`, optional
         CBMA Estimator with which to run the MACMs.
         Default is :obj:`nimare.meta.cbma.ale.ALE`.
@@ -68,8 +78,10 @@ class CoordCBP(NiMAREBase):
         self.meta_estimator = meta_estimator
         self.target_image = target_image
         self.n_clusters = listify(n_clusters)
-        self.r = r
-        self.n = n
+        self.filter_selection = isinstance(r, list) or isinstance(n, list)
+        self.filter_type = "r" if r else "n"
+        self.r = listify(r)
+        self.n = listify(n)
 
     def _preprocess_input(self, dataset):
         """Mask required input images using either the dataset's mask or the estimator's.
@@ -80,7 +92,7 @@ class CoordCBP(NiMAREBase):
 
         # All extra (non-ijk) parameters for a kernel should be overrideable as
         # parameters to __init__, so we can access them with get_params()
-        kt_args = list(self.kernel_transformer.get_params().keys())
+        kt_args = list(self.meta_estimator.kernel_transformer.get_params().keys())
 
         # Integrate "sample_size" from metadata into DataFrame so that
         # kernel_transformer can access it.
@@ -104,36 +116,67 @@ class CoordCBP(NiMAREBase):
         """
         self.dataset = dataset
         self.masker = self.masker or dataset.masker
-        self.null_distributions_ = {}
 
         # Loop through voxels in target_mask, selecting studies for each and running MACMs (no MCC)
         target_ijk = np.vstack(np.where(self.target_mask.get_fdata()))
         target_xyz = vox2mm(target_ijk, self.masker.mask_img.affine)
-        for i_coord in target_xyz.shape[1]:
-            xyz = target_xyz[:, i_coord]
-            macm_ids = dataset.get_studies_by_coordinate(xyz, r=self.r, n=self.n)
-            coord_dset = dataset.slice(macm_ids)
+        n_target_voxels = target_xyz.shape[1]
+        n_mask_voxels = self.masker.transform(self.masker.mask_img).shape[0]
 
-            # This seems like a somewhat inelegant solution
-            # Check if the meta method is a pairwise estimator
-            if "dataset2" in inspect.getfullargspec(self.meta_estimator.fit).args:
-                unselected_ids = sorted(list(set(dataset.ids) - set(macm_ids)))
-                unselected_dset = dataset.slice(unselected_ids)
-                self.meta_estimator.fit(coord_dset, unselected_dset)
-            else:
-                self.meta_estimator.fit(coord_dset)
+        n_filters = len(getattr(self, self.filter_type))
+        labels = np.zeros((n_filters, len(self.n_clusters), n_target_voxels), dtype=int)
+        kwargs = {"r": None, "n": None}
 
-            macm_data = self.meta_estimator.results.get_map(self.target_image, return_type="array")
-            if i_coord == 0:
-                data = np.zeros((target_xyz.shape[1], len(macm_data)))
+        # Use a memmapped 2D array
+        start_time = datetime.datetime.now().strftime("%Y%m%dT%H%M%S")
+        dataset_dir = _get_dataset_dir("temporary_files", data_dir=None)
+        _, memmap_filename = mkstemp(
+            prefix=self.__class__.__name__,
+            suffix=start_time,
+            dir=dataset_dir,
+        )
+        data = np.memmap(
+            memmap_filename,
+            dtype=float,
+            mode="w+",
+            shape=(n_target_voxels, n_mask_voxels),
+        )
 
-            data[i_coord, :] = macm_data
+        for i_filter in range(n_filters):
+            kwargs[self.filter_type] = getattr(self, self.filter_type)[i_filter]
+            for j_coord in n_target_voxels:
+                xyz = target_xyz[:, j_coord]
+                macm_ids = dataset.get_studies_by_coordinate(xyz, **kwargs)
+                coord_dset = dataset.slice(macm_ids)
 
-        # Perform clustering
-        labels = np.zeros((len(self.n_clusters), len(macm_data)), dtype=int)
-        for i_cluster, cluster_count in enumerate(self.n_clusters):
-            kmeans = KMeans(n_clusters=cluster_count, random_state=0).fit(data)
-            labels[i_cluster, :] = kmeans.labels_
+                # This seems like a somewhat inelegant solution
+                # Check if the meta method is a pairwise estimator
+                if "dataset2" in inspect.getfullargspec(self.meta_estimator.fit).args:
+                    unselected_ids = sorted(list(set(dataset.ids) - set(macm_ids)))
+                    unselected_dset = dataset.slice(unselected_ids)
+                    self.meta_estimator.fit(coord_dset, unselected_dset)
+                else:
+                    self.meta_estimator.fit(coord_dset)
+
+                data[j_coord, :] = self.meta_estimator.results.get_map(
+                    self.target_image,
+                    return_type="array",
+                )  # data is overwritten across filters
+
+            # Perform clustering
+            for j_cluster, cluster_count in enumerate(self.n_clusters):
+                kmeans = KMeans(
+                    n_clusters=cluster_count,
+                    init="k-means++",
+                    n_init=10,
+                    random_state=0,
+                    algorithm="elkan",
+                ).fit(data)
+                labels[i_filter, j_cluster, :] = kmeans.labels_
+
+        # Clean up MACM data memmap
+        LGR.info(f"Removing temporary file: {memmap_filename}")
+        os.remove(memmap_filename)
 
         images = {"labels": labels}
         return images
