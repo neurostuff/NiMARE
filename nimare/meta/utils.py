80a9eb8b28c150b9e4583d2381763531120400f1
Taylor Salo <tsalo006@fiu.edu>
2021-08-08 12:43:19 -0400

    Fix more info.

--- nimare/meta/utils.py
+++ nimare/meta/utils.py
@@ -19,7 +19,7 @@ LGR = logging.getLogger(__name__)
 def model_fn(features, labels, mode, params):
     """Run model function used internally by peaks2maps.
 
-    .. versionadeed:: 0.0.4
+    .. versionadded:: 0.0.4
 
     """
     import tensorflow as tf
@@ -163,7 +163,7 @@ def model_fn(features, labels, mode, params):
 def _get_resize_arg(target_shape):
     """Get resizing arguments, as used by peaks2maps.
 
-    .. versionadeed:: 0.0.1
+    .. versionadded:: 0.0.1
 
     """
     mni_shape_mm = np.array([148.0, 184.0, 156.0])
@@ -183,11 +183,7 @@ def _get_resize_arg(target_shape):
 
 
 def _get_generator(contrasts_coordinates, target_shape, affine, skip_out_of_bounds=False):
-    """Get generator, as used by peaks2maps.
-
-    .. versionadeed:: 0.0.1
-
-    """
+    """Get generator, as used by peaks2maps."""
 
     def generator():
         for contrast in contrasts_coordinates:
@@ -211,8 +207,6 @@ def compute_p2m_ma(
     contrasts_coordinates, skip_out_of_bounds=True, tf_verbosity_level=None, model_dir="auto"
 ):
     """Generate modeled activation (MA) maps using deep ConvNet model peaks2maps.
-
-    .. versionadeed:: 0.0.1
 
     Parameters
     ----------
@@ -374,8 +368,6 @@ def compute_kda_ma(
 def compute_ale_ma(shape, ijk, kernel):
     """Generate ALE modeled activation (MA) maps.
 
-    .. versionadeed:: 0.0.1
-
     Replaces the values around each focus in ijk with the contrast-specific
     kernel. Takes the element-wise maximum when looping through foci, which
     accounts for foci which are near to one another and may have overlapping
@@ -436,11 +428,7 @@ def compute_ale_ma(shape, ijk, kernel):
 
 @due.dcite(references.ALE_KERNEL, description="Introduces sample size-dependent kernels to ALE.")
 def get_ale_kernel(img, sample_size=None, fwhm=None):
-    """Estimate 3D Gaussian and sigma (in voxels) for ALE kernel given sample size or fwhm.
-
-    .. versionadeed:: 0.0.1
-
-    """
+    """Estimate 3D Gaussian and sigma (in voxels) for ALE kernel given sample size or fwhm."""
     if sample_size is not None and fwhm is not None:
         raise ValueError('Only one of "sample_size" and "fwhm" may be specified')
     elif sample_size is None and fwhm is None:
