[FIX] Separate IJK calculation from coordinate space conversion (#556)
* Separate IJK calculation from coordinate space conversion.

* Calculate IJK as needed.

* Replace deprecated numpy dtype.

* Fix known bug with `get_studies_by_label`.

Namely that, when the label isn't present in the Dataset, all studies were returned. Now it raises a ValueError.

* Restructure tests and fix problems with them.

* Fix?

* Keep replacing IJK with XYZ.

* Just realized that replacing loc with iloc was a mistake.

The distances are a _boolean_ array, not integer indices.

* Ignore resolution.

nimare/dataset.py
@@ -17,7 +17,7 @@ from .utils import (
     get_template,
     listify,
     mm2vox,
-    transform_coordinates_to_ijk,
+    transform_coordinates_to_space,
     try_prepend,
     validate_df,
     validate_images_df,
@@ -184,14 +184,10 @@ class Dataset(NiMAREBase):
         if hasattr(self, "masker") and not np.array_equal(
             self.masker.mask_img.affine, mask.mask_img.affine
         ):
-            LGR.info(
-                "New masker does not match old masker. "
-                "Space is assumed to be the same, but coordinates will "
-                "be transformed to new matrix."
-            )
-            coords = self.coordinates
-            coords[["i", "j", "k"]] = mm2vox(coords[["x", "y", "z"]], mask.mask_img.affine)
-            self.coordinates = coords
+            # This message does not have an associated effect,
+            # since matrix indices are calculated as necessary
+            LGR.warning("New masker does not match old masker. Space is assumed to be the same.")
+
         self.__masker = mask
 
     @property
@@ -214,10 +210,13 @@ class Dataset(NiMAREBase):
     def coordinates(self):
         """:class:`pandas.DataFrame`: Coordinates in the dataset.
 
+        .. versionchanged:: 0.0.10
+
+            The coordinates attribute no longer includes the associated matrix indices
+            (columns 'i', 'j', and 'k'). These columns are calculated as needed.
+
         Each study has one row for each peak.
-        Columns include ['x', 'y', 'z'] (peak locations in mm),
-        ['i', 'j', 'k'] (peak locations in voxel index based on Dataset's space),
-        and 'space' (Dataset's space).
+        Columns include ['x', 'y', 'z'] (peak locations in mm) and 'space' (Dataset's space).
         """
         return self.__coordinates
 
@@ -331,7 +330,7 @@ class Dataset(NiMAREBase):
             new_df = new_df.where(~new_df.isna(), None)
             setattr(new_dset, attribute, new_df)
 
-        new_dset.coordinates = transform_coordinates_to_ijk(
+        new_dset.coordinates = transform_coordinates_to_space(
             new_dset.coordinates,
             self.masker,
             self.space,
@@ -584,6 +583,10 @@ class Dataset(NiMAREBase):
     def get_studies_by_label(self, labels=None, label_threshold=0.001):
         """Extract list of studies with a given label.
 
+        .. versionchanged:: 0.0.10
+
+            Fix bug in which all IDs were returned when a label wasn't present in the Dataset.
+
         .. versionchanged:: 0.0.9
 
             Default value for label_threshold changed to 0.001.
@@ -604,19 +607,20 @@ class Dataset(NiMAREBase):
         """
         if isinstance(labels, str):
             labels = [labels]
-        elif labels is None:
-            # For now, labels are all we can search by.
-            return self.ids
         elif not isinstance(labels, list):
             raise ValueError(f"Argument 'labels' cannot be {type(labels)}")
 
-        found_labels = [label for label in labels if label in self.annotations.columns]
-        temp_annotations = self.annotations[self._id_cols + found_labels]
-        found_rows = (temp_annotations[found_labels] >= label_threshold).all(axis=1)
+        missing_labels = [label for label in labels if label not in self.annotations.columns]
+        if missing_labels:
+            raise ValueError(f"Missing label(s): {', '.join(missing_labels)}")
+
+        temp_annotations = self.annotations[self._id_cols + labels]
+        found_rows = (temp_annotations[labels] >= label_threshold).all(axis=1)
         if any(found_rows):
             found_ids = temp_annotations.loc[found_rows, "id"].tolist()
         else:
             found_ids = []
+
         return found_ids
 
     def get_studies_by_mask(self, mask):
@@ -638,11 +642,11 @@ class Dataset(NiMAREBase):
 
         dset_mask = self.masker.mask_img
         if not np.array_equal(dset_mask.affine, mask.affine):
-            from nilearn.image import resample_to_img
+            LGR.warning("Mask affine does not match Dataset affine. Assuming same space.")
 
-            mask = resample_to_img(mask, dset_mask, interpolation="nearest")
+        dset_ijk = mm2vox(self.coordinates[["x", "y", "z"]].values, mask.affine)
         mask_ijk = np.vstack(np.where(mask.get_fdata())).T
-        distances = cdist(mask_ijk, self.coordinates[["i", "j", "k"]].values)
+        distances = cdist(mask_ijk, dset_ijk)
         distances = np.any(distances == 0, axis=0)
         found_ids = list(self.coordinates.loc[distances, "id"].unique())
         return found_ids

nimare/meta/kernel.py
@@ -16,7 +16,13 @@ import pandas as pd
 from nilearn import image
 
 from ..base import Transformer
-from ..utils import add_metadata_to_dataframe, safe_transform, use_memmap, vox2mm
+from ..utils import (
+    add_metadata_to_dataframe,
+    mm2vox,
+    safe_transform,
+    use_memmap,
+    vox2mm,
+)
 from .utils import compute_ale_ma, compute_kda_ma, compute_p2m_ma, get_ale_kernel
 
 LGR = logging.getLogger(__name__)
@@ -118,6 +124,10 @@ class KernelTransformer(Transformer):
             assert (
                 return_type != "dataset"
             ), "Input dataset must be a Dataset if return_type='dataset'."
+
+            # Calculate IJK. Must assume that the masker is in same space,
+            # but has different affine, from original IJK.
+            coordinates[["i", "j", "k"]] = mm2vox(coordinates[["x", "y", "z"]], mask.affine)
         else:
             masker = dataset.masker if not masker else masker
             mask = masker.mask_img
@@ -141,6 +151,12 @@ class KernelTransformer(Transformer):
                     elif return_type == "dataset":
                         return dataset.copy()
 
+            # Calculate IJK
+            if not np.array_equal(mask.affine, dataset.masker.mask_img.affine):
+                LGR.warning("Mask affine does not match Dataset affine. Assuming same space.")
+
+            coordinates[["i", "j", "k"]] = mm2vox(coordinates[["x", "y", "z"]], mask.affine)
+
             # Add any metadata the Transformer might need to the coordinates DataFrame
             # This approach is probably inferior to one which uses a _required_inputs attribute
             # (like the MetaEstimators), but it should work just fine as long as individual
@@ -160,7 +176,7 @@ class KernelTransformer(Transformer):
 
         # Generate the MA maps if they weren't already available as images
         if return_type == "array":
-            mask_data = mask.get_fdata().astype(np.bool)
+            mask_data = mask.get_fdata().astype(bool)
         elif return_type == "image":
             dtype = type(self.value) if hasattr(self, "value") else float
             mask_data = mask.get_fdata().astype(dtype)

nimare/workflows/scale.py
@@ -9,6 +9,7 @@ import numpy as np
 from ..dataset import Dataset
 from ..io import convert_sleuth_to_dataset
 from ..meta import SCALE
+from ..utils import vox2mm
 
 LGR = logging.getLogger(__name__)
 
@@ -60,11 +61,14 @@ rates. NeuroImage, 99, 559-570.
     # indices matching the dataset template, where the base rate for a given
     # voxel is reflected by the number of times that voxel appears in the array
     if not baseline:
-        ijk = np.vstack(np.where(dset.masker.mask_img.get_fdata())).T
+        xyz = vox2mm(
+            np.vstack(np.where(dset.masker.mask_img.get_fdata())).T,
+            dset.masker.mask_img.affine,
+        )
     else:
-        ijk = np.loadtxt(baseline)
+        xyz = np.loadtxt(baseline)
 
-    estimator = SCALE(ijk=ijk, n_iters=n_iters, n_cores=n_cores)
+    estimator = SCALE(xyz=xyz, n_iters=n_iters, n_cores=n_cores)
     estimator.fit(dset)
 
     if output_dir is None: