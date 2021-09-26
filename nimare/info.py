ea5a9f27e5fce3b5ef75169859b171ac4a85d90a
Taylor Salo <tsalo006@fiu.edu>
2021-09-26 12:19:29 -0400

    Pin best-guess minimum versions.

--- nimare/info.py
+++ nimare/info.py
@@ -90,6 +90,16 @@ EXTRA_REQUIRES = {
     ],
     "tests": TESTS_REQUIRES,
     "duecredit": ["duecredit"],
+    "minimum": [
+        "indexed_gzip==1.4.0",
+        "nibabel==3.0.0",
+        "nilearn==0.7.1",
+        "numpy==1.18.0",
+        "pandas==1.1.0",
+        "pymare==0.0.2",
+        "scikit-learn==0.22",
+        "scipy==1.6.0",
+    ]
 }
 
 # Enable a handle to install all extra dependencies at once
