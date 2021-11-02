92bed0757e150c53d33a325070e2453c74b6b48f
Taylor Salo <tsalo006@fiu.edu>
2021-11-02 15:38:37 -0400

    Improve docstrings and support IBMAs.

--- nimare/diagnostics.py
+++ nimare/diagnostics.py
@@ -4,7 +4,7 @@ from abc import ABCMeta
 import nibabel as nib
 import numpy as np
 import pandas as pd
-from nilearn import input_data
+from nilearn import image, input_data
 from scipy import ndimage
 from tqdm.auto import tqdm
 
@@ -12,39 +12,94 @@ from .utils import vox2mm
 
 
 class Jackknife(metaclass=ABCMeta):
-    """Run a jackknife analysis on a meta-analysis result."""
+    """Run a jackknife analysis on a meta-analysis result.
 
-    def __init__(self):
-        ...
+    Parameters
+    ----------
+    target_image
+        The default is z because log-p will not always have value of zero for non-cluster voxels.
+    voxel_thresh
+    n_cores
+
+    Notes
+    -----
+    This analysis characterizes the relative contribution of each experiment in a meta-analysis
+    to the resulting clusters by looping through experiments, calculating the Estimator's summary
+    statistic for all experiments *except* the target experiment, dividing the resulting test
+    summary statistics by the summary statistics from the original meta-analysis, and finally
+    averaging the resulting proportion values across all voxels in each cluster.
+    """
+
+    def __init__(
+        self,
+        target_image="z_level-cluster_corr-FWE_method-montecarlo",
+        voxel_thresh=None,
+        n_cores=1,
+    ):
+        self.target_image = target_image
+        self.voxel_thresh = voxel_thresh
+        self.n_cores = n_cores
 
     def transform(self, result):
+        """Apply the analysis to a MetaResult.
+
+        Parameters
+        ----------
+        result : :obj:`nimare.results.MetaResult`
+            A MetaResult produced by a coordinate- or image-based meta-analysis.
+            Multiple comparisons correction must be performed prior to applying the Jackknife.
+
+        Returns
+        -------
+        contribution_table : :obj:`pandas.DataFrame`
+            A DataFrame with information about relative contributions of each experiment to each
+            cluster in the thresholded map.
+            There is one row for each experiment, as well as one more row at the top of the table
+            (below the header), which has the center of mass of each cluster.
+            The centers of mass are not guaranteed to fall within the actual clusters, but can
+            serve as a useful heuristic for identifying them.
+            There is one column for each cluster, with column names being integers indicating the
+            cluster's associated value in the ``labeled_cluster_img`` output.
+        labeled_cluster_img : :obj:`nibabel.nifti1.Nifti1Image`
+            The labeled, thresholded map that is used to identify clusters characterized by this
+            analysis.
+            Each cluster in the map has a single value, which corresponds to the cluster's column
+            name in ``contribution_table``.
+        """
         dset = result.estimator.dataset
         estimator = result.estimator
         original_masker = estimator.masker
-        # Using z because log-p will not always have value of zero for non-cluster voxels
-        cfwe_img = result.get_map(
-            "z_level-cluster_corr-FWE_method-montecarlo",
-            return_type="image",
-        )
-        stat_values = result.get_map("stat", return_type="array")
-        cfwe_arr = cfwe_img.get_fdata()
 
-        # Let's label the clusters in the cFWE map so we can use it as a NiftiLabelsMasker
+        target_img = result.get_map(self.target_image, return_type="image")
+        if self.voxel_threshold:
+            thresh_img = image.threshold_img(target_img, self.voxel_threshold)
+        else:
+            thresh_img = target_img
+
+        # CBMAs have "stat" maps, while IBMAs have "est" maps.
+        if "est" in result.maps:
+            stat_values = result.get_map("est", return_type="array")
+        else:
+            stat_values = result.get_map("stat", return_type="array")
+
+        thresh_arr = thresh_img.get_fdata()
+
+        # Let's label the clusters in the thresholded map so we can use it as a NiftiLabelsMasker
         # This won't work when the Estimator's masker isn't a NiftiMasker... :(
         conn = np.zeros((3, 3, 3), int)
         conn[:, :, 1] = 1
         conn[:, 1, :] = 1
         conn[1, :, :] = 1
-        cfwe_arr_labeled, n_clusters = ndimage.measurements.label(cfwe_arr, conn)
+        cfwe_arr_labeled, n_clusters = ndimage.measurements.label(thresh_arr, conn)
         cluster_ids = list(range(1, n_clusters + 1))
         coms = ndimage.center_of_mass(cfwe_arr_labeled, cfwe_arr_labeled, cluster_ids)
         coms = np.array(coms)
-        coms = vox2mm(coms, cfwe_img.affine)
+        coms = vox2mm(coms, target_img.affine)
 
-        cfwe_img_labeled = nib.Nifti1Image(
+        labeled_cluster_img = nib.Nifti1Image(
             cfwe_arr_labeled,
-            affine=cfwe_img.affine,
-            header=cfwe_img.header,
+            affine=target_img.affine,
+            header=target_img.header,
         )
 
         cluster_peak_strs = []
@@ -54,19 +109,20 @@ class Jackknife(metaclass=ABCMeta):
             cluster_peak_strs.append(xyz_str)
 
         # Mask using a labels masker
-        cluster_masker = input_data.NiftiLabelsMasker(cfwe_img_labeled)
-        cluster_masker.fit(cfwe_img_labeled)
+        cluster_masker = input_data.NiftiLabelsMasker(labeled_cluster_img)
+        cluster_masker.fit(labeled_cluster_img)
 
         # Should probably go off IDs in estimator.inputs_ instead,
         # in case some experiments are filtered out based on available data.
-        rows = ["Center of Mass"] + list(dset.ids)
-        output = pd.DataFrame(index=rows, columns=cluster_ids)
-        output.index.name = "Cluster ID"
-        output.loc["Center of Mass"] = cluster_peak_strs
+        meta_ids = estimator.inputs_["id"]
+        rows = ["Center of Mass"] + list(meta_ids)
+        contribution_table = pd.DataFrame(index=rows, columns=cluster_ids)
+        contribution_table.index.name = "Cluster ID"
+        contribution_table.loc["Center of Mass"] = cluster_peak_strs
 
-        for i_expid in tqdm(range(len(dset.ids))):
-            expid = dset.ids[i_expid]
-            other_ids = [id_ for id_ in dset.ids if id_ != expid]
+        for i_expid in tqdm(range(len(meta_ids))):
+            expid = meta_ids[i_expid]
+            other_ids = [id_ for id_ in meta_ids if id_ != expid]
             temp_dset = dset.slice(other_ids)
             temp_result = estimator.fit(temp_dset)
             temp_stat_img = temp_result.get_map("stat", return_type="image")
@@ -82,6 +138,6 @@ class Jackknife(metaclass=ABCMeta):
 
             # Now get the cluster-wise mean of the proportion values
             stat_prop_values = cluster_masker.transform(stat_prop_img)
-            output.loc[expid] = stat_prop_values
+            contribution_table.loc[expid] = stat_prop_values
 
-        return output, cfwe_img_labeled
+        return contribution_table, labeled_cluster_img
