60def09db7df0156932c4fb2120b8e50a40fcfbe
Taylor Salo <tsalo006@fiu.edu>
2021-11-06 11:58:07 -0400

    Add major classes to parent namespaces.

--- nimare/annotate/__init__.py
+++ nimare/annotate/__init__.py
@@ -1,5 +1,21 @@
 """Automated annotation tools."""
 
 from . import cogat, gclda, lda, text, utils
+from .cogat import CogAtLemmatizer, expand_counts, extract_cogat
+from .gclda import GCLDAModel
+from .lda import LDAModel
+from .text import generate_counts
 
-__all__ = ["cogat", "gclda", "lda", "text", "utils"]
+__all__ = [
+    "CogAtLemmatizer",
+    "expand_counts",
+    "extract_cogat",
+    "GCLDAModel",
+    "LDAModel",
+    "generate_counts",
+    "cogat",
+    "gclda",
+    "lda",
+    "text",
+    "utils",
+]
