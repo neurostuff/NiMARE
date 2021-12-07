b0629a91b1e0e9a61036a6e4fec7852735e6a22b
Taylor Salo <tsalo006@fiu.edu>
2021-12-06 19:06:20 -0500

    Update examples.

--- examples/02_meta-analyses/01_plot_cbma.py
+++ examples/02_meta-analyses/01_plot_cbma.py
@@ -104,7 +104,7 @@ ale.fit(dset)
 corr = FWECorrector(method="montecarlo", n_iters=10, n_cores=1)
 cres = corr.transform(ale.results)
 plot_stat_map(
-    cres.get_map("logp_level-cluster_corr-FWE_method-montecarlo"),
+    cres.get_map("logp_desc-size_level-cluster_corr-FWE_method-montecarlo"),
     cut_coords=[0, 0, -8],
     draw_cross=False,
     cmap="RdBu_r",
