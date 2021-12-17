c59f1dceeca3c8cbbf333c7151d97f37fdddee82
Taylor Salo <tsalo006@fiu.edu>
2021-12-17 11:01:53 -0500

    Remove matplotlib as a dependency.

--- nimare/info.py
+++ nimare/info.py
@@ -50,7 +50,6 @@ REQUIRES = [
     "cognitiveatlas",
     "fuzzywuzzy",
     "indexed_gzip>=1.4.0",
-    "matplotlib<3.5",  # this is for nilearn, which doesn't include it in its reqs
     "nibabel>=3.0.0",
     "nilearn>=0.7.1",
     "numpy",
