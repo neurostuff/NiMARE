edc35219ff98a9ec6881fdb66d6250b72f4f42d9
Taylor Salo <tsalo006@fiu.edu>
2021-12-21 11:50:39 -0500

    Create nimads.py

---
+++ nimare/nimads.py
@@ -0,0 +1,25 @@
+"""NIMADS-related classes for NiMARE."""
+
+
+class Studyset:
+    ...
+
+
+class Study:
+    ...
+
+
+class Analysis:
+    ...
+
+
+class Annotation:
+    ...
+
+
+class Images:
+    ...
+
+
+class Coordinates:
+    ...
