f8b4dd6727e28c90ee64301175ec94f3664a8239
Taylor Salo <tsalo006@fiu.edu>
2022-01-16 10:42:36 -0500

    Fix the simulated data.

--- nimare/meta/cbma/sdm.py
+++ nimare/meta/cbma/sdm.py
@@ -660,6 +660,8 @@ def permute_subject_values(y, n_subjects):
 def simulate_subject_values(n_subjects1, n_subjects2):
     """Simulate (true) subject values.
 
+    Simply draw from a normal distribution, then add a small effect size (0.2) to the first group.
+
     Parameters
     ----------
     n_subjects1, n_subjects2 : :obj:`numpy.ndarray` of shape (n_studies,)
@@ -689,7 +691,7 @@ def simulate_subject_values(n_subjects1, n_subjects2):
         # Any columns outside of the individual study's sample size will have NaNs
         y[i_study, (n_subjects1[i_study] + n_subjects2[i_study]) :] = np.nan
         # Add a small effect size
-        y[i_study, : (n_subjects1[i_study] + n_subjects2[i_study])] += 0.2
+        y[i_study, : n_subjects1[i_study]] += 0.2
 
     return y
 
@@ -713,99 +715,7 @@ def run_simulations2(n_perms=1000, n_sims=10, n_subjects=20, n_studies=10):
     -----
     TODO: Support one-sample studies.
     TODO: Support voxel-wise data, instead of single points per subject.
-
-    R Code
-    ------
-    library(doParallel)
-    library(metafor)
-    registerDoParallel(cores = detectCores() - 1)
-    par(mfrow = 1:2)
-
-    # Simulation parameters #######################################################
-    n.perm <- 1000 # Number of permutations
-    n.sim <- 1000 # Number of simulations
-    n.subj <- 20 # Number of subjects per group of a study
-    n.stud <- 10 # Number of studies
-
-    # Constants
-    df <- 2 * n.subj - 2 # Degrees of freedom
-    J <- gamma(df / 2) / gamma((df - 1) / 2) * sqrt(2 / df) # Hedges' correction
-
-    sim <- do.call(rbind.data.frame, foreach (i.sim = 1:n.sim, .packages = "metafor")
-    %dopar% {
-        # Simulate subject data of all studies
-        y.unperm <- sim.y()
-
-        # Calculate Hedges' g
-        g.unperm <- g(y.unperm)
-        g_var.unperm <- g_var(g.unperm)
-
-        # Meta-analysis
-        m.unperm <- rma(g.unperm, g_var.unperm)
-        z.unperm <- m.unperm$zval
-
-        # Save null distributions of z-values
-        nd.z.perm_stud <- z.unperm
-        nd.z.perm_subj <- z.unperm
-
-        # Time before study-based permutation test
-        time0 <- Sys.time()
-
-        # Study-based permutation test
-        for (i.perm in 1:(n.perm - 1)) {
-            # Permute study data
-            g.stud_perm <- perm1(g.unperm)
-
-            # Meta-analysis of permuted study data
-            m.stud_perm <- rma(g.stud_perm, g_var.unperm)
-
-            # Save null distribution of z-values
-            nd.z.perm_stud <- c(nd.z.perm_stud, m.stud_perm$zval)
-        }
-
-        # Time between study-based and subject-based permutation tests
-        time1 <- Sys.time()
-
-        # Subject-based permutation test
-        for (i.perm in 1:(n.perm - 1)) {
-            # Permute subject data
-            y.subj_perm <- perm2(y.unperm)
-
-            # Calculate Hedges' g of permuted subject data
-            g.subj_perm <- g(y.subj_perm)
-            g_var.subj_perm <- g_var(g.subj_perm)
-
-            # Meta-analysis of permuted subject data
-            m.subj_perm <- rma(g.subj_perm, g_var.subj_perm)
-
-            # Save null distribution of z-values
-            nd.z.perm_subj <- c(nd.z.perm_subj, m.subj_perm$zval)
-        }
-
-        # Time after subject-based permutation tests
-        time2 <- Sys.time()
-
-        # Save times and two-tailed p-values
-        data.frame(
-            time.perm_stud = as.numeric(time1 - time0),
-            time.perm_subj = as.numeric(time2 - time1),
-            p.z = m.unperm$pval,
-            p.perm_stud = 1 - 2 * abs(mean(z.unperm > nd.z.perm_stud) - 0.5),
-            p.perm_subj = 1 - 2 * abs(mean(z.unperm > nd.z.perm_subj) - 0.5)
-        )
-    })
-
-    # Output results
-    time.perm_stud <- sim$time.perm_stud
-    time.perm_subj <- sim$time.perm_subj
-    mse.perm_stud <- (sim$p.perm_stud - sim$p.z)^2
-    mse.perm_subj <- (sim$p.perm_subj - sim$p.z)^2
-    cat("Decrease in execution time: ", round(
-        (mean(time.perm_subj) - mean(time.perm_stud)) / mean(time.perm_subj),
-        2) * 100, "%\n", sep = "")
-    cat("Increase in mean squared error: ", round(
-        (mean(mse.perm_stud) - mean(mse.perm_subj)) / mean(mse.perm_subj),
-        2) * 100, "%\n", sep = "")
+    See nimare/meta/cbma/sdmpsi_supplement_analysis2.r.
     """
     from pprint import pprint
 
@@ -953,7 +863,7 @@ def compute_sdm_ma(
 
     References
     ----------
-    * Radua, J., Rubia, K., Canales, E. J., Pomarol-Clotet, E., Fusar-Poli, P., &
+    *   Radua, J., Rubia, K., Canales, E. J., Pomarol-Clotet, E., Fusar-Poli, P., &
         Mataix-Cols, D. (2014). Anisotropic kernels for coordinate-based meta-analyses of
         neuroimaging studies. Frontiers in psychiatry, 5, 13.
     """
