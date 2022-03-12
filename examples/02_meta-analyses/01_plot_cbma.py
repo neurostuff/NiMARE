a3b20e966e4fce4b8755dee9bd1ed99d1f060e5c
Taylor Salo <tsalo006@fiu.edu>
2022-03-12 12:14:23 -0500

    Update the example.

--- examples/02_meta-analyses/01_plot_cbma.py
+++ examples/02_meta-analyses/01_plot_cbma.py
@@ -8,18 +8,13 @@ Coordinate-Based Meta-Analysis Algorithms
 
 A tour of CBMA algorithms in NiMARE.
 
-.. note::
-    The data used in this example come from a collection of NIDM-Results packs
-    downloaded from Neurovault collection 1425, uploaded by Dr. Camille Maumet.
-
-    Creation of the Dataset from the NIDM-Results packs was done with custom
-    code. The Results packs for collection 1425 are not completely
-    NIDM-Results-compliant, so the nidmresults library could not be used to
-    facilitate data extraction.
+This tutorial is intended to provide a brief description and example of each of
+the CBMA algorithms implemented in NiMARE.
+For a more detailed introduction to the elements of a coordinate-based
+meta-analysis, see other stuff.
 """
 import os
 
-import numpy as np
 from nilearn.plotting import plot_stat_map
 
 from nimare.correct import FWECorrector
@@ -29,6 +24,14 @@ from nimare.utils import get_resource_path
 ###############################################################################
 # Load Dataset
 # --------------------------------------------------
+# .. note::
+#   The data used in this example come from a collection of NIDM-Results packs
+#   downloaded from Neurovault collection 1425, uploaded by Dr. Camille Maumet.
+#
+#   Creation of the Dataset from the NIDM-Results packs was done with custom
+#   code. The Results packs for collection 1425 are not completely
+#   NIDM-Results-compliant, so the nidmresults library could not be used to
+#   facilitate data extraction.
 dset_file = os.path.join(get_resource_path(), "nidm_pain_dset.json")
 dset = Dataset(dset_file)
 
@@ -49,6 +52,13 @@ corr = FWECorrector(method="montecarlo", n_iters=10, n_cores=1)
 cres = corr.transform(results)
 
 plot_stat_map(
+    results.get_map("z"),
+    cut_coords=[0, 0, -8],
+    draw_cross=False,
+    cmap="RdBu_r",
+    threshold=0.1,
+)
+plot_stat_map(
     cres.get_map("z_level-voxel_corr-FWE_method-montecarlo"),
     cut_coords=[0, 0, -8],
     draw_cross=False,
@@ -68,10 +78,16 @@ corr = FWECorrector(method="montecarlo", n_iters=10, n_cores=1)
 cres = corr.transform(results)
 
 plot_stat_map(
+    results.get_map("z_desc-consistency"),
+    draw_cross=False,
+    cmap="RdBu_r",
+    threshold=0.1,
+)
+plot_stat_map(
     cres.get_map("z_desc-consistencySize_level-cluster_corr-FWE_method-montecarlo"),
     draw_cross=False,
     cmap="RdBu_r",
-    threshold=1.65,
+    threshold=0.1,
 )
 
 ###############################################################################
@@ -86,7 +102,14 @@ corr = FWECorrector(method="montecarlo", n_iters=10, n_cores=1)
 cres = corr.transform(results)
 
 plot_stat_map(
-    cres.get_map("z_level-voxel_corr-FWE_method-montecarlo"),
+    results.get_map("z"),
+    cut_coords=[0, 0, -8],
+    draw_cross=False,
+    cmap="RdBu_r",
+    threshold=0.1,
+)
+plot_stat_map(
+    cres.get_map("z_desc-size_level-cluster_corr-FWE_method-montecarlo"),
     cut_coords=[0, 0, -8],
     draw_cross=False,
     cmap="RdBu_r",
@@ -105,6 +128,13 @@ corr = FWECorrector(method="montecarlo", n_iters=10, n_cores=1)
 cres = corr.transform(results)
 
 plot_stat_map(
+    results.get_map("z"),
+    cut_coords=[0, 0, -8],
+    draw_cross=False,
+    cmap="RdBu_r",
+    threshold=0.1,
+)
+plot_stat_map(
     cres.get_map("z_desc-size_level-cluster_corr-FWE_method-montecarlo"),
     cut_coords=[0, 0, -8],
     draw_cross=False,
@@ -115,29 +145,27 @@ plot_stat_map(
 ###############################################################################
 # Specific Co-Activation Likelihood Estimation
 # --------------------------------------------------
-from nimare.meta.cbma.ale import SCALE
-from nimare.utils import vox2mm
+#
+# .. important::
+#
+#   The SCALE algorithm is very memory intensive, so we don't run it within the
+#   documentation.
+#
+# .. code-block:: python
+#
+#   import numpy as np
+#
+#   from nimare.meta.cbma.ale import SCALE
+#   from nimare.utils import vox2mm
+#
+#   xyz = vox2mm(
+#       np.vstack(np.where(dset.masker.mask_img.get_fdata())).T,
+#       dset.masker.mask_img.affine,
+#   )
+#
+#   meta = SCALE(xyz=xyz, n_iters=10)
+#   results = meta.fit(dset)
 
-# Here, we would use a list of coordinates from the database from which the
-# Dataset is drawn.
-# However, for the sake of a light-weight example, we will just first the
-# coordinates in the brain mask.
-"""xyz = vox2mm(
-    np.vstack(np.where(dset.masker.mask_img.get_fdata())).T,
-    dset.masker.mask_img.affine,
-)
-
-meta = SCALE(xyz=xyz, n_iters=10)
-results = meta.fit(dset)
-
-plot_stat_map(
-    results.get_map("z"),
-    cut_coords=[0, 0, -8],
-    draw_cross=False,
-    cmap="RdBu_r",
-    threshold=0.1,
-)
-"""
 ###############################################################################
 # ALE-Based Subtraction Analysis
 # --------------------------------------------------
