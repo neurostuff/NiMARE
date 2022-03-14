94b9e9d6f9bace9997c329d98a8edbcf045e1d54
Taylor Salo <tsalo006@fiu.edu>
2022-03-14 15:35:09 -0400

    Update 03_plot_kernel_transformers.py

--- examples/02_meta-analyses/03_plot_kernel_transformers.py
+++ examples/02_meta-analyses/03_plot_kernel_transformers.py
@@ -17,10 +17,10 @@ used to generate the MA maps differs by algorithm.
 This example provides an introduction to the ``KernelTransformer`` class and
 a tour of available types.
 """
-# sphinx_gallery_thumbnail_number = 2
 ###############################################################################
 # Start with the necessary imports
 # -----------------------------------------------------------------------------
+# sphinx_gallery_thumbnail_number = 2
 import os
 
 import matplotlib.pyplot as plt
@@ -35,25 +35,33 @@ from nimare.utils import get_resource_path
 dset_file = os.path.join(get_resource_path(), "nidm_pain_dset.json")
 dset = Dataset(dset_file)
 
+# First, let us reduce this Dataset to only two studies
+dset = dset.slice[dset.ids[2:4]]
+
 ###############################################################################
 # Kernels ingest Datasets and can produce a few types of outputs
 # -----------------------------------------------------------------------------
 from nimare.meta.kernel import MKDAKernel
 
 kernel = MKDAKernel()
+
+###############################################################################
+# ``return_type="image"`` returns a list of 3D niimg objects
 image_output = kernel.transform(dset, return_type="image")
 print(type(image_output))
 print(type(image_output[0]))
+print(image_output[0].shape)
 
 ###############################################################################
-
+# ``return_type="array"`` returns a 2D numpy array
 array_output = kernel.transform(dset, return_type="array")
 print(type(array_output))
 print(array_output.shape)
 
 ###############################################################################
-# There is also an option to return an updated Dataset, with the MA maps saved
-# as nifti files and references in the Dataset's images attribute.
+# There is also an option to return an updated Dataset
+# (``return_type="dataset"``), with the MA maps saved as nifti files and
+# references in the Dataset's images attribute.
 # However, this will only work if the Dataset has a location set for its
 # images.
 try:
@@ -69,51 +77,24 @@ except ValueError as error:
 help(MKDAKernel)
 
 ###############################################################################
-# For example, :class:`~nimare.meta.kernel.MKDAKernel` kernel accepts an `r`
+# For example, :class:`~nimare.meta.kernel.MKDAKernel` kernel accepts an ``r``
 # argument to control the radius of the kernel.
-kernel = MKDAKernel(r=2)
-mkda_r02 = kernel.transform(dset, return_type="image")
-kernel = MKDAKernel(r=6)
-mkda_r06 = kernel.transform(dset, return_type="image")
-kernel = MKDAKernel(r=10)
-mkda_r10 = kernel.transform(dset, return_type="image")
-kernel = MKDAKernel(r=14)
-mkda_r14 = kernel.transform(dset, return_type="image")
+RADIUS_VALUES = [2, 6, 10]
+fig, axes = plt.subplots(ncols=3, figsize=(20, 10))
 
-fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(20, 10))
-plot_stat_map(
-    mkda_r02[2],
-    cut_coords=[-2, -10, -4],
-    title="r=2mm",
-    vmax=2,
-    axes=axes[0, 0],
-    draw_cross=False,
-)
-plot_stat_map(
-    mkda_r06[2],
-    cut_coords=[-2, -10, -4],
-    title="r=6mm",
-    vmax=2,
-    axes=axes[0, 1],
-    draw_cross=False,
-)
-plot_stat_map(
-    mkda_r10[2],
-    cut_coords=[-2, -10, -4],
-    title="r=10mm",
-    vmax=2,
-    axes=axes[1, 0],
-    draw_cross=False,
-)
-plot_stat_map(
-    mkda_r14[2],
-    cut_coords=[-2, -10, -4],
-    title="r=14mm",
-    vmax=2,
-    axes=axes[1, 1],
-    draw_cross=False,
-)
-fig.show()
+for i, radius in RADIUS_VALUES:
+    kernel = MKDAKernel(r=radius)
+    ma_maps = kernel.transform(dset, return_type="image")
+
+    plot_stat_map(
+        ma_maps[0],
+        display_mode="z",
+        cut_coords=[-4],
+        title=f"r={radius}mm",
+        vmax=2,
+        axes=axes[i],
+        draw_cross=False,
+    )
 
 ###############################################################################
 # There are several kernels available
@@ -121,10 +102,10 @@ fig.show()
 # :class:`~nimare.meta.kernel.MKDAKernel` convolves coordinates with a
 # sphere and takes the union across voxels.
 kernel = MKDAKernel(r=10)
-mkda_res = kernel.transform(dset, return_type="image")
+ma_maps = kernel.transform(dset, return_type="image")
 
 plot_stat_map(
-    mkda_res[2],
+    ma_maps[0],
     cut_coords=[-2, -10, -4],
     title="MKDA",
     draw_cross=False,
@@ -136,10 +117,10 @@ plot_stat_map(
 from nimare.meta.kernel import KDAKernel
 
 kernel = KDAKernel(r=10)
-kda_res = kernel.transform(dset, return_type="image")
+ma_maps = kernel.transform(dset, return_type="image")
 
 plot_stat_map(
-    kda_res[2],
+    ma_maps[0],
     cut_coords=[-2, -10, -4],
     title="KDA",
     draw_cross=False,
@@ -148,13 +129,16 @@ plot_stat_map(
 ###############################################################################
 # :class:`~nimare.meta.kernel.ALEKernel` convolves coordinates with a 3D
 # Gaussian, for which the FWHM is determined by the sample size of each study.
+# This sample size will be inferred automatically, if that information is
+# available in the Dataset, or it can be set as a constant value across all
+# studies in the Dataset with the ``sample_size`` argument.
 from nimare.meta.kernel import ALEKernel
 
 kernel = ALEKernel(sample_size=20)
-ale_res = kernel.transform(dset, return_type="image")
+ma_maps = kernel.transform(dset, return_type="image")
 
 plot_stat_map(
-    ale_res[2],
+    ma_maps[0],
     cut_coords=[-2, -10, -4],
     title="ALE",
     draw_cross=False,
