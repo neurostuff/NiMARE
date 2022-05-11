36482ea316fd16254da7aa2d5c6d2028ae54ffcb
JulioAPeraza <jpera054@fiu.edu>
2022-05-11 14:19:49 -0400

    another whitespace

--- nimare/meta/utils.py
+++ nimare/meta/utils.py
@@ -329,7 +329,7 @@ def compute_kda_ma(
     -------
     kernel_data_lst : :obj:`list` of :class:`sparse._compressed.compressed.GCXS` \
             or kernel_data: :obj:`numpy.array`
-        If memmap_filename is passed, 3d or 4d array. If `exp_idx` is none, a 3d array in the same 
+        If memmap_filename is passed, 3d or 4d array. If `exp_idx` is none, a 3d array in the same
         shape as the `shape` argument is returned. If `exp_idx` is passed, a 4d array
         is returned, where the first dimension has size equal to the number of
         unique experiments, and the remaining 3 dimensions are equal to `shape`.
