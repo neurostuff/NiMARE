8378f8eef605587e8d17c5dc5cb8be1a5649dee1
Alejandro de la Vega <aleph4@gmail.com>
2022-05-27 20:32:07 -0500

    Replace with scikit-image version

--- nimare/meta/utils.py
+++ nimare/meta/utils.py
@@ -274,19 +274,47 @@ def compute_p2m_ma(
     niis = [nib.Nifti1Image(np.squeeze(result), affine) for result in results]
     return niis
 
-import pandas as pd
-def unique_rows(a):
-    # np.unique() is slow, in part because it sorts;
-    # pd.unique() is much faster, but only 1D
-    # This is inspired by https://github.com/numpy/numpy/issues/11136#issue-325345618
-    # It creates a 1D view where each element is a byte-encoding of a row, then uses
-    # pd.unique(), and then reconstruct the original type.
-    if a.ndim != 2:
-        raise ValueError(f'bad array dimension {a.ndim}; should be 2')
-    b = np.ascontiguousarray(a).view(
-        np.dtype((np.void, a.dtype.itemsize * a.shape[1]))
-    )[:, 0]
-    return pd.unique(b).view(a.dtype).reshape(-1, a.shape[1])
+def unique_rows(ar):
+    """Remove repeated rows from a 2D array.
+    In particular, if given an array of coordinates of shape
+    (Npoints, Ndim), it will remove repeated points.
+    Parameters
+    ----------
+    ar : 2-D ndarray
+        The input array.
+    Returns
+    -------
+    ar_out : 2-D ndarray
+        A copy of the input array with repeated rows removed.
+    Raises
+    ------
+    ValueError : if `ar` is not two-dimensional.
+    Notes
+    -----
+    The function will generate a copy of `ar` if it is not
+    C-contiguous, which will negatively affect performance for large
+    input arrays.
+    Examples
+    --------
+    >>> ar = np.array([[1, 0, 1],
+    ...                [0, 1, 0],
+    ...                [1, 0, 1]], np.uint8)
+    >>> unique_rows(ar)
+    array([[0, 1, 0],
+           [1, 0, 1]], dtype=uint8)
+    """
+    if ar.ndim != 2:
+        raise ValueError("unique_rows() only makes sense for 2D arrays, "
+                         "got %dd" % ar.ndim)
+    # the view in the next line only works if the array is C-contiguous
+    ar = np.ascontiguousarray(ar)
+    # np.unique() finds identical items in a raveled array. To make it
+    # see each row as a single item, we create a view of each row as a
+    # byte string of length itemsize times number of columns in `ar`
+    ar_row_view = ar.view('|S%d' % (ar.itemsize * ar.shape[1]))
+    _, unique_row_indices = np.unique(ar_row_view, return_index=True)
+    ar_out = ar[unique_row_indices]
+    return ar_out
 
 def compute_kda_ma(
     shape,
