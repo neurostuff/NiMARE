5c9bf9782bb2988cb1bbc6d3b62423909381a109
JulioAPeraza <jpera054@fiu.edu>
2022-07-14 21:55:34 -0400

    Return sparse if pre-generated MA maps are found

--- nimare/meta/cbma/base.py
+++ nimare/meta/cbma/base.py
@@ -214,14 +214,33 @@ class CBMAEstimator(Estimator):
 
         Returns
         -------
-        ma_maps : :obj:`numpy.ndarray`, or :obj:`sparse._coo.core.COO`
-            Return a 2D numpy array of shape (n_studies, n_voxels) with MA values,
-            if pre-generated MA maps exist. Otherwise, a 4D sparse array of shape
-            (n_studies, mask.shape) with MA maps is returned.
+        ma_maps : :obj:`sparse._coo.core.COO`
+            Return a 4D sparse array of shape
+            (n_studies, mask.shape) with MA maps.
         """
         if maps_key in self.inputs_.keys():
             LGR.debug(f"Loading pre-generated MA maps ({maps_key}).")
-            ma_maps = self.masker.transform(self.inputs_[maps_key])
+            all_exp = []
+            all_coords = []
+            all_data = []
+            for i_exp, img in enumerate(self.inputs_[maps_key]):
+                img_data = nib.load(img).get_fdata()
+                nonzero_idx = np.where(img_data != 0)
+
+                all_exp.append(np.full(nonzero_idx[0].shape[0], i_exp))
+                all_coords.append(np.vstack(nonzero_idx))
+                all_data.append(img_data[nonzero_idx])
+
+            n_studies = len(self.inputs_[maps_key])
+            shape = img_data.shape
+            kernel_shape = (n_studies,) + shape
+
+            exp = np.hstack(all_exp)
+            coords = np.vstack((exp.flatten(), np.hstack(all_coords)))
+            data = np.hstack(all_data).flatten()
+
+            ma_maps = sparse.COO(coords, data, shape=kernel_shape)
+
         else:
             LGR.debug(f"Generating MA maps from coordinates ({coords_key}).")
 
