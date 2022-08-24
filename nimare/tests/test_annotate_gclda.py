6844ce0495c1d9b8276fe6b22f77fcf3c0e1cbcf
JulioAPeraza <jpera054@fiu.edu>
2022-08-24 17:12:28 -0400

    `DISABLE_JIT=False` after GCLDA tests

--- nimare/tests/test_annotate_gclda.py
+++ nimare/tests/test_annotate_gclda.py
@@ -7,12 +7,12 @@ from numba import config
 
 from nimare import annotate, decode
 
+# Disable numba execution to allow pytest-cov to detect coverage in functions with @jit
+config.DISABLE_JIT = True
+
 
 def test_gclda_symmetric(testdata_laird):
     """A smoke test for GCLDA with symmetric regions."""
-    # Disable numba execution to allow pytest-cov to detect coverage in functions with @jit
-    config.DISABLE_JIT = True
-
     counts_df = annotate.text.generate_counts(
         testdata_laird.texts,
         text_column="abstract",
@@ -87,3 +87,7 @@ def test_gclda_asymmetric(testdata_laird):
     # Encode text
     encoded_img, _ = decode.encode.gclda_encode(model, "fmri activation")
     assert isinstance(encoded_img, nib.Nifti1Image)
+
+
+# Disable numba execution to allow pytest-cov to detect coverage in functions with @jit
+config.DISABLE_JIT = False
