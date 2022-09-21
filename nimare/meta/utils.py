1044a9fa57a48d50588ee447158fdad5598a30f8
Zhen-Qi Liu <liuzhenqi77@gmail.com>
2022-09-20 20:33:55 -0400

    Optimize compute_kda_ma

--- nimare/meta/utils.py
+++ nimare/meta/utils.py
@@ -133,16 +133,18 @@ def compute_kda_ma(
         if sum_overlap:
             counts = counts[idx]
 
-        ma_values = np.zeros(shape)
-        ma_values[tuple(all_spheres.T)] = counts
-        # Set voxel outside the mask to zero.
-        ma_values[~mask_data] = 0
+        sphere_idx_inside_mask = np.where(mask_data[tuple(all_spheres.T)])[0]
+        sphere_idx_filtered = all_spheres[sphere_idx_inside_mask, :].T
+        nonzero_idx = tuple(sphere_idx_filtered)
 
-        nonzero_idx = np.where(ma_values > 0)
+        if isinstance(counts, list):
+            nonzero_to_append = counts[sphere_idx_filtered]
+        else:
+            nonzero_to_append = np.ones((len(sphere_idx_inside_mask),)) * counts
 
         all_exp.append(np.full(nonzero_idx[0].shape[0], i_exp))
         all_coords.append(np.vstack(nonzero_idx))
-        all_data.append(ma_values[nonzero_idx])
+        all_data.append(nonzero_to_append)
 
     exp = np.hstack(all_exp)
     coords = np.vstack((exp.flatten(), np.hstack(all_coords)))
