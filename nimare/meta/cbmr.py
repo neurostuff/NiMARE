[skip CI][WIP] fix a bug in log-likelihood function of CNB model

nimare/meta/cbmr.py
@@ -1130,7 +1130,7 @@ class CBMRInference(object):
                     involved_log_intensity_per_voxel = np.stack(
                         involved_log_intensity_per_voxel, axis=0
                     )
-                else:  # GLH: group-comparison
+                else:  # GLH: group comparison
                     involved_log_intensity_per_voxel = list()
                     for group in con_group_involved:
                         group_log_intensity_per_voxel = np.log(
@@ -1159,15 +1159,8 @@ class CBMRInference(object):
                             k * spatial_coef_dim : (k + 1) * spatial_coef_dim,
                             s * spatial_coef_dim : (s + 1) * spatial_coef_dim,
                         ]
-                        Cov_group_log_intensity = np.empty(shape=(1, 0))
-                        for j in range(n_brain_voxel):
-                            x_j = self.CBMRResults.estimator.inputs_["Coef_spline_bases"][
-                                j, :
-                            ].reshape((1, spatial_coef_dim))
-                            Cov_group_log_intensity_j = x_j @ Cov_beta_ks @ x_j.T
-                            Cov_group_log_intensity = np.concatenate(
-                                (Cov_group_log_intensity, Cov_group_log_intensity_j), axis=1
-                            )
+                        X = self.CBMRResults.estimator.inputs_["Coef_spline_bases"]
+                        Cov_group_log_intensity = (X.dot(Cov_beta_ks) * X).sum(axis=1).reshape((1, -1))
                         Cov_log_intensity = np.concatenate(
                             (Cov_log_intensity, Cov_group_log_intensity), axis=0
                         )  # (m^2, n_voxels)
@@ -1722,6 +1715,8 @@ class GLMCNB(torch.nn.Module):
         device="cpu",
     ):
         n_groups = len(all_foci_per_voxel)
+        all_v = [1 / overdispersion_coef  for overdispersion_coef in all_overdispersion_coef]
+        # estimated intensity and log estimated intensity
         all_log_spatial_intensity = [
             torch.matmul(Coef_spline_bases, all_spatial_coef[i, :, :]) for i in range(n_groups)
         ]
@@ -1747,17 +1742,20 @@ class GLMCNB(torch.nn.Module):
                 torch.exp(log_moderator_effect)
                 for log_moderator_effect in all_log_moderator_effect
             ]
+        all_mu_sum_per_study = [torch.sum(all_spatial_intensity[i]) * all_moderator_effect[i] for i in range(n_groups)]
+        all_group_n_study = [group_foci_per_study.shape[0] for group_foci_per_study in all_foci_per_study]
 
-        # all_mu_sum_per_study = [
-        # torch.sum(all_spatial_intensity[i]) * all_moderator_effect[i] for i in range(n_groups)
-        # ]
         log_l = 0
         for i in range(n_groups):
             log_l += (
-                torch.sum(all_foci_per_voxel[i] * all_log_spatial_intensity[i])
-                + torch.sum(all_foci_per_study[i] * all_log_moderator_effect[i])
-                - torch.sum(all_spatial_intensity[i]) * torch.sum(all_moderator_effect[i])
-            )
+                    all_group_n_study[i] * all_v[i] * torch.log(all_v[i])
+                    - all_group_n_study[i] * torch.lgamma(all_v[i])
+                    + torch.sum(torch.lgamma(all_foci_per_study[i] + all_v[i]))
+                    - torch.sum((all_foci_per_study[i] + all_v[i]) * torch.log(all_mu_sum_per_study[i] + all_v[i]))
+                    + torch.sum(all_foci_per_voxel[i] * all_log_spatial_intensity[i])
+                    + torch.sum(all_foci_per_study[i] * all_log_moderator_effect[i])
+                    )
+            
         return log_l
 
     def forward(self, Coef_spline_bases, all_moderators, all_foci_per_voxel, all_foci_per_study):
@@ -1779,7 +1777,6 @@ class GLMCNB(torch.nn.Module):
             if self.study_level_moderators:
                 log_mu_moderators = all_log_mu_moderators[group]
                 mu_moderators = torch.exp(log_mu_moderators)
-                
             else:
                 n_group_study, _ = group_foci_per_study.shape
                 log_mu_moderators = torch.tensor([0] * n_group_study, device=self.device).reshape(

nimare/utils.py
@@ -1162,6 +1162,7 @@ def _get_cluster_coms(labeled_cluster_arr):
 
     return cluster_coms
 
+
 def coef_spline_bases(axis_coords, spacing, margin):
     """
     Coefficient of cubic B-spline bases in any x/y/z direction
@@ -1169,14 +1170,14 @@ def coef_spline_bases(axis_coords, spacing, margin):
     Parameters
     ----------
     axis_coords : value range in x/y/z direction
-    spacing: (equally spaced) knots spacing in x/y/z direction, 
+    spacing: (equally spaced) knots spacing in x/y/z direction,
     margin: extend the region where B-splines are constructed (min-margin, max_margin)
-            to avoid weakly-supported B-spline on the edge 
+            to avoid weakly-supported B-spline on the edge
     Returns
     -------
     coef_spline : 2-D ndarray (n_points x n_spline_bases)
     """
-    ## create B-spline basis for x/y/z coordinate
+    # create B-spline basis for x/y/z coordinate
     wider_axis_coords = np.arange(np.min(axis_coords) - margin, np.max(axis_coords) + margin)
     knots = np.arange(np.min(axis_coords) - margin, np.max(axis_coords) + margin, step=spacing)
     design_matrix = patsy.dmatrix(
@@ -1194,24 +1195,24 @@ def coef_spline_bases(axis_coords, spacing, margin):
 
 
 def B_spline_bases(masker_voxels, spacing, margin=10):
-    """ Cubic B-spline bases for spatial intensity
+    """Cubic B-spline bases for spatial intensity
 
     The whole coefficient matrix is constructed by taking tensor product of
-    all B-spline bases coefficient matrix in three direction. 
+    all B-spline bases coefficient matrix in three direction.
 
     Parameters
     ----------
     masker_voxels : matrix with element either 0 or 1, indicating if it's within brain mask,
-    spacing: (equally spaced) knots spacing in x/y/z direction, 
+    spacing: (equally spaced) knots spacing in x/y/z direction,
     margin: extend the region where B-splines are constructed (min-margin, max_margin)
-            to avoid weakly-supported B-spline on the edge 
+            to avoid weakly-supported B-spline on the edge
     Returns
     -------
     X : 2-D ndarray (n_voxel x n_spline_bases)
         only keeps with within-brain voxels
     """
-    dim_mask = masker_voxels.shape
-    n_brain_voxel = np.sum(masker_voxels)
+    # dim_mask = masker_voxels.shape
+    # n_brain_voxel = np.sum(masker_voxels)
     # remove the blank space around the brain mask
     xx = np.where(np.apply_over_axes(np.sum, masker_voxels, [1, 2]) > 0)[0]
     yy = np.where(np.apply_over_axes(np.sum, masker_voxels, [0, 2]) > 0)[1]
@@ -1228,11 +1229,19 @@ def B_spline_bases(masker_voxels, spacing, margin=10):
     z_spline_sparse = sparse.COO(z_spline_coords, z_spline[z_spline_coords])
 
     # create spatial design matrix by tensor product of spline bases in 3 dimesion
-    X = np.kron(np.kron(x_spline_sparse, y_spline_sparse), z_spline_sparse)  # Row sums of X are all 1=> There is no need to re-normalise X
+    # Row sums of X are all 1=> There is no need to re-normalise X
+    X = np.kron(np.kron(x_spline_sparse, y_spline_sparse), z_spline_sparse)
     # remove the voxels outside brain mask
     axis_dim = [xx.shape[0], yy.shape[0], zz.shape[0]]
-    brain_voxels_index = [(z - np.min(zz))+ axis_dim[2] * (y - np.min(yy))+ axis_dim[1] * axis_dim[2] * (x - np.min(xx))
-                        for x in xx for y in yy for z in zz if masker_voxels[x, y, z] == 1]
+    brain_voxels_index = [
+        (z - np.min(zz))
+        + axis_dim[2] * (y - np.min(yy))
+        + axis_dim[1] * axis_dim[2] * (x - np.min(xx))
+        for x in xx
+        for y in yy
+        for z in zz
+        if masker_voxels[x, y, z] == 1
+    ]
     X = X[brain_voxels_index, :].todense()
     # remove tensor product basis that have no support in the brain
     x_df, y_df, z_df = x_spline.shape[1], y_spline.shape[1], z_spline.shape[1]
@@ -1241,22 +1250,23 @@ def B_spline_bases(masker_voxels, spacing, margin=10):
     for bx in range(x_df):
         for by in range(y_df):
             for bz in range(z_df):
-                basis_index = bz + z_df*by + z_df*y_df*bx
+                basis_index = bz + z_df * by + z_df * y_df * bx
                 basis_coef = X[:, basis_index]
-                if np.max(basis_coef) >= 0.1: 
+                if np.max(basis_coef) >= 0.1:
                     support_basis.append(basis_index)
     X = X[:, support_basis]
 
     return X
+
 
 def standardize_field(dataset, metadata):
     moderators = dataset.annotations[metadata]
     standardize_moderators = moderators - np.mean(moderators, axis=0)
     standardize_moderators /= np.std(standardize_moderators, axis=0)
     if isinstance(metadata, str):
-        column_name = 'standardized_' + metadata
+        column_name = "standardized_" + metadata
     elif isinstance(metadata, list):
-        column_name = ['standardized_' + moderator for moderator in metadata]
+        column_name = ["standardized_" + moderator for moderator in metadata]
     dataset.annotations[column_name] = standardize_moderators
 
     return dataset
@@ -1272,9 +1282,13 @@ def index2vox(vals, masker_voxels):
     for i in range(image_dim[0]):
         for j in range(image_dim[1]):
             for k in range(image_dim[2]):
-                x,y,z = xx[i], yy[j], zz[k]
-                if masker_voxels[x,y,z] == 1:
-                    voxel_array[x,y,z] = vals[index_count]
+                x, y, z = xx[i], yy[j], zz[k]
+                if masker_voxels[x, y, z] == 1:
+                    voxel_array[x, y, z] = vals[index_count]
                     index_count += 1
 
     return voxel_array
+
+def contrast_matrix_generator():
+    
+    return