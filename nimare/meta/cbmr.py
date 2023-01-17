2ec109f3f4ec32989adbb6db3010c39e2cafcda7
James Kent <jamesdkent21@gmail.com>
2023-01-16 18:06:43 -0600

    function name suggestions

--- nimare/meta/cbmr.py
+++ nimare/meta/cbmr.py
@@ -297,15 +297,16 @@ class CBMREstimator(Estimator):
             'spatial_coef_dim': self.inputs_["coef_spline_bases"].shape[1],
             'moderators_coef_dim': len(self.moderators) if self.moderators else None,
         }
-        if isinstance(self.model, models.NegativeBinomialEstimator):
-            init_weight_kwargs["square_root"] = True
-        if isinstance(self.model, models.ClusteredNegativeBinomialEstimator):
-            init_weight_kwargs["square_root"] = False
 
         self.model.init_weights(**init_weight_kwargs)
 
         moderators_by_group = self.inputs_["moderators_by_group"] if self.moderators else None
-        self.model.fit(self.inputs_["coef_spline_bases"], moderators_by_group, self.inputs_["foci_per_voxel"], self.inputs_["foci_per_study"])
+        self.model.fit(
+            self.inputs_["coef_spline_bases"],
+            moderators_by_group,
+            self.inputs_["foci_per_voxel"],
+            self.inputs_["foci_per_study"]
+        )
 
 
         maps, tables = self.model.summary()
@@ -373,6 +374,7 @@ class CBMRInference(object):
                     f"""The shape of {str(wrong_con_group_idx)}th contrast vector(s) in group-wise
                     intensity contrast matrix doesn't match with groups"""
                 )
+            # remove zero rows in contrast matrix (function: remove_empty_rows)
             con_group_zero_row = [
                 np.where(np.sum(np.abs(con_group), axis=1) == 0)[0]
                 for con_group in self.t_con_group
@@ -390,7 +392,7 @@ class CBMRInference(object):
                         contrast matrix are all zeros"""
                     )
             self._Name_of_con_group()
-            # standardization
+            # standardization (function: standardize_contrast)
             self.t_con_group = [
                 con_group / np.sum(np.abs(con_group), axis=1).reshape((-1, 1))
                 for con_group in self.t_con_group
