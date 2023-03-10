c30add51bb8b3d34f1f3ecb32cffc6f0abbacb89
Julio A. Peraza <52050407+JulioAPeraza@users.noreply.github.com>
2023-03-10 16:58:20 -0500

    Support clusters table in Diagnostics (#765)
    * Support clusters table in Diagnostics
    
    * Fix ale workflow
    
    * Update ale.py
    
    * Use reporting.get_clusters_table to get clusters table
    
    * Skip get_clusters_table if table exist in result object
    
    * Change order of testing workflows
    
    * Revert "Change order of testing workflows"
    
    This reverts commit d25d8ce3735def4754dfaff10a15f0eb252bfcb6.
    
    * Show clusters table in the example
    
    * Update test_diagnostics.py
    
    * Update diagnostics.py
    
    * Add _get_clusters_table from nilearn
    
    * Update diagnostics.py
    
    * Update utils.py
    
    * Update setup.cfg
    
    * Pin minimum version of nilearn
    
    * Update doc, add latest changes from nilearn

--- nimare/workflows/ale.py
+++ nimare/workflows/ale.py
@@ -115,7 +115,7 @@ References
             target_image="z_desc-size_level-cluster_corr-FWE_method-montecarlo",
             voxel_thresh=None,
         )
-        count_df, _ = fcounter.transform(cres)
+        count_df, _, _ = fcounter.transform(cres)
 
         boilerplate = boilerplate.format(
             n_exps=len(dset.ids),
@@ -214,10 +214,10 @@ References
             target_image="z_desc-size_level-cluster_corr-FWE_method-montecarlo",
             voxel_thresh=None,
         )
-        count_df1, _ = fcounter.transform(cres1)
+        count_df1, _, _ = fcounter.transform(cres1)
 
         cres2 = corr.transform(res2)
-        count_df2, _ = fcounter.transform(cres2)
+        count_df2, _, _ = fcounter.transform(cres2)
 
         sub = ALESubtraction(n_iters=n_iters, kernel__fwhm=fwhm)
         sres = sub.fit(dset1, dset2)
