833bf2f9cbc44575111ac2fe0cd0d9bcd06b28af
James Kent <jamesdkent21@gmail.com>
2023-03-14 08:47:32 -0500

    add little more detail to example

--- examples/01_datasets/05_plot_nimads.py
+++ examples/01_datasets/05_plot_nimads.py
@@ -13,29 +13,37 @@ import os.path as op
 
 from nimare.io import convert_nimads_to_dataset
 from nimare.nimads import Studyset
-from nimare.utils import get_test_data_path
+from nimare.tests.utils import get_test_data_path
 
 ###############################################################################
 # Access Data
 # -----------------------------------------------------------------------------
+
 nimads_studyset = op.join(get_test_data_path(), "nimads_studyset.json")
 nimads_annotation = op.join(get_test_data_path(), "nimads_annotation.json")
 
 ###############################################################################
 # Load Data
 # -----------------------------------------------------------------------------
+# Load the json files into a NiMADS Studyset object.
+
 studyset = Studyset(nimads_studyset, nimads_annotation)
 
 
 ###############################################################################
 # Convert to NiMARE Dataset
 # -----------------------------------------------------------------------------
+# Convert the NiMADS Studyset object to a NiMARE Dataset object.
+# Then you can run NiMARE analyses on the Dataset object.
 
 nimare_dset = studyset.to_dataset()
-
+nimare_dset.coordinates.head()
 
 ###############################################################################
 # Directly to NiMARE Dataset
 # -----------------------------------------------------------------------------
+# Alternatively, you can convert the NiMADS json files directly to a NiMARE Dataset object
+# if you wish to skip using the nimads studyset object directly.
 
 nimare_dset_2 = convert_nimads_to_dataset(nimads_studyset, nimads_annotation)
+nimare_dset_2.coordinates.head()
