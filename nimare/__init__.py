1f4db733fff514f1dc4a043011a69785b10415f5
JulioAPeraza <jpera054@fiu.edu>
2023-03-31 16:24:59 -0400

    Drop Python 3.6 and 3.7. Support Python 3.11

--- nimare/__init__.py
+++ nimare/__init__.py
@@ -1,6 +1,5 @@
 """NiMARE: Neuroimaging Meta-Analysis Research Environment."""
 import logging
-import sys
 import warnings
 
 from ._version import get_versions
@@ -41,34 +40,3 @@ with warnings.catch_warnings(record=True) as w:
     ]
 
 del get_versions
-
-
-def _py367_deprecation_warning():
-    """Deprecation warnings message.
-
-    Notes
-    -----
-    Adapted from Nilearn.
-    """
-    py36_warning = (
-        "Python 3.6 and 3.7 support is deprecated and will be removed in release 0.1.0 of NiMARE. "
-        "Consider switching to Python 3.8, 3.9 or 3.10."
-    )
-    warnings.filterwarnings("once", message=py36_warning)
-    warnings.warn(message=py36_warning, category=FutureWarning, stacklevel=3)
-
-
-def _python_deprecation_warnings():
-    """Raise deprecation warnings.
-
-    Notes
-    -----
-    Adapted from Nilearn.
-    """
-    if sys.version_info.major == 3 and (
-        sys.version_info.minor == 6 or sys.version_info.minor == 7
-    ):
-        _py367_deprecation_warning()
-
-
-_python_deprecation_warnings()
