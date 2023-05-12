6bc6e0f3c1960cd28dcc44885c0007dd9acdc891
JulioAPeraza <jpera054@fiu.edu>
2023-05-12 16:14:29 -0400

    Update test_reports.py

--- nimare/tests/test_reports.py
+++ nimare/tests/test_reports.py
@@ -7,18 +7,17 @@ from nimare.reports.base import run_reports
 
 def test_reports_function_smoke(tmp_path_factory, testdata_cbma_full):
     """Run smoke test for CBMA workflow."""
-    # tmpdir = tmp_path_factory.mktemp("test_reports_function_smoke")
-    output_dir = "/Users/jperaza/Desktop/test_report"
+    tmpdir = tmp_path_factory.mktemp("test_reports_function_smoke")
     results = workflows.cbma_workflow(
         testdata_cbma_full,
         estimator="ale",
         corrector="fdr",
         diagnostics="focuscounter",
-        output_dir=output_dir,
+        output_dir=tmpdir,
     )
 
-    run_reports(results, output_dir)
+    run_reports(results, tmpdir)
 
     filename = "report.html"
-    outpath = op.join(output_dir, filename)
+    outpath = op.join(tmpdir, filename)
     assert op.isfile(outpath)
