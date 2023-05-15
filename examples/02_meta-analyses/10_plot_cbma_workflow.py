d374adbe3442bf472bf96f2f2e24cad1adb39aee
JulioAPeraza <jpera054@fiu.edu>
2023-05-15 16:07:48 -0400

    [skip ci] explore rtd directory tree

--- examples/02_meta-analyses/10_plot_cbma_workflow.py
+++ examples/02_meta-analyses/10_plot_cbma_workflow.py
@@ -92,6 +92,15 @@ run_reports(result, html_dir)
 print(os.listdir(html_dir))
 print(os.listdir(root_dir))
 
+for dirname, dirnames, filenames in os.walk(root_dir):
+    # print path to all subdirectories first.
+    for subdirname in dirnames:
+        print(os.path.join(dirname, subdirname))
+
+    # print path to all filenames.
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
 ####################################
 # .. raw:: html
 #
