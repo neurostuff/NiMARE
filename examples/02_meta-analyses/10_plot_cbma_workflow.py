e485ec2b3711e54f9260867b8c8cab80f8480c5e
JulioAPeraza <jpera054@fiu.edu>
2023-05-15 21:18:22 -0400

    [skip ci] update path

--- examples/02_meta-analyses/10_plot_cbma_workflow.py
+++ examples/02_meta-analyses/10_plot_cbma_workflow.py
@@ -82,12 +82,15 @@ result.tables["z_corr-FDR_method-indep_Jackknife"]
 root_dir = Path(os.getcwd()).parents[1]
 # Use the follofing path to test locally:
 # html_dir = root_dir / "docs" / "_build" / "html" / "auto_examples" / "02_meta-analyses"
-html_dir = root_dir / "docs" / "auto_examples" / "02_meta-analyses"
+# html_dir = root_dir / "docs" / "auto_examples" / "02_meta-analyses"
+html_dir = root_dir / "_readthedocs" / "html" / "auto_examples" / "02_meta-analyses"
 html_dir.mkdir(parents=True, exist_ok=True)
+print(os.listdir(root_dir / "_readthedocs" / "html"))
+print(os.listdir(html_dir))
 
 run_reports(result, html_dir)
 
 ####################################
 # .. raw:: html
 #
-#     <iframe src="report.html" width="100%" height="1000px"></iframe>
+#     <iframe src="./report.html" width="100%" height="1000px"></iframe>
