088845080e33dfc78b730386004c559719b14c6a
JulioAPeraza <jpera054@fiu.edu>
2023-06-23 22:22:26 -0400

    Add test for IBMA reports

--- nimare/tests/test_reports.py
+++ nimare/tests/test_reports.py
@@ -8,36 +8,51 @@ from nimare.diagnostics import FocusCounter, Jackknife
 from nimare.meta.cbma import ALESubtraction
 from nimare.meta.cbma.base import PairwiseCBMAEstimator
 from nimare.reports.base import run_reports
-from nimare.workflows import CBMAWorkflow, PairwiseCBMAWorkflow
+from nimare.workflows import CBMAWorkflow, IBMAWorkflow, PairwiseCBMAWorkflow
 
 
 @pytest.mark.parametrize(
-    "estimator,corrector,diagnostics",
+    "estimator,corrector,diagnostics,meta_type",
     [
-        ("ale", FWECorrector(method="montecarlo", n_iters=10), "jackknife"),
-        ("kda", "fdr", "focuscounter"),
+        ("ale", FWECorrector(method="montecarlo", n_iters=10), "jackknife", "cbma"),
+        ("kda", "fdr", "focuscounter", "cbma"),
         (
             "mkdachi2",
             FWECorrector(method="montecarlo", n_iters=10),
             Jackknife(voxel_thresh=0.1),
+            "pairwise_cbma",
         ),
         (
             ALESubtraction(n_iters=10),
             "fdr",
             FocusCounter(voxel_thresh=0.01, display_second_group=True),
+            "pairwise_cbma",
         ),
+        ("stouffers", "fdr", "jackknife", "ibma"),
     ],
 )
 def test_reports_function_smoke(
     tmp_path_factory,
     testdata_cbma_full,
+    testdata_ibma,
     estimator,
     corrector,
     diagnostics,
+    meta_type,
 ):
     """Run smoke test for CBMA workflow."""
     tmpdir = tmp_path_factory.mktemp("test_reports_function_smoke")
-    if estimator == "mkdachi2" or issubclass(type(estimator), PairwiseCBMAEstimator):
+
+    if meta_type == "cbma":
+        workflow = CBMAWorkflow(
+            estimator=estimator,
+            corrector=corrector,
+            diagnostics=diagnostics,
+            output_dir=tmpdir,
+        )
+        results = workflow.fit(testdata_cbma_full)
+
+    elif meta_type == "pairwise_cbma":
         dset1 = testdata_cbma_full.slice(testdata_cbma_full.ids[:10])
         dset2 = testdata_cbma_full.slice(testdata_cbma_full.ids[10:])
 
@@ -48,14 +63,15 @@ def test_reports_function_smoke(
             output_dir=tmpdir,
         )
         results = workflow.fit(dset1, dset2)
-    else:
-        workflow = CBMAWorkflow(
+
+    elif meta_type == "ibma":
+        workflow = IBMAWorkflow(
             estimator=estimator,
             corrector=corrector,
             diagnostics=diagnostics,
             output_dir=tmpdir,
         )
-        results = workflow.fit(testdata_cbma_full)
+        results = workflow.fit(testdata_ibma)
 
     run_reports(results, tmpdir)
 
