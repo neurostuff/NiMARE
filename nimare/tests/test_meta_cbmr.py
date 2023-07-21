e59a083b813e615eed7b9e2308ca6921deeff91f
James Kent <jamesdkent21@gmail.com>
2023-07-21 13:18:13 -0400

    [FIX] regular expression for no moderators (#821)
    * add a test with none moderator variable for CBMRInference
    
    * add a separate test for moderators=None
    
    * fix none moderators
    
    * add file back in from main
    
    ---------
    
    Co-authored-by: Yifan Yu <40786074+yifan0330@users.noreply.github.com>
    Co-authored-by: jdkent <jdkent@users.noreply.github.com>

--- nimare/tests/test_meta_cbmr.py
+++ nimare/tests/test_meta_cbmr.py
@@ -39,7 +39,7 @@ def cbmr_result(testdata_cbmr_simulated, model):
     cbmr = CBMREstimator(
         group_categories=["diagnosis", "drug_status"],
         moderators=["standardized_sample_sizes", "standardized_avg_age", "schizophrenia_subtype"],
-        spline_spacing=200,
+        spline_spacing=100,
         model=model,
         penalty=False,
         lr=1e-2,
@@ -56,7 +56,7 @@ def cbmr_result(testdata_cbmr_simulated, model):
 @pytest.fixture(scope="session")
 def inference_results(testdata_cbmr_simulated, cbmr_result):
     """Test inference results for CBMR estimator."""
-    inference = CBMRInference(device="cuda")
+    inference = CBMRInference(device="cpu")
     inference.fit(cbmr_result)
     t_con_groups = inference.create_contrast(
         [
@@ -121,7 +121,39 @@ def test_firth_penalty(testdata_cbmr_simulated):
     )
     res = cbmr.fit(dataset=dset)
     assert isinstance(res, nimare.results.MetaResult)
+    
+    
+def test_moderators_none(testdata_cbmr_simulated):
+    """Unit test for Firth penalty."""
+    dset = StandardizeField(fields=["sample_sizes", "avg_age", "schizophrenia_subtype"]).transform(
+        testdata_cbmr_simulated
+    )
+    cbmr = CBMREstimator(
+        group_categories=["diagnosis", "drug_status"],
+        moderators=None,
+        spline_spacing=100,
+        model=models.PoissonEstimator,
+        penalty=False,
+        lr=1e-2,
+        tol=1e7,
+        device="cpu",
+    )
+    res = cbmr.fit(dataset=dset)
+    assert isinstance(res, nimare.results.MetaResult)
+    inference = CBMRInference(device="cpu")
+    inference.fit(res)
 
+    t_con_groups = inference.create_contrast(
+        [
+            "DepressionYes",
+        ],
+        source="groups",
+    )
+    inference_results = inference.transform(
+        t_con_groups=t_con_groups
+    )
+    
+    assert isinstance(inference_results, nimare.results.MetaResult)
 
 def test_CBMREstimator_update(testdata_cbmr_simulated):
     """Unit test for CBMR estimator update function."""
