8c35daa9c029a71a4d8a66f00e68cb7c58193526
JulioAPeraza <jpera054@fiu.edu>
2023-12-13 17:43:23 -0500

    Update cli.py

--- nimare/cli.py
+++ nimare/cli.py
@@ -3,7 +3,6 @@ import argparse
 import os.path as op
 
 from nimare.io import convert_neurosynth_to_json, convert_sleuth_to_json
-from nimare.workflows.ale import ale_sleuth_workflow
 from nimare.workflows.macm import macm_workflow
 
 
@@ -24,71 +23,6 @@ def _get_parser():
     """
     parser = argparse.ArgumentParser(prog="nimare")
     subparsers = parser.add_subparsers(help="NiMARE workflows")
-    # ALE sleuth workflow
-    ale_parser = subparsers.add_parser(
-        "ale",
-        help=(
-            "Run an activation likelihood estimation (ALE) meta-analysis on "
-            "a Sleuth text file. ALE is a permutation-based meta-analysis "
-            "of coordinates that uses 3D Gaussians to model activation."
-        ),
-    )
-    ale_parser.set_defaults(func=ale_sleuth_workflow)
-    ale_parser.add_argument(
-        "sleuth_file",
-        type=lambda x: _is_valid_file(parser, x),
-        help=("Sleuth text file to analyze."),
-    )
-    ale_parser.add_argument(
-        "--output_dir",
-        dest="output_dir",
-        metavar="PATH",
-        type=str,
-        help=("Output directory."),
-        default=".",
-    )
-    ale_parser.add_argument(
-        "--prefix", dest="prefix", type=str, help=("Common prefix for output maps."), default=""
-    )
-    ale_parser.add_argument(
-        "--file2",
-        dest="sleuth_file2",
-        type=str,
-        help=("Optional second Sleuth file for subtraction analysis."),
-        default=None,
-    )
-    ale_parser.add_argument(
-        "--n_iters",
-        dest="n_iters",
-        type=int,
-        help=("Number of iterations for permutation testing."),
-        default=10000,
-    )
-    ale_parser.add_argument(
-        "--v_thr",
-        dest="v_thr",
-        type=float,
-        help=("Voxel p-value threshold used to create clusters."),
-        default=0.001,
-    )
-    ale_parser.add_argument(
-        "--fwhm",
-        dest="fwhm",
-        type=float,
-        help=(
-            "Override sample size-based kernel determination with a "
-            "single FWHM (in mm) applied to all experiments. Useful "
-            "when sample size is not available for all data."
-        ),
-        default=None,
-    )
-    ale_parser.add_argument(
-        "--n_cores",
-        dest="n_cores",
-        type=int,
-        default=1,
-        help=("Number of processes to use for meta-analysis. If -1, use all available cores."),
-    )
 
     # MACM
     macm_parser = subparsers.add_parser(
