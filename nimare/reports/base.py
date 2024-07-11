34ad68d4a4827151945f64a5934b1f8df78bf16b
Julio A. Peraza <52050407+JulioAPeraza@users.noreply.github.com>
2024-07-11 04:42:00 -0700

    Support Python 3.12 (#853)
    * Support Python 3.12
    
    * Attempt to fix distutils deprecation error in Python 3.12
    
    * Deprecate pkg_resources in favor of importlib.resources
    
    * Use importlib_resources in Python 3.8
    
    * fix cognitive atlas
    
    ---------
    
    Co-authored-by: James Kent <jamesdkent21@gmail.com>

--- nimare/reports/base.py
+++ nimare/reports/base.py
@@ -26,9 +26,14 @@ import textwrap
 from glob import glob
 from pathlib import Path
 
+try:
+    from importlib.resources import files
+except ImportError:
+    # Python < 3.9
+    from importlib_resources import files
+
 import jinja2
 import pandas as pd
-from pkg_resources import resource_filename as pkgrf
 
 from nimare.meta.cbma.base import CBMAEstimator, PairwiseCBMAEstimator
 from nimare.reports.figures import (
@@ -545,8 +550,9 @@ class Report:
             _gen_figures(self.results, img_key, diag_name, threshold, self.fig_dir)
 
         # Default template from nimare
-        self.template_path = Path(pkgrf("nimare", "reports/report.tpl"))
-        self._load_config(Path(pkgrf("nimare", "reports/default.yml")))
+        nimare_path = files("nimare")
+        self.template_path = nimare_path / "reports" / "report.tpl"
+        self._load_config(nimare_path / "reports" / "default.yml")
         assert self.template_path.exists()
 
     def _load_config(self, config):
