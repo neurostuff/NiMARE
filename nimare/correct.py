[FIX] underflow (and overflow) issues (#1069)
* fix numeric underflow/overflow issues

* handle other areas of under/overflow

* test infinite values

* with updated numpy 2.4.4

nimare/correct.py
@@ -10,7 +10,7 @@ from pymare.stats import bonferroni, fdr
 from nimare.base import NiMAREBase
 from nimare.results import MetaResult
 from nimare.transforms import p_to_z
-from nimare.utils import DEFAULT_FLOAT_DTYPE
+from nimare.utils import DEFAULT_FLOAT_DTYPE, _clip_p_values, _p_to_logp_values
 
 LGR = logging.getLogger(__name__)
 
@@ -99,7 +99,8 @@ class Corrector(NiMAREBase):
 
     def _generate_secondary_maps(self, result, corr_maps, rm):
         """Generate corrected version of z and log-p maps if they exist."""
-        p = corr_maps[rm]
+        p = _clip_p_values(corr_maps[rm], dtype=DEFAULT_FLOAT_DTYPE, copy=False)
+        corr_maps[rm] = p
 
         if rm == "p":
             z_map_name, logp_map_name = "z", "logp"
@@ -109,7 +110,7 @@ class Corrector(NiMAREBase):
             corr_maps[z_map_name] = p_to_z(p) * np.sign(result.maps[z_map_name])
 
         if logp_map_name in result.maps:
-            corr_maps[logp_map_name] = -np.log10(p)
+            corr_maps[logp_map_name] = _p_to_logp_values(p, dtype=DEFAULT_FLOAT_DTYPE)
 
         return corr_maps
 

nimare/decode/discrete.py
@@ -12,7 +12,7 @@ from nimare.decode.utils import weight_priors
 from nimare.meta.kernel import KernelTransformer, MKDAKernel
 from nimare.stats import one_way, pearson, two_way
 from nimare.transforms import p_to_z
-from nimare.utils import _check_type, _mask_img_to_bool, get_masker
+from nimare.utils import _check_type, _clip_p_values, _mask_img_to_bool, get_masker
 
 
 def gclda_decode_roi(model, roi, topic_priors=None, prior_weight=1.0):
@@ -330,7 +330,8 @@ def brainmap_decode(
 
     # Significance testing
     # Forward inference significance is determined with a binomial distribution
-    p_fi = 1 - binom.cdf(k=n_selected_term, n=n_term_foci, p=p_selected)
+    p_fi = binom.sf(k=n_selected_term, n=n_term_foci, p=p_selected)
+    p_fi = _clip_p_values(p_fi, dtype=p_fi.dtype, copy=False)
     sign_fi = np.sign(
         n_selected_term - np.mean(n_selected_term)
     ).ravel()  # pylint: disable=no-member
@@ -344,6 +345,7 @@ def brainmap_decode(
     ).T
     chi2_ri = two_way(cells)
     p_ri = special.chdtrc(1, chi2_ri)
+    p_ri = _clip_p_values(p_ri, dtype=p_ri.dtype, copy=False)
     sign_ri = np.sign(p_selected_g_term - p_selected_g_noterm).ravel()  # pylint: disable=no-member
 
     # Ignore rare features
@@ -627,6 +629,7 @@ def neurosynth_decode(
     # One-way chi-square test for uniformity of term frequency across terms
     chi2_fi = one_way(n_selected_term, n_term)
     p_fi = special.chdtrc(1, chi2_fi)
+    p_fi = _clip_p_values(p_fi, dtype=p_fi.dtype, copy=False)
     sign_fi = np.sign(
         n_selected_term - np.mean(n_selected_term)
     ).ravel()  # pylint: disable=no-member
@@ -640,6 +643,7 @@ def neurosynth_decode(
     ).T
     chi2_ri = two_way(cells)
     p_ri = special.chdtrc(1, chi2_ri)
+    p_ri = _clip_p_values(p_ri, dtype=p_ri.dtype, copy=False)
     sign_ri = np.sign(p_selected_g_term - p_selected_g_noterm).ravel()  # pylint: disable=no-member
 
     # Multiple comparisons correction across terms. Separately done for FI and RI.

nimare/meta/cbma/ale.py
@@ -33,6 +33,7 @@ from nimare.transforms import p_to_z
 from nimare.utils import (
     DEFAULT_FLOAT_DTYPE,
     _check_ncores,
+    _p_to_logp_values,
     mm2vox,
     use_memmap,
 )
@@ -968,7 +969,7 @@ class ALESubtraction(PairwiseCBMAEstimator):
                     iter_diff_values._mmap.close()
 
         z_arr = p_to_z(p_values, tail="two") * diff_signs
-        logp_arr = -np.log10(p_values)
+        logp_arr = _p_to_logp_values(p_values, dtype=DEFAULT_FLOAT_DTYPE)
 
         maps = {
             "stat_desc-group1MinusGroup2": diff_ale_values,
@@ -1277,7 +1278,6 @@ class ALESubtraction(PairwiseCBMAEstimator):
         stat_values = result.get_map("stat_desc-group1MinusGroup2", return_type="array")
         z_values = result.get_map("z_desc-group1MinusGroup2", return_type="array")
         sign = np.sign(z_values)
-        eps = np.spacing(1)
 
         if n_iters is None:
             n_iters = self.n_iters
@@ -1365,8 +1365,7 @@ class ALESubtraction(PairwiseCBMAEstimator):
         vfwe_null = self.null_distributions_["values_level-voxel_corr-fwe_method-montecarlo"]
         p_vfwe_vals = null_to_p(np.abs(stat_values), vfwe_null, tail="upper")
         z_vfwe_vals = p_to_z(p_vfwe_vals, tail="two") * sign
-        logp_vfwe_vals = -np.log10(p_vfwe_vals)
-        logp_vfwe_vals[np.isinf(logp_vfwe_vals)] = -np.log10(eps)
+        logp_vfwe_vals = _p_to_logp_values(p_vfwe_vals, dtype=DEFAULT_FLOAT_DTYPE)
 
         maps = {
             "p_desc-group1MinusGroup2_level-voxel": p_vfwe_vals,
@@ -1420,12 +1419,10 @@ class ALESubtraction(PairwiseCBMAEstimator):
             )
 
             z_cmfwe_vals = p_to_z(p_cmfwe_values, tail="two") * sign
-            logp_cmfwe_vals = -np.log10(p_cmfwe_values)
-            logp_cmfwe_vals[np.isinf(logp_cmfwe_vals)] = -np.log10(eps)
+            logp_cmfwe_vals = _p_to_logp_values(p_cmfwe_values, dtype=DEFAULT_FLOAT_DTYPE)
 
             z_csfwe_vals = p_to_z(p_csfwe_values, tail="two") * sign
-            logp_csfwe_vals = -np.log10(p_csfwe_values)
-            logp_csfwe_vals[np.isinf(logp_csfwe_vals)] = -np.log10(eps)
+            logp_csfwe_vals = _p_to_logp_values(p_csfwe_values, dtype=DEFAULT_FLOAT_DTYPE)
 
             maps.update(
                 {
@@ -1649,8 +1646,7 @@ class SCALE(CBMAEstimator):
 
         p_values, z_values = self._scale_to_p(stat_values, exceedance_counts)
 
-        logp_values = -np.log10(p_values)
-        logp_values[np.isinf(logp_values)] = -np.log10(np.finfo(float).eps)
+        logp_values = _p_to_logp_values(p_values, dtype=DEFAULT_FLOAT_DTYPE, copy=False)
 
         # Write out unthresholded value images
         maps = {"stat": stat_values, "logp": logp_values, "z": z_values}
@@ -1826,8 +1822,11 @@ class SCALE(CBMAEstimator):
 
         p_vfwe_values = null_to_p(stat_values, fwe_voxel_max, tail="upper")
         z_vfwe_values = p_to_z(p_vfwe_values, tail="one")
-        logp_vfwe_values = -np.log10(p_vfwe_values)
-        logp_vfwe_values[np.isinf(logp_vfwe_values)] = -np.log10(np.finfo(float).eps)
+        logp_vfwe_values = _p_to_logp_values(
+            p_vfwe_values,
+            dtype=DEFAULT_FLOAT_DTYPE,
+            copy=False,
+        )
 
         self.null_distributions_["values_level-voxel_corr-fwe_method-montecarlo"] = fwe_voxel_max
 

nimare/meta/cbma/base.py
@@ -26,6 +26,7 @@ from nimare.utils import (
     _check_ncores,
     _check_type,
     _mask_img_to_bool,
+    _p_to_logp_values,
     get_masker,
     get_masker_mask_image,
     mm2vox,
@@ -912,8 +913,7 @@ class CBMAEstimator(Estimator):
                         nib.Nifti1Image(p_cmfwe_map, self.masker.mask_img.affine)
                     )
                 )
-                logp_cmfwe_values = -np.log10(p_cmfwe_values)
-                logp_cmfwe_values[np.isinf(logp_cmfwe_values)] = -np.log10(np.finfo(float).eps)
+                logp_cmfwe_values = _p_to_logp_values(p_cmfwe_values, dtype=DEFAULT_FLOAT_DTYPE)
                 z_cmfwe_values = p_to_z(p_cmfwe_values, tail="one")
 
                 # Cluster size-based inference
@@ -926,8 +926,7 @@ class CBMAEstimator(Estimator):
                         nib.Nifti1Image(p_csfwe_map, self.masker.mask_img.affine)
                     )
                 )
-                logp_csfwe_values = -np.log10(p_csfwe_values)
-                logp_csfwe_values[np.isinf(logp_csfwe_values)] = -np.log10(np.finfo(float).eps)
+                logp_csfwe_values = _p_to_logp_values(p_csfwe_values, dtype=DEFAULT_FLOAT_DTYPE)
                 z_csfwe_values = p_to_z(p_csfwe_values, tail="one")
 
                 self.null_distributions_[
@@ -945,8 +944,7 @@ class CBMAEstimator(Estimator):
             )
 
         z_vfwe_values = p_to_z(p_vfwe_values, tail="one")
-        logp_vfwe_values = -np.log10(p_vfwe_values)
-        logp_vfwe_values[np.isinf(logp_vfwe_values)] = -np.log10(np.finfo(float).eps)
+        logp_vfwe_values = _p_to_logp_values(p_vfwe_values, dtype=DEFAULT_FLOAT_DTYPE)
 
         if vfwe_only:
             # Return unthresholded value images

nimare/meta/cbma/mkda.py
@@ -19,7 +19,13 @@ from nimare.meta.kernel import KDAKernel, MKDAKernel
 from nimare.meta.utils import _calculate_cluster_measures
 from nimare.stats import null_to_p, one_way, two_way
 from nimare.transforms import p_to_z
-from nimare.utils import DEFAULT_FLOAT_DTYPE, _check_ncores, _mask_img_to_bool, vox2mm
+from nimare.utils import (
+    DEFAULT_FLOAT_DTYPE,
+    _check_ncores,
+    _mask_img_to_bool,
+    _p_to_logp_values,
+    vox2mm,
+)
 
 LGR = logging.getLogger(__name__)
 __version__ = _version.get_versions()["version"]
@@ -861,7 +867,6 @@ class MKDAChi2(PairwiseCBMAEstimator):
         rand_idx2 = np.random.choice(null_xyz.shape[0], size=(iter_df2.shape[0], n_iters))
         rand_xyz2 = null_xyz[rand_idx2, :]
         iter_xyzs2 = np.split(rand_xyz2, rand_xyz2.shape[1], axis=1)
-        eps = np.spacing(1)
 
         # Identify summary statistic corresponding to intensity threshold
         ss_thresh = chi2.isf(voxel_thresh, 1)
@@ -946,25 +951,19 @@ class MKDAChi2(PairwiseCBMAEstimator):
         # Convert p-values
         # pAgF
         pAgF_z_vfwe_vals = p_to_z(pAgF_p_vfwe_vals, tail="two") * pAgF_sign
-        pAgF_logp_vfwe_vals = -np.log10(pAgF_p_vfwe_vals)
-        pAgF_logp_vfwe_vals[np.isinf(pAgF_logp_vfwe_vals)] = -np.log10(eps)
+        pAgF_logp_vfwe_vals = _p_to_logp_values(pAgF_p_vfwe_vals, dtype=DEFAULT_FLOAT_DTYPE)
         pAgF_z_cmfwe_vals = p_to_z(pAgF_p_cmfwe_vals, tail="two") * pAgF_sign
-        pAgF_logp_cmfwe_vals = -np.log10(pAgF_p_cmfwe_vals)
-        pAgF_logp_cmfwe_vals[np.isinf(pAgF_logp_cmfwe_vals)] = -np.log10(eps)
+        pAgF_logp_cmfwe_vals = _p_to_logp_values(pAgF_p_cmfwe_vals, dtype=DEFAULT_FLOAT_DTYPE)
         pAgF_z_csfwe_vals = p_to_z(pAgF_p_csfwe_vals, tail="two") * pAgF_sign
-        pAgF_logp_csfwe_vals = -np.log10(pAgF_p_csfwe_vals)
-        pAgF_logp_csfwe_vals[np.isinf(pAgF_logp_csfwe_vals)] = -np.log10(eps)
+        pAgF_logp_csfwe_vals = _p_to_logp_values(pAgF_p_csfwe_vals, dtype=DEFAULT_FLOAT_DTYPE)
 
         # pFgA
         pFgA_z_vfwe_vals = p_to_z(pFgA_p_vfwe_vals, tail="two") * pFgA_sign
-        pFgA_logp_vfwe_vals = -np.log10(pFgA_p_vfwe_vals)
-        pFgA_logp_vfwe_vals[np.isinf(pFgA_logp_vfwe_vals)] = -np.log10(eps)
+        pFgA_logp_vfwe_vals = _p_to_logp_values(pFgA_p_vfwe_vals, dtype=DEFAULT_FLOAT_DTYPE)
         pFgA_z_cmfwe_vals = p_to_z(pFgA_p_cmfwe_vals, tail="two") * pFgA_sign
-        pFgA_logp_cmfwe_vals = -np.log10(pFgA_p_cmfwe_vals)
-        pFgA_logp_cmfwe_vals[np.isinf(pFgA_logp_cmfwe_vals)] = -np.log10(eps)
+        pFgA_logp_cmfwe_vals = _p_to_logp_values(pFgA_p_cmfwe_vals, dtype=DEFAULT_FLOAT_DTYPE)
         pFgA_z_csfwe_vals = p_to_z(pFgA_p_csfwe_vals, tail="two") * pFgA_sign
-        pFgA_logp_csfwe_vals = -np.log10(pFgA_p_csfwe_vals)
-        pFgA_logp_csfwe_vals[np.isinf(pFgA_logp_csfwe_vals)] = -np.log10(eps)
+        pFgA_logp_csfwe_vals = _p_to_logp_values(pFgA_p_csfwe_vals, dtype=DEFAULT_FLOAT_DTYPE)
 
         maps = {
             # uniformity analysis